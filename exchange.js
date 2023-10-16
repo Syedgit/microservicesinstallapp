c39512cead392b5d6b2424efea9b5cbc88a9262c
Syed Hussain <aftab.jersey@gmail.com>
2023-10-15 22:42:05 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -31,3 +31,72 @@ const endDate = moment.utc('2023-10-05');
 
 const filteredArray = filterByDate(startDate, endDate);
 console.log(filteredArray);
+
+
+export interface IRequestResponseMap {
+    indexOfRequest: number;
+    errorInfo: IErrorInfo;
+    lob: string;
+    request: any;
+    response?: any;
+}
+export interface IErrorInfo {
+    errorMsg: string;
+    details?: any;
+}
+
+export const SUCCESS_STATUS_CODES = '0000';
+
+export function handleResponseRetail(requestResponseMap: IRequestResponseMap[], parentSpan: any): IOrdersEntity {
+
+
+    const orderEntity = {} as IOrdersEntity;
+
+    for (const requestResponseEntity of requestResponseMap) {
+
+        if (requestResponseEntity.lob === "RETAIL") {
+
+            if (!SUCCESS_STATUS_CODES.includes(requestResponseEntity.response.statusCode)) {
+                requestResponseEntity.errorInfo = requestResponseEntity.response;
+            } else {
+                const uniqueRxObj: IRxNumber =  {
+                    refill: ["test"]
+                }
+                orderEntity.lineOfBusiness = "RETAIL";
+                orderEntity.patientFirstName = requestResponseEntity.response.patientFirstName;
+                orderEntity.pickupDateAndTime = requestResponseEntity.request.pickupDateAndTime;
+                orderEntity.storeNumber = requestResponseEntity.request.storePickupDetails.pickUpStoreId.toString();
+                orderEntity.uniqueRxId = uniqueRxObj
+            }
+        }
+    }
+
+export interface IRxNumber {
+    refill?: string[];
+    renewExpired?: string[];
+    renewZeroRefill?: string[];
+}
+
+export interface IOrdersEntity {
+    deliveryAddress?: IAddress;
+    lineOfBusiness: string;
+    planId?: string;
+    uniqueRxId?: IRxNumber[] | string[] | string;
+    patientFirstName?: string;
+    pickupDateAndTime?: string;
+    storeNumber?: string;
+    indexId?: string;
+    orderConfirmationNumber?: string;
+    purchaseId?: string;
+}
+
+export interface IAddress {
+    addressLine1: string;
+    addressLine2: string;
+    addressTypeCode?: string;
+    city: string;
+    state: string;
+    zipCode: string;
+}
+
+  
