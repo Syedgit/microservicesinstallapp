a7263bfba9278a6eeb5d590804b953a371adb47d
Syed Hussain <aftab.jersey@gmail.com>
2023-12-27 17:22:15 -0500

    Update Javacript.js

--- Javacript.js
+++ Javacript.js
@@ -3,3 +3,22 @@
         }, passedParam).then(function (res) {
             return res;
         });
+
+
+const crypto = require('crypto');
+const key = 'EiE0BVQle0xFjZvYOupKjXCWAcAwBaTjlZ7G7rryNos=';
+
+
+function encrypt(text) {
+let iv = Buffer.from(crypto.randomBytes(16), 'base64'); 
+let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'base64'), iv);
+let encrypted = cipher.update(text);
+encrypted = Buffer.concat([encrypted, cipher.final()]);
+var encryptedWithIVPrepended = Buffer.concat([iv, encrypted]);
+
+const encryptedData = { 	iv						: iv.toString('base64'), 
+      encryptedWithIVPrepended: encryptedWithIVPrepended.toString('base64') 
+    };
+
+  return encodeURIComponent(encryptedData.encryptedWithIVPrepended);
+}
