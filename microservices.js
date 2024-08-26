349d4d6f288f6be1c1f98e36574fd80232ce5bcd
Syed Hussain <aftab.jersey@gmail.com>
2024-08-26 12:06:50 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,23 +1,55 @@
-import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
-import { MemberAuthenticationService } from './member-authentication.service';
-import { SsrAuthFacade } from '../../../../../../../pharmacy/shared/store/ssr-auth/src';
-import { ConfigFacade } from '@digital-blocks/angular/core/store/config';
-import { HttpService } from '@digital-blocks/angular/core/util/services';
-import { PLATFORM_ID } from '@angular/core';
-import { GetMemberInfoAndTokenRequest, GetMemberInfoAndTokenResponse, MemberInfo, OauthResponse } from '../+state/member-authentication.interfaces';
+reducer specs error 
 
-describe('MemberAuthenticationService', () => {
-  let service: MemberAuthenticationService;
-  const mockHttpService = { post: jest.fn() };
-  const mockSsrAuthFacade = {
-    getSsrAuth: jest.fn(),
-    ssrAuth$: of({ access_token: 'mockAccessToken' } as OauthResponse)
-  };
-  const mockConfigFacade = {
-    config$: of({ environment: { basePath: 'https://sit2wwww.caremark.com' } })
-  };
+ FAIL   angular-pharmacy-transfer-prescriptions-store-member-authentication  libs/angular/pharmacy/transfer-prescriptions/store/member-authentication/src/lib/+state/member-authentication.reducer.spec.ts (5.119 s)
+  ● MemberAuthentication Reducer › should set token and tokenType on getMemberInfoAndTokenSuccess action
 
+    expect(received).toEqual(expected) // deep equality
+
+    - Expected  - 6
+    + Received  + 0
+
+    @@ -1,15 +1,9 @@
+      Object {
+        "error": undefined,
+        "loading": false,
+        "memberTokenResponse": Object {
+    -     "access_token": "",
+    -     "statusCode": "",
+    -     "statusDescription": "",
+    -     "token_type": "",
+    -   },
+    -   "mockResponse": Object {
+          "access_token": "test_token",
+          "statusCode": "0000",
+          "statusDescription": "Success",
+          "token_type": "Bearer",
+        },
+
+      53 |     const state = reducer(initialState, action);
+      54 |
+    > 55 |     expect(state).toEqual({
+         |                   ^
+      56 |       ...initialState,
+      57 |       mockResponse
+      58 |     });
+
+      at src/lib/+state/member-authentication.reducer.spec.ts:55:19
+      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:416:32)
+      at ProxyZoneSpec.Object.<anonymous>.ProxyZoneSpec.onInvoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone-testing.umd.js:2164:43)
+      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:415:38)
+      at ZoneImpl.run (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:147:47)
+      at Object.wrappedFunc (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone-testing.umd.js:450:38)
+
+
+      reducer specs file 
+
+      import { mockReportableError } from '@digital-blocks/angular/pharmacy/view-all-rx/util';
+
+import { MemberAuthenticationActions } from './member-authentication.actions';
+import { MemberInfo } from './member-authentication.interfaces';
+import { reducer, initialState } from './member-authentication.reducer';
+
+describe('MemberAuthentication Reducer', () => {
   const patientInfo: MemberInfo = {
     memberId: '12345',
     firstName: 'John',
@@ -27,48 +59,121 @@ describe('MemberAuthenticationService', () => {
     source: 'CMK'
   };
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        MemberAuthenticationService,
-        { provide: HttpService, useValue: mockHttpService },
-        { provide: SsrAuthFacade, useValue: mockSsrAuthFacade },
-        { provide: ConfigFacade, useValue: mockConfigFacade },
-        { provide: PLATFORM_ID, useValue: 'browser' }
-      ]
+  it('should return the initial state', () => {
+    const action = { type: 'Unknown' } as any;
+    const state = reducer(undefined, action);
+
+    expect(state).toEqual(initialState);
+  });
+
+  it('should set loading to true on getMemberInfoAndToken action', () => {
+    const action = MemberAuthenticationActions.getMemberInfoAndToken({
+      request: {
+        data: {
+          idType: 'PBM_QL_ENC_PARTICIPANT_ID_TYPE',
+          lookupReq: patientInfo
+        }
+      },
+      useTransferSecret: true
     });
+    const state = reducer(initialState, action);
 
-    service = TestBed.inject(MemberAuthenticationService);
+    expect(state).toEqual({
+      ...initialState,
+      loading: true,
+      error: undefined
+    });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
-  it('should be created with isPlatformServer', () => {
-    service = TestBed.inject(MemberAuthenticationService);
-    expect(service).toBeTruthy();
-  });
-
-  it('should execute getMemberInfoAndToken successfully', (done) => {
-    const mockRequest: GetMemberInfoAndTokenRequest = {
-      data: {
-        idType: '',
-        lookupReq: patientInfo
-      }
-    };
-    const mockResponse: GetMemberInfoAndTokenResponse = {
-      statusCode: '200',
-      statusDescription: 'Success',
+  it('should set token and tokenType on getMemberInfoAndTokenSuccess action', () => {
+    const mockResponse = {
+      access_token: 'test_token',
       token_type: 'Bearer',
-      access_token: 'test_token'
+      statusCode: '0000',
+      statusDescription: 'Success'
     };
+    const action = MemberAuthenticationActions.getMemberInfoAndTokenSuccess({
+      memberTokenResponse: mockResponse
+    });
+    const state = reducer(initialState, action);
 
-    mockHttpService.post.mockReturnValue(of({ body: mockResponse }));
-
-    service.getMemberInfoAndToken(mockRequest).subscribe(response => {
-      expect(response).toEqual(mockResponse);
-      done();
+    expect(state).toEqual({
+      ...initialState,
+      mockResponse
     });
   });
+
+  it('error state getMemberInfoAndTokenFailure action"', () => {
+    const action = MemberAuthenticationActions.getMemberInfoAndTokenFailure({
+      error: mockReportableError
+    });
+    const expectedState = {
+      ...initialState,
+      error: mockReportableError,
+      loading: false
+    };
+
+    const result = reducer(initialState, action);
+
+    expect(result).toEqual(expectedState);
+  });
+});
+
+
+      reducer.ts 
+
+      import { ReportableError } from '@digital-blocks/core/util/error-handler';
+import { ActionReducer, createFeature, createReducer, on } from '@ngrx/store';
+
+import { MemberAuthenticationActions } from './member-authentication.actions';
+import { GetMemberInfoAndTokenResponse, MemberInfo } from './member-authentication.interfaces';
+
+export interface MemberAuthenticationState {
+  memberTokenResponse: GetMemberInfoAndTokenResponse;
+  loading: boolean;
+  error: ReportableError | undefined;
+}
+
+export const initialState: MemberAuthenticationState = {
+  memberTokenResponse: {
+    statusCode: '',
+    statusDescription: '',
+    token_type: '',
+    access_token: ''
+  },
+  loading: false,
+  error: undefined
+};
+
+export const MEMBER_AUTHENTICATION_FEATURE_KEY = 'member-authentication';
+
+export const reducer: ActionReducer<MemberAuthenticationState> = createReducer(
+  initialState,
+  on(MemberAuthenticationActions.getMemberInfoAndToken, (state) => ({
+    ...state,
+    loading: true,
+    error: undefined
+  })),
+  on(
+    MemberAuthenticationActions.getMemberInfoAndTokenSuccess,
+    (state, { memberTokenResponse }) => ({
+      ...state,
+      memberTokenResponse,
+      loading: false,
+      error: undefined
+    })
+  ),
+  on(
+    MemberAuthenticationActions.getMemberInfoAndTokenFailure,
+    (state, { error }) => ({
+      ...state,
+      loading: false,
+      error
+    })
+  )
+);
+
+export const MemberAuthenticationFeature = createFeature({
+  name: MEMBER_AUTHENTICATION_FEATURE_KEY,
+  reducer
 });
