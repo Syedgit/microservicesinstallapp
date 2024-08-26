051b9d129e63bfd31f72ba9dbd4e169ee86da6ed
Syed Hussain <aftab.jersey@gmail.com>
2024-08-26 13:26:51 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,80 +1,42 @@
-import { Injectable, inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformServer } from '@angular/common';
-import {
-  HttpService,
-  mapResponseBody
-} from '@digital-blocks/angular/core/util/services';
-import { SsrAuthFacade } from '@digital-blocks/angular/pharmacy/shared/store/ssr-auth';
-import { filter, map, Observable, switchMap } from 'rxjs';
-import { ConfigFacade } from '@digital-blocks/angular/core/store/config';
+import { inject, Injectable } from '@angular/core';
+import { errorMessage } from '@digital-blocks/core/util/error-handler';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { catchError, map, of, switchMap } from 'rxjs';
 
-import {
-  GetMemberInfoAndTokenRequest,
-  GetMemberInfoAndTokenResponse,
-  OauthResponse
-} from '../+state/member-authentication.interfaces';
+import { MemberAuthenticationService } from '../services/member-authentication.service';
 
-import { b2bConfig } from './member-authentication.config';
-import { HttpHeaders } from '@angular/common/http';
+import { MemberAuthenticationActions } from './member-authentication.actions';
+import { MemberAuthenticationState } from './member-authentication.reducer';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class MemberAuthenticationService {
-  private readonly ssrAuthFacade = inject(SsrAuthFacade);
-  private readonly configFacade = inject(ConfigFacade);
-  private readonly httpService = inject(HttpService);
-  private readonly platformId = inject(PLATFORM_ID);
+@Injectable()
+export class MemberAuthenticationEffects {
+  private readonly actions$ = inject(Actions);
+  private readonly memberAuthService = inject(MemberAuthenticationService);
+  private readonly store = inject(Store<MemberAuthenticationState>);
+  private readonly errorTag = 'MemberAuthenticationEffects';
 
-  getMemberInfoAndToken(
-    request: GetMemberInfoAndTokenRequest,
-    useTransferSecret = true
-  ): Observable<GetMemberInfoAndTokenResponse> {
-    this.ssrAuthFacade.getSsrAuth(useTransferSecret);
-
-    return this.ssrAuthFacade.ssrAuth$.pipe(
-      filter(
-        (ssrAuth): ssrAuth is OauthResponse =>
-          !!ssrAuth && !!ssrAuth.access_token
-      ),
-      switchMap((ssrAuth) => {
-        return this.configFacade.config$.pipe(
-          filter((config) => !isPlatformServer(this.platformId) && !!config),
-          switchMap((config) => {
-            const requestData = {
-              data: {
-                idType: 'PBM_QL_ENC_PARTICIPANT_ID_TYPE',
-                lookupReq: request.data.lookupReq
-              }
-            };
-
-            const headers = new HttpHeaders({
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${ssrAuth.access_token}`,
-              'x-experienceId': b2bConfig.expId,
-              'x-api-key': b2bConfig['x-api-key']
-            });
-
-            return this.httpService
-              .post<GetMemberInfoAndTokenResponse>(
-                `${config.environment.basePath}${b2bConfig.b2bUrl}`,
-                 b2bConfig.MOCK,
-                { 
-                  headers, 
-                  withCredentials: true 
-                },
-                requestData
-              )
-              .pipe(
-                mapResponseBody(),
-                map((response: GetMemberInfoAndTokenResponse) => {
-                  return response
-                } 
-                )
+  public getMemberInfoAndToken$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(MemberAuthenticationActions.getMemberInfoAndToken),
+      switchMap(({ request, useTransferSecret }) => {
+        return this.memberAuthService
+          .getMemberInfoAndToken(request, useTransferSecret)
+          .pipe(
+            map((memberTokenResponse) => {
+              return MemberAuthenticationActions.getMemberInfoAndTokenSuccess({
+                memberTokenResponse
+              });
+            }),
+            catchError((error: unknown) => {
+              return of(
+                MemberAuthenticationActions.getMemberInfoAndTokenFailure({
+                  error: errorMessage(this.errorTag, error)
+                })
               );
-          })
-        );
+            })
+          );
       })
     );
-  }
+  });
 }
