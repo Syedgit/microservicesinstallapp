5894787a8f8e5945da7e14c4305283ac2d10bd32
Syed Hussain <aftab.jersey@gmail.com>
2024-08-27 13:02:06 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,42 +1,55 @@
-import { inject, Injectable } from '@angular/core';
-import { errorMessage } from '@digital-blocks/core/util/error-handler';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
-import { catchError, map, of, switchMap } from 'rxjs';
+service
 
-import { MemberAuthenticationService } from '../services/member-authentication.service';
+if (searchTerm?.length >= 3) {
+      this.store.autoSugguestSearch(searchTerm);
+      this.store.resourceSet$.pipe().subscribe({
+        next: (resp: ResourceSet[]) => {
+          const locationList: Value[] = resp?.[0]?.resources?.[0]?.value;
+          if (locationList?.length === 0) {
+            this.locationSuggestions = [];
+          } else {
+            this.locationSuggestions = [];
+            this.constructLocationSuggestionsList([...(locationList || [])]);
+          }
+        }
+      });
 
-import { MemberAuthenticationActions } from './member-authentication.actions';
-import { MemberAuthenticationState } from './member-authentication.reducer';
+interface
 
-@Injectable()
-export class MemberAuthenticationEffects {
-  private readonly actions$ = inject(Actions);
-  private readonly memberAuthService = inject(MemberAuthenticationService);
-  private readonly store = inject(Store<MemberAuthenticationState>);
-  private readonly errorTag = 'MemberAuthenticationEffects';
-
-  public getMemberInfoAndToken$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(MemberAuthenticationActions.getMemberInfoAndToken),
-      switchMap(({ request, useTransferSecret }) => {
-        return this.memberAuthService
-          .getMemberInfoAndToken(request, useTransferSecret)
-          .pipe(
-            map((memberTokenResponse) => {
-              return MemberAuthenticationActions.getMemberInfoAndTokenSuccess({
-                memberTokenResponse
-              });
-            }),
-            catchError((error: unknown) => {
-              return of(
-                MemberAuthenticationActions.getMemberInfoAndTokenFailure({
-                  error: errorMessage(this.errorTag, error)
-                })
-              );
-            })
-          );
-      })
-    );
-  });
+export interface AutoSuggestResponse {
+  authenticationResultCode?: string;
+  brandLogoUri?: string;
+  copyright?: string;
+  resourceSets: ResourceSet[];
+  statusCode: number;
+  statusDescription: string;
+  traceId?: string;
+}
+ 
+export interface ResourceSet {
+  estimatedTotal: number;
+  resources: Resource[];
+}
+ 
+interface Resource {
+  __type?: string;
+  value?: Value[];
+}
+ 
+export interface Value {
+  __type: string;
+  address?: Address;
+}
+ 
+export interface Address {
+  countryRegion?: string;
+  locality?: string;
+  adminDistrict?: string;
+  adminDistrict2?: string;
+  countryRegionIso2?: string;
+  postalCode?: string;
+  formattedAddress: string;
+  houseNumber?: string;
+  addressLine?: string;
+  streetName?: string;
 }
