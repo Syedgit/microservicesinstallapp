124ae86c3e7b7ee196964d4da9c8b15b87d37f8b
Syed Hussain <aftab.jersey@gmail.com>
2024-08-29 14:23:23 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,153 +1,82 @@
-html
-
-<ng-container *ngIf="loading$ | async; else memberAuthTemplate">
-  <ps-tile>
-    <util-spinning-loader [loading]="loading$ | async"></util-spinning-loader>
-  </ps-tile>
-</ng-container>
-<ng-template #memberAuthTemplate>
-  <div class="member-container">
-    <ng-container>
-      <form [formGroup]="memberForm">
-        <ps-helper helper-id="helper-245">
-          Hyvee will not be in your network staring November 1, 2024, we will
-          help you transfer your current prescription to new pharmacy
-        </ps-helper>
-        <ps-input
-          formControlName="firstName"
-          ngDefaultControl
-          name="firstName"
-          input-required="true"
-          label="First Name">
-        </ps-input>
-        <ps-input
-          formControlName="lastName"
-          ngDefaultControl
-          name="lastName"
-          input-required="true"
-          label="Last Name">
-        </ps-input>
-        <ps-date-fieldset
-        legend="Date of Birth"
-      >
-        <input slot="month" />
-        <input slot="day" />
-        <input slot="year" />
-      </ps-date-fieldset>
-        <ps-input
-          formControlName="memberId"
-          ngDefaultControl
-          name="memberId"
-          input-required="true"
-          label="Member ID">
-        </ps-input>
-        <ps-helper helper-id="helper-123"> CMS content </ps-helper>
-        <ps-checkbox label="I Agree"></ps-checkbox>
-        <div>
-          <ps-button
-            is-full-width="true"
-            size="md"
-            submit="true"
-            variant="solid"
-            [isFullWidth]="layoutFacade.breakpointSmall$ | async"
-            (keydown)="handleKeyDown($event)"
-            (click)="getMemberInfoAndToken()"
-            class="continue-button">
-            Continue
-          </ps-button>
-        </div>
-      </form>
-    </ng-container>
-  </div>
-</ng-template>
-
-
-ts
-
-import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable, inject, PLATFORM_ID } from '@angular/core';
+import { ConfigFacade } from '@digital-blocks/angular/core/store/config';
 import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators
-} from '@angular/forms';
-import { SpinningLoaderComponent } from '@digital-blocks/angular/core/components';
-import { LayoutFacade } from '@digital-blocks/angular/core/store/layout';
-import { NavigationService } from '@digital-blocks/angular/core/util/services';
+  HttpService,
+  mapResponseBody
+} from '@digital-blocks/angular/core/util/services';
+import { SsrAuthFacade } from '@digital-blocks/angular/pharmacy/shared/store/ssr-auth';
+import { filter, map, Observable, switchMap } from 'rxjs';
+
 import {
-  MemberInfo,
-  MemberAuthenticationModule,
-  GetMemberInfoAndTokenResponse
-} from '@digital-blocks/angular/pharmacy/transfer-prescriptions/store/member-authentication';
+  GetMemberInfoAndTokenRequest,
+  GetMemberInfoAndTokenResponse,
+  OauthResponse
+} from '../+state/member-authentication.interfaces';
 
-import { MemberAuthenticationStore } from './member-authentication.store';
+import { b2bConfig } from './member-authentication.config';
 
-@Component({
-  selector: 'lib-member-authentication',
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MemberAuthenticationModule,
-    SpinningLoaderComponent
-  ], // Use FormsModule for template-driven forms
-  templateUrl: 'member-authentication.component.html',
-  styleUrls: ['member-authentication.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [MemberAuthenticationStore],
-  host: { ngSkipHydration: 'true' }
+@Injectable({
+  providedIn: 'root'
 })
-export class MemberAuthenticationComponent {
-  protected readonly navigationService = inject(NavigationService);
-  protected readonly layoutFacade = inject(LayoutFacade);
-  public readonly store = inject(MemberAuthenticationStore);
-  private fb = inject(FormBuilder);
-  public readonly memberTokenResponse$ = this.store.memberTokenResponse$;
-  public readonly loading$ = this.store.loading$;
-  memberForm: FormGroup;
+export class MemberAuthenticationService {
+  private readonly ssrAuthFacade = inject(SsrAuthFacade);
+  private readonly configFacade = inject(ConfigFacade);
+  private readonly httpService = inject(HttpService);
+  private readonly platformId = inject(PLATFORM_ID);
 
-  constructor() {
-    this.memberForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      dateOfBirth: ['', Validators.required],
-      memberId: ['', Validators.required]
-    });
-  }
+  getMemberInfoAndToken(
+    request: GetMemberInfoAndTokenRequest,
+    useTransferSecret = true
+  ): Observable<GetMemberInfoAndTokenResponse> {
+    this.ssrAuthFacade.getSsrAuth(useTransferSecret);
 
-  handleKeyDown(event: KeyboardEvent): void {
-    if (event.key === 'Enter' || event.key === ' ') {
-      this.getMemberInfoAndToken();
-      event.preventDefault();
-    }
-  }
-
-  public getMemberInfoAndToken(): void {
-    if (this.memberForm.valid) {
-      const patientInfo: MemberInfo = {
-        ...this.memberForm.value,
-        flowName: 'MEMBER_ID_LOOKUP',
-        source: 'CMK'
-      };
-
-      this.store.getMemberInfoAndToken(patientInfo);
-      this.memberTokenResponse$.subscribe(
-        (data: GetMemberInfoAndTokenResponse) => {
-          if (data.statusCode === '0000' && data.access_token) {
-            sessionStorage.setItem('mTkn', data.access_token);
-            this.navigationService.navigate(
-              '/pharmacy/-/transfer/current-prescriptions',
-              { queryParamsHandling: 'preserve' },
-              {
-                navigateByPath: true
+    return this.ssrAuthFacade.ssrAuth$.pipe(
+      filter(
+        (ssrAuth): ssrAuth is OauthResponse =>
+          !!ssrAuth && !!ssrAuth.access_token
+      ),
+      switchMap((ssrAuth) => {
+        return this.configFacade.config$.pipe(
+          filter((config) => !isPlatformServer(this.platformId) && !!config),
+          switchMap((config) => {
+            const requestData = {
+              data: {
+                idType: 'PBM_QL_ENC_PARTICIPANT_ID_TYPE',
+                lookupReq: request.data.lookupReq
               }
-            );
-          }
-        }
-      );
-    }
+            };
+
+            const headers = new HttpHeaders({
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${ssrAuth.access_token}`,
+              'x-experienceId': b2bConfig.expId,
+              'x-api-key': b2bConfig['x-api-key']
+            });
+
+            return this.httpService
+              .post<GetMemberInfoAndTokenResponse>(
+                `${config.environment.basePath}${b2bConfig.b2bUrl}`,
+                b2bConfig.MOCK,
+                {
+                  headers,
+                  withCredentials: true
+                },
+                requestData,
+                {
+                  maxRequestTime: 10_000
+                }
+              )
+              .pipe(
+                mapResponseBody(),
+                map((response: GetMemberInfoAndTokenResponse) => {
+                  return response;
+                })
+              );
+          })
+        );
+      })
+    );
   }
 }
