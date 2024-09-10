0cdb5fdb34ca0b05158ca075263ce58a53fc838d
Syed Hussain <aftab.jersey@gmail.com>
2024-09-10 16:10:10 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,132 +1,3 @@
-<ng-container *ngIf="loading$ | async; else memberAuthTemplate">
-  <ps-tile>
-    <util-spinning-loader [loading]="loading$ | async"></util-spinning-loader>
-  </ps-tile>
-</ng-container>
-<ng-template #memberAuthTemplate>
-  <div class="member-container">
-    <ng-container>
-      <form [formGroup]="memberForm">
-        <ps-helper>
-          <h3>Transfer your HyVee prescriptions to an in-network pharmacy</h3>
-        </ps-helper>
-        <ps-input
-          formControlName="firstName"
-          ngDefaultControl
-          name="firstName"
-          input-required="true"
-          label="First Name"
-          is-error-announced="true"
-          [error]="
-          hasErrors &&
-          !memberForm.controls['firstName'].valid
-            ? formErrors['firstName']
-              ? formErrors['firstName']
-              : ''
-            : undefined
-        ">
-        </ps-input>
-        <ps-input
-          formControlName="lastName"
-          ngDefaultControl
-          name="lastName"
-          input-required="true"
-          label="Last Name"
-          is-error-announced="true"
-          [error]="
-          hasErrors &&
-          !memberForm.controls['lastName'].valid
-            ? formErrors['lastName']
-              ? formErrors['lastName']
-              : ''
-            : undefined
-        ">
-        </ps-input>
-        <ps-date-fieldset legend="Date of Birth" is-dob>
-          <!-- eslint-disable-next-line -- custom inputs are needed -->
-          <input slot="month" #month />
-          <!-- eslint-disable-next-line -- custom inputs are needed -->
-          <input slot="day" #day />
-          <!-- eslint-disable-next-line -- custom inputs are needed -->
-          <input slot="year" #year />
-        </ps-date-fieldset>
-        @if (formErrors['dateOfBirth']) {
-          <ps-error>{{formErrors['dateOfBirth']}}</ps-error>
-        }
-        <ps-input
-          formControlName="memberId"
-          ngDefaultControl
-          name="memberId"
-          input-required="true"
-          label="Member ID"
-          is-error-announced="true"
-          [error]="
-          hasErrors &&
-          !memberForm.controls['memberId'].valid
-            ? formErrors['memberId']
-              ? formErrors['memberId']
-              : ''
-            : undefined
-        ">
-        </ps-input>
-        <div>
-          <ps-button
-            is-full-width="true"
-            size="md"
-            submit="true"
-            variant="solid"
-            [isFullWidth]="layoutFacade.breakpointSmall$ | async"
-            (keydown)="handleKeyDown($event)"
-            (click)="getMemberInfoAndToken()"
-            class="continue-button">
-            Continue
-          </ps-button>
-        </div>
-      </form>
-    </ng-container>
-  </div>
-</ng-template>
-
-
-component 
-
-import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, inject, ViewChild } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators
-} from '@angular/forms';
-import { SpinningLoaderComponent } from '@digital-blocks/angular/core/components';
-import { LayoutFacade } from '@digital-blocks/angular/core/store/layout';
-import { NavigationService } from '@digital-blocks/angular/core/util/services';
-import {
-  MemberInfo,
-  MemberAuthenticationModule,
-  GetMemberInfoAndTokenResponse
-} from '@digital-blocks/angular/pharmacy/transfer-prescriptions/store/member-authentication';
-
-import { MemberAuthenticationStore } from './member-authentication.store';
-import { Constants } from './member-authentication.constant';
-
-@Component({
-  selector: 'lib-member-authentication',
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MemberAuthenticationModule,
-    SpinningLoaderComponent
-  ],
-  templateUrl: 'member-authentication.component.html',
-  styleUrls: ['member-authentication.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [MemberAuthenticationStore],
-  host: { ngSkipHydration: 'true' }
-})
 export class MemberAuthenticationComponent  {
   @ViewChild('month') month!: ElementRef;
   @ViewChild('day') day!: ElementRef;
@@ -151,14 +22,28 @@ export class MemberAuthenticationComponent  {
     this.memberForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
       lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
-      dateOfBirth: [''],
+      dateOfBirth: [''], // dateOfBirth will be manually handled
       memberId: ['', [Validators.required, Validators.pattern('^[0-9]+$')]]
     });
   }
 
-  handleKeyDown(event: KeyboardEvent): void {
-    if (event.key === 'Enter' || event.key === ' ') {
-      event.preventDefault();
+  ngOnInit(): void {
+    // Add listeners to the date fields to trigger validation when they change
+    this.addDateFieldListeners();
+  }
+
+  addDateFieldListeners(): void {
+    this.month.nativeElement.addEventListener('input', () => this.validateDateField());
+    this.day.nativeElement.addEventListener('input', () => this.validateDateField());
+    this.year.nativeElement.addEventListener('input', () => this.validateDateField());
+  }
+
+  validateDateField(): void {
+    const isValidDate = this.isDateValid();
+    if (isValidDate) {
+      this.formErrors['dateOfBirth'] = '';  // Clear error when date is valid
+    } else {
+      this.formErrors['dateOfBirth'] = Constants.DOB_ERROR;  // Show error when invalid
     }
   }
 
@@ -183,7 +68,7 @@ export class MemberAuthenticationComponent  {
         }
       );
     } else {
-      this.hasErrors = true
+      this.hasErrors = true;
     }
   }
 
@@ -197,29 +82,16 @@ export class MemberAuthenticationComponent  {
     return patientInfo;
   }
 
-  isDateValid() {
+  isDateValid(): boolean {
     const { month, day, year } = this;
     const m = parseInt(month.nativeElement.value, 10);
     const d = parseInt(day.nativeElement.value, 10);
     const y = parseInt(year.nativeElement.value, 10);
     const isValidMonth = m >= 1 && m <= 12;
-    const isValidDay = d >= 1 && d <= 31; 
+    const isValidDay = d >= 1 && d <= 31;
     const isValidYear = y >= 1900 && y <= new Date().getFullYear();
 
     return isValidMonth && isValidDay && isValidYear;
-  }
-
-  updateFormWithDate()  {
-    const m = this.month.nativeElement.value;
-    const d = this.day.nativeElement.value;
-    const y = this.year.nativeElement.value;
-    const dateOfBirth = `${y}-${m}-${d}`;
-
-    this.memberForm.patchValue({
-      dateOfBirth: dateOfBirth
-    });
-   
-    return dateOfBirth
   }
 
   validateFormFields(): void {
@@ -255,12 +127,6 @@ export class MemberAuthenticationComponent  {
     }
 
     // Validate dateOfBirth (manually)
-    if (!this.isDateValid()) {
-      this.formErrors['dateOfBirth'] = Constants.DOB_ERROR;
-    } else {
-      this.formErrors['dateOfBirth'] = '';
-    }
+    this.validateDateField();  // Call this whenever validating all fields
   }
-  
 }
-
