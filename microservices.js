10fea34ce19bf52304d65bf397f5add158606274
Syed Hussain <aftab.jersey@gmail.com>
2024-09-11 21:10:20 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,53 +1,30 @@
+  public getMemberInfoAndToken(): void {
+    const isDateValid = this.isDateValid();
 
-  it('should call updateFormWithDate, patch dateOfBirth, validate the form, and dispatch getMemberInfoAndToken action with correct payload', () => {
-    const updateFormWithDateSpy = jest.spyOn(component, 'updateFormWithDate').mockReturnValue('1990-12-25');
-    const patientInfo  = {
-      firstName: 'John',
-      lastName: 'Doe',
-      memberId: '12345',
-      dateOfBirth: '1990-12-25',
-      flowName: 'MEMBER_ID_LOOKUP',
-      source: 'CMK'
-    };
-    // const patchValueSpy = jest.spyOn(component.memberForm, 'patchValue');
-    component.memberForm.setValue({
-      firstName: ['John'],
-      lastName: ['Doe'],
-      memberId: ['12345'],
-      dateOfBirth: ['']
-    });
-    
-    const storeSpy = jest.spyOn(component.store, 'getMemberInfoAndToken');
-    component.getMemberInfoAndToken();
-  
-    expect(updateFormWithDateSpy).toHaveBeenCalled();
-    expect(storeSpy).toHaveBeenCalledWith(patientInfo);
-    expect(component.memberForm.value.dateOfBirth).toEqual({ dateOfBirth: '1990-12-25' });
-    
-    
-  });
-
-Error
-
-  ● MemberAuthenticationComponent › should call updateFormWithDate, patch dateOfBirth, validate the form, and dispatch getMemberInfoAndToken action with correct payload
-
-    expect(jest.fn()).toHaveBeenCalledWith(...expected)
-
-    Expected: {"dateOfBirth": "1990-12-25", "firstName": "John", "flowName": "MEMBER_ID_LOOKUP", "lastName": "Doe", "memberId": "12345", "source": "CMK"}
-
-    Number of calls: 0
-
-      108 |   
-      109 |     expect(updateFormWithDateSpy).toHaveBeenCalled();
-    > 110 |     expect(storeSpy).toHaveBeenCalledWith(patientInfo);
-          |                      ^
-      111 |     expect(component.memberForm.value.dateOfBirth).toEqual({ dateOfBirth: '1990-12-25' });
-      112 |     
-      113 |     
-
-      at src/lib/member-authentication.component.spec.ts:110:22
-      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:416:32)
-      at ProxyZoneSpec.Object.<anonymous>.ProxyZoneSpec.onInvoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone-testing.umd.js:2164:43)
-      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:415:38)
-      at ZoneImpl.run (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:147:47)
-      at Object.wrappedFunc (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone-testing.umd.js:450:38)
+    this.memberForm.markAllAsTouched();
+    this.validateFormFields();
+    this.updateFormWithDate();
+    if (this.memberForm.valid && isDateValid) {
+      const patientInfo: MemberInfo = {
+        ...this.memberForm.value,
+        flowName: 'MEMBER_ID_LOOKUP',
+        source: 'CMK'
+      };
+      this.store.getMemberInfoAndToken(patientInfo);
+      this.memberTokenResponse$.subscribe(
+        (data: GetMemberInfoAndTokenResponse) => {
+          if (data.statusCode === '0000' && data.access_token) {
+            this.navigationService.navigate(
+              '/pharmacy/-/transfer/current-prescriptions',
+              { queryParamsHandling: 'preserve' },
+              {
+                navigateByPath: true
+              }
+            );
+          }
+        }
+      );
+    } else {
+      this.hasErrors = true;
+    }
+  }
