62ac7ac2bdac2a0a65b892fc52fc37bdc496c457
Syed Hussain <aftab.jersey@gmail.com>
2024-09-30 14:53:20 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,209 +1,46 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore, MockStore } from '@ngrx/store/testing';
-import { of } from 'rxjs';
-import { SubmitTransferStore } from './submit-transfer.store';
-import { SubmitTransferComponent } from './submit-transfer.component';
-import { SubmitTransferResponse, TransferOrderRequest } from '@digital-blocks/angular/pharmacy/transfer-prescriptions/store/prescriptions-list';
+ponent.spec.ts
+  ● SubmitTransferComponent › submitTransfer › should handle a successful transfer submission
 
-describe('SubmitTransferComponent', () => {
-  let component: SubmitTransferComponent;
-  let fixture: ComponentFixture<SubmitTransferComponent>;
-  let store: SubmitTransferStore;
-  let mockStore: MockStore;
-  let currentPrescriptions: any[];
+    expect(jest.fn()).toHaveBeenCalledWith(...expected)
 
-  const initialState = {
-    config: {
-      loading: false,
-      currentPrescriptions: [],
-      submitTransferResponse: null,
-    }
-  };
+    Expected: {"data": {"externalTransfer": [{"carrierId": "", "clinicalRuleDate": "09/16/2024", "patient": {"address": {"city": "", "line": [""], "phoneNumber": "", "postalCode": "", "state": ""}, "dateOfBirth": "01/01/1990", "email": "john.doe@example.com", "firstName": "John", "gender": undefined, "lastName": "Doe", "memberId": "7389902", "patientId": "7389902", "patientIdType": "PBM_QL_PARTICIPANT_ID_TYPE", "profileId": null}, "requestedChannel": "", "rxDetails": [{"drugDetails": [{"daySupply": undefined, "drugName": "Drug 1", "encPrescriptionLookupKey": "lookupKey1", "prescriptionLookupKey": {"id": "133225401", "idType": "PBM_QL_PARTICIPANT_ID_TYPE", "rxNumber": "lookupKey1"}, "provider": {"address": {"city": "Town", "line": [Array], "phoneNumber": "123-456-7890", "postalCode": "90210", "state": "CA"}, "faxNumber": "", "firstName": "John", "lastName": "Doe", "npi": "1234567890", "phoneNumber": ""}, "quantity": undefined, "recentFillDate": undefined}], "fromPharmacy": {"address": {"city": "WOONSOCKET", "line": ["GREY 1 CVS DRIVE"], "phoneNumber": "8005414959", "postalCode": "02895", "state": "RI"}, "pharmacyName": "ALLIANCERX WALGREENS PRIME 16280", "storeId": "99999"}, "toPharmacy": {"address": {"city": "WOONSOCKET", "line": ["GREY 1 CVS DRIVE"], "phoneNumber": "8005414959", "postalCode": "02895", "state": "RI"}, "pharmacyName": "ALLIANCERX WALGREENS PRIME 16280", "storeId": "99999"}}]}], "idType": "PBM_QL_PARTICIPANT_ID_TYPE", "profile": null}}
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [
-        SubmitTransferStore,
-        provideMockStore({ initialState })
-      ],
-      declarations: [SubmitTransferComponent]
-    }).compileComponents();
+    Number of calls: 0
 
-    fixture = TestBed.createComponent(SubmitTransferComponent);
-    component = fixture.componentInstance;
-    store = TestBed.inject(SubmitTransferStore);
-    mockStore = TestBed.inject(MockStore);
+      110 |       component.submitTransfer();
+      111 |
+    > 112 |       expect(spySubmitTransfer).toHaveBeenCalledWith(expectedRequest);
+          |                                 ^
+      113 |       expect(component.errorMessage).toBeNull();
+      114 |     });
+      115 |
 
-    currentPrescriptions = [
-      {
-        id: 7389902,
-        firstName: 'John',
-        lastName: 'Doe',
-        dateOfBirth: '01/01/1990',
-        emailAddresses: [
-          {
-            value: 'john.doe@example.com'
-          }
-        ],
-        prescriptionforPatient: [
-          {
-            isSelected: true,
-            id: '133225401',
-            drugInfo: {
-              drug: {
-                name: 'Drug 1'
-              }
-            },
-            prescriptionLookupKey: 'lookupKey1',
-            prescriber: {
-              firstName: 'John',
-              lastName: 'Doe',
-              npi: '1234567890',
-              address: {
-                line: ['123 Main St'],
-                city: 'Town',
-                state: 'CA',
-                postalCode: '90210',
-                phoneNumber: '123-456-7890'
-              }
-            },
-            storeDetails: {
-              pharmacyName: 'Pharmacy 1',
-              address: {
-                line: ['456 Other St'],
-                city: 'City',
-                state: 'CA',
-                postalCode: '90210',
-                phoneNumber: '987-654-3210'
-              }
-            }
-          }
-        ]
-      }
-    ];
+      at src/lib/submit-transfer.component.spec.ts:112:33
+      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:416:32)
+      at ProxyZoneSpec.Object.<anonymous>.ProxyZoneSpec.onInvoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone-testing.umd.js:2164:43)
+      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:415:38)
+      at ZoneImpl.run (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:147:47)
+      at Object.wrappedFunc (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone-testing.umd.js:450:38)
 
-    fixture.detectChanges();
-  });
+  ● SubmitTransferComponent › submitTransfer › should handle a failed transfer submission
 
-  it('should create the component', () => {
-    expect(component).toBeTruthy();
-  });
+    expect(jest.fn()).toHaveBeenCalledWith(...expected)
 
-  describe('submitTransfer', () => {
-    const mockedResponse: SubmitTransferResponse = {
-      statusCode: "0000",
-      statusDescription: "Success",
-      data: [
-        {
-          statusCode: "0000",
-          statusDescription: "Success",
-          confirmationNumber: "WE202409251821481QRP"
-        }
-      ]
-    };
+    Expected: {"data": {"externalTransfer": [{"carrierId": "", "clinicalRuleDate": "09/16/2024", "patient": {"address": {"city": "", "line": [""], "phoneNumber": "", "postalCode": "", "state": ""}, "dateOfBirth": "01/01/1990", "email": "john.doe@example.com", "firstName": "John", "gender": undefined, "lastName": "Doe", "memberId": "7389902", "patientId": "7389902", "patientIdType": "PBM_QL_PARTICIPANT_ID_TYPE", "profileId": null}, "requestedChannel": "", "rxDetails": [{"drugDetails": [{"daySupply": undefined, "drugName": "Drug 1", "encPrescriptionLookupKey": "lookupKey1", "prescriptionLookupKey": {"id": "133225401", "idType": "PBM_QL_PARTICIPANT_ID_TYPE", "rxNumber": "lookupKey1"}, "provider": {"address": {"city": "Town", "line": [Array], "phoneNumber": "123-456-7890", "postalCode": "90210", "state": "CA"}, "faxNumber": "", "firstName": "John", "lastName": "Doe", "npi": "1234567890", "phoneNumber": ""}, "quantity": undefined, "recentFillDate": undefined}], "fromPharmacy": {"address": {"city": "WOONSOCKET", "line": ["GREY 1 CVS DRIVE"], "phoneNumber": "8005414959", "postalCode": "02895", "state": "RI"}, "pharmacyName": "ALLIANCERX WALGREENS PRIME 16280", "storeId": "99999"}, "toPharmacy": {"address": {"city": "WOONSOCKET", "line": ["GREY 1 CVS DRIVE"], "phoneNumber": "8005414959", "postalCode": "02895", "state": "RI"}, "pharmacyName": "ALLIANCERX WALGREENS PRIME 16280", "storeId": "99999"}}]}], "idType": "PBM_QL_PARTICIPANT_ID_TYPE", "profile": null}}
 
-    it('should handle a successful transfer submission', () => {
-      const expectedRequest: TransferOrderRequest = component.buildTransferOrderRequest(currentPrescriptions);
+    Number of calls: 0
 
-      // Mock currentPrescriptions$ observable
-      jest.spyOn(store, 'currentPrescriptions$', 'get').mockReturnValue(of(currentPrescriptions));
+      129 |       component.submitTransfer();
+      130 |
+    > 131 |       expect(spySubmitTransfer).toHaveBeenCalledWith(expectedRequest);
+          |                                 ^
+      132 |
+      133 |       mockStore.setState({ submitTransferResponse: errorResponse });
+      134 |       mockStore.refreshState();
 
-      // Spy on submitTransfer to ensure it gets called
-      const spySubmitTransfer = jest.spyOn(store, 'submitTransfer').mockImplementation(() => {});
-
-      component.submitTransfer();
-
-      expect(spySubmitTransfer).toHaveBeenCalledWith(expectedRequest);
-      expect(component.errorMessage).toBeNull();
-    });
-
-    it('should handle a failed transfer submission', () => {
-      const errorResponse = {
-        statusCode: "5000",
-        statusDescription: "Error occurred",
-        data: []
-      };
-
-      const spySubmitTransfer = jest.spyOn(store, 'submitTransfer').mockImplementation(() => {});
-
-      component.currentPrescriptions = currentPrescriptions;
-
-      const expectedRequest: TransferOrderRequest = component.buildTransferOrderRequest(currentPrescriptions);
-
-      component.submitTransfer();
-
-      expect(spySubmitTransfer).toHaveBeenCalledWith(expectedRequest);
-
-      mockStore.setState({ submitTransferResponse: errorResponse });
-      mockStore.refreshState();
-      fixture.detectChanges();
-
-      expect(component.errorMessage).toBe('Error occurred');
-    });
-
-    it('should warn if no prescriptions are selected', () => {
-      currentPrescriptions[0].prescriptionforPatient[0].isSelected = false;
-      component.currentPrescriptions = currentPrescriptions;
-      component.submitTransfer();
-
-      expect(component.errorMessage).toBe('No prescriptions selected for transfer.');
-    });
-  });
-
-  describe('buildTransferOrderRequest', () => {
-    it('should build the transfer order request correctly', () => {
-      component.currentPrescriptions = currentPrescriptions;
-
-      const request = component.buildTransferOrderRequest(currentPrescriptions);
-
-      expect(request.data.externalTransfer.length).toBe(1);
-      expect(request.data.externalTransfer[0].patient.firstName).toBe('John');
-      expect(request.data.externalTransfer[0].rxDetails[0].fromPharmacy.pharmacyName).toBe('Pharmacy 1');
-    });
-
-    it('should return empty externalTransfer array if no prescriptions are selected', () => {
-      currentPrescriptions[0].prescriptionforPatient[0].isSelected = false;
-      component.currentPrescriptions = currentPrescriptions;
-      const request = component.buildTransferOrderRequest(currentPrescriptions);
-      expect(request.data.externalTransfer.length).toBe(0);
-    });
-  });
-
-  describe('mapRxDetails', () => {
-    it('should map the prescription details correctly', () => {
-      const prescription = currentPrescriptions[0];
-      const result = component.mapRxDetails(prescription);
-
-      expect(result?.drugDetails.length).toBe(1);
-      expect(result?.fromPharmacy.pharmacyName).toBe('Pharmacy 1');
-    });
-
-    it('should return null if no selected prescriptions', () => {
-      currentPrescriptions[0].prescriptionforPatient[0].isSelected = false;
-      const prescription = currentPrescriptions[0];
-      const result = component.mapRxDetails(prescription);
-
-      expect(result).toBeNull();
-    });
-  });
-
-  describe('mapPatientDetails', () => {
-    it('should map the patient details correctly', () => {
-      const prescription = currentPrescriptions[0];
-      const result = component.mapPatientDetails(prescription);
-
-      expect(result.firstName).toBe('John');
-      expect(result.email).toBe('john.doe@example.com');
-    });
-
-    it('should handle missing email addresses', () => {
-      currentPrescriptions[0].emailAddresses = [];
-      const prescription = currentPrescriptions[0];
-      const result = component.mapPatientDetails(prescription);
-
-      expect(result.email).toBe('');
-    });
-  });
-});
+      at src/lib/submit-transfer.component.spec.ts:131:33
+      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:416:32)
+      at ProxyZoneSpec.Object.<anonymous>.ProxyZoneSpec.onInvoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone-testing.umd.js:2164:43)
+      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:415:38)
+      at ZoneImpl.run (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone.umd.js:147:47)
+      at Object.wrappedFunc (../../../../../../node_modules/.pnpm/zone.js@0.14.8/node_modules/zone.js/bundles/zone-testing.umd.js:450:38)
