b406662fd2d8946ae14799028e43acb25117024a
Syed Hussain <aftab.jersey@gmail.com>
2024-10-02 23:00:29 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,36 +1,9 @@
-it('should submit transfer and return a successful response', async () => {
-  const mockHttpResponse = new HttpResponse<SubmitTransferResponse>({
-    body: mockResponse,
-    status: 200
-  });
-
-  jest.spyOn(experienceService, 'post').mockReturnValue(of(mockHttpResponse));
-
-  const response = await firstValueFrom(service.submitTransfer(mockRequest));
-
-  expect(response).toEqual(mockResponse);
-
-  expect(experienceService.post).toHaveBeenCalledWith(
-    Config.clientId,
-    Config.experiences,
-    Config.mock,
-    { data: mockRequest.data },
-    { maxRequestTime: 10_000 }
-  );
-});
-
-
 it('should return an error when the transfer fails', async () => {
   const mockError = { message: 'Transfer failed' };
 
-  jest
-    .spyOn(experienceService, 'post')
-    .mockReturnValue(throwError(() => mockError));
+  jest.spyOn(experienceService, 'post').mockReturnValue(throwError(() => mockError));
 
-  try {
-    await firstValueFrom(service.submitTransfer(mockRequest));
-    throw new Error('Expected an error, but got success response');
-  } catch (error) {
-    expect(error).toEqual(mockError);
-  }
+  await expect(firstValueFrom(service.submitTransfer(mockRequest)))
+    .rejects
+    .toEqual(mockError);
 });
