2b6b5c43bdd22f1ef7aa8e0d722f837905cdaaa2
Syed Hussain <aftab.jersey@gmail.com>
2024-10-02 23:39:46 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,9 +1,41 @@
-it('should return an error when the transfer fails', async () => {
-  const mockError = { message: 'Transfer failed' };
+This assertion is unnecessary since it does not change the type of the expression.
 
-  jest.spyOn(experienceService, 'post').mockReturnValue(throwError(() => mockError));
 
-  await expect(firstValueFrom(service.submitTransfer(mockRequest)))
-    .rejects
-    .toEqual(mockError);
-});
+  public buildTransferOrderRequest(
+    currentPrescriptions: IPrescriptionDetails[]
+  ): TransferOrderRequest {
+    const externalTransfer: ExternalTransfer[] =
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- null' is not assignable to type
+      currentPrescriptions!.length > 0
+        ? currentPrescriptions
+            .map((prescription) => {
+              const rxDetails: RxDetails | null =
+                this.mapRxDetails(prescription);
+
+              if (rxDetails && rxDetails.drugDetails.length > 0) {
+                const patient: Patient = this.mapPatientDetails(prescription);
+
+                return {
+                  requestedChannel: '',
+                  carrierId: '',
+                  clinicalRuleDate: '09/16/2024',
+                  patient,
+                  rxDetails: [rxDetails]
+                };
+              }
+
+              return null;
+            })
+            .filter(
+              (transfer): transfer is ExternalTransfer => transfer !== null
+            )
+        : [];
+
+    return {
+      data: {
+        idType: 'PBM_QL_PARTICIPANT_ID_TYPE',
+        profile: 'test',
+        externalTransfer
+      }
+    };
+  }
