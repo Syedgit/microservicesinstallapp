fd6b4902f9298f9373ed7db9f497a82a29e3b7f2
Syed Hussain <aftab.jersey@gmail.com>
2024-10-02 22:55:53 -0400

    Update microservices.js

--- microservices.js
+++ microservices.js
@@ -1,45 +1,36 @@
-for this test case i have linting error  136:66  error  Return a Promise instead of relying on callback parameter 
-
-it('should submit transfer and return a successful response', (done) => {
-    const mockHttpResponse = new HttpResponse<SubmitTransferResponse>({
-      body: mockResponse,
-      status: 200
-    });
-
-    jest.spyOn(experienceService, 'post').mockReturnValue(of(mockHttpResponse));
-
-    service.submitTransfer(mockRequest).subscribe((response) => {
-      expect(response).toEqual(mockResponse);
-      done();
-    });
-
-    expect(experienceService.post).toHaveBeenCalledWith(
-      Config.clientId,
-      Config.experiences,
-      Config.mock,
-      { data: mockRequest.data },
-      { maxRequestTime: 10_000 }
-    );
+it('should submit transfer and return a successful response', async () => {
+  const mockHttpResponse = new HttpResponse<SubmitTransferResponse>({
+    body: mockResponse,
+    status: 200
   });
 
-for this test i am getting two errors  
-  1- error  Return a Promise instead of relying on callback parameter 
-  2- 58:57  error  Return a Promise instead of relying on callback parameter                       jest/no-done-callback
-  167:9   error  Illegal usage of `fail`, prefer throwing an error, or the `done.fail` callback
- it('should return an error when the transfer fails', (done) => {
-    const mockError = { message: 'Transfer failed' };
+  jest.spyOn(experienceService, 'post').mockReturnValue(of(mockHttpResponse));
 
-    jest
-      .spyOn(experienceService, 'post')
-      .mockReturnValue(throwError(() => mockError));
+  const response = await firstValueFrom(service.submitTransfer(mockRequest));
 
-    service.submitTransfer(mockRequest).subscribe({
-      next: () => {
-        fail('Expected an error, but got success response');
-      },
-      error: (error) => {
-        expect(error).toEqual(mockError);
-        done();
-      }
-    });
-  });
+  expect(response).toEqual(mockResponse);
+
+  expect(experienceService.post).toHaveBeenCalledWith(
+    Config.clientId,
+    Config.experiences,
+    Config.mock,
+    { data: mockRequest.data },
+    { maxRequestTime: 10_000 }
+  );
+});
+
+
+it('should return an error when the transfer fails', async () => {
+  const mockError = { message: 'Transfer failed' };
+
+  jest
+    .spyOn(experienceService, 'post')
+    .mockReturnValue(throwError(() => mockError));
+
+  try {
+    await firstValueFrom(service.submitTransfer(mockRequest));
+    throw new Error('Expected an error, but got success response');
+  } catch (error) {
+    expect(error).toEqual(mockError);
+  }
+});
