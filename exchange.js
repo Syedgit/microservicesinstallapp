990274ffadea748e08e78605545135d99e760f3d
Syed Hussain <aftab.jersey@gmail.com>
2024-10-14 13:58:56 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,42 +1,25 @@
-public mapPharmacyDetails(pharmacy: any): Pharmacy {
-  if (!pharmacy) {
-    this.errorMessage = 'Missing pharmacy details.';
-    throw new Error(this.errorMessage);
-  }
-
-  const pharmacyName = pharmacy.pharmacyName || '';
-  const address = pharmacy.address 
-    ? this.mapAddressDetails(pharmacy.address)
-    : pharmacy.addresses 
-      ? this.mapAddressDetails(pharmacy.addresses)
-      : undefined;
-
-  // Determine storeId: if pharmacyName contains "CVS" or "HYVEE", use pharmacy.storeId, otherwise default to "99999"
-  const storeId = /cvs|hyvee/i.test(pharmacyName) ? (pharmacy.storeId || '') : '99999';
-
-  // Add phoneNumber to address if available
-  if (pharmacy.phoneNumber && address) {
-    address.phoneNumber = pharmacy.phoneNumber;
-  }
-
-  // Additional fields for "CVS" pharmacies
-  const cvsSpecificFields = pharmacyName.toLowerCase().includes('cvs')
-    ? {
-        open24Hours: pharmacy.open24Hours || false,
-        indicatorPharmacyTwentyFourHoursOpen: pharmacy.open24Hours ? 'Y' : 'N',
-        instorePickupService: pharmacy.instorePickupService === 'Y' ? 'Y' : 'N',
-        indicatorDriveThruService: pharmacy.indicatorDriveThruService === 'Y' ? 'Y' : 'N',
-        pharmacyHours: {
-          dayHours: pharmacy.open24Hours ? [] : pharmacy.pharmacyHours?.dayHours || []
-        }
-      }
-    : {};
-
-  // Construct the final pharmacy object
-  return {
-    pharmacyName,
-    address,
-    storeId,
-    ...cvsSpecificFields
+public mapAddressDetails(address: Address, includePhoneNumber: boolean = true): Address {
+  const mappedAddress: Address = {
+    line: address.line || [''],
+    city: address.city || '',
+    state: address.state || '',
+    postalCode: address.postalCode || '',
   };
+
+  if (includePhoneNumber && address.phoneNumber) {
+    mappedAddress.phoneNumber = address.phoneNumber;
+  }
+
+  return mappedAddress;
 }
+
+
+function deleteCookie(name, path) {
+    document.cookie = `${name}=; Path=${path}; Expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+}
+
+// Delete the 'access_token' cookie from the root path
+deleteCookie('access_token', '/');
+
+// Delete the 'access_token' cookie from a subpath
+deleteCookie('access_token', '/subpath');
