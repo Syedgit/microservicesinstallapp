38dfec50cb15b89095c7d4d152e45870ca32e9c7
Syed Hussain <aftab.jersey@gmail.com>
2024-10-14 21:23:42 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,20 +1,81 @@
-deleteCookie(): void {
-  const cookieName = "access_token";
+import { isPlatformServer } from '@angular/common';
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable, inject, PLATFORM_ID } from '@angular/core';
+import { ConfigFacade } from '@digital-blocks/angular/core/store/config';
+import {
+  HttpService,
+  mapResponseBody
+} from '@digital-blocks/angular/core/util/services';
+import { SsrAuthFacade } from '@digital-blocks/angular/pharmacy/shared/store/ssr-auth';
+import { filter, map, Observable, switchMap } from 'rxjs';
 
-  if (typeof document === 'undefined') {
-    return;
-  }
+import {
+  GetMemberInfoAndTokenRequest,
+  GetMemberInfoAndTokenResponse,
+  OauthResponse
+} from '../+state/member-authentication.interfaces';
 
-  // Function to delete a cookie with a specific path
-  const deleteCookieAtPath = (path: string): void => {
-    document.cookie = `${cookieName}=; Path=${path}; Expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
-  };
+import { b2bConfig } from './member-authentication.config';
 
-  // List of possible paths where the cookies might exist
-  const possiblePaths = ['/', '/some-path']; // Add other paths as needed
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberAuthenticationService {
+  private readonly ssrAuthFacade = inject(SsrAuthFacade);
+  private readonly configFacade = inject(ConfigFacade);
+  private readonly httpService = inject(HttpService);
+  private readonly platformId = inject(PLATFORM_ID);
 
-  // Loop through the possible paths and delete the cookie at each path
-  for (const path of possiblePaths) {
-    deleteCookieAtPath(path);
+  getMemberInfoAndToken(
+    request: GetMemberInfoAndTokenRequest,
+    useTransferSecret = true
+  ): Observable<GetMemberInfoAndTokenResponse> {
+    this.ssrAuthFacade.getSsrAuth(useTransferSecret);
+
+    return this.ssrAuthFacade.ssrAuth$.pipe(
+      filter(
+        (ssrAuth): ssrAuth is OauthResponse =>
+          !!ssrAuth && !!ssrAuth.access_token
+      ),
+      switchMap((ssrAuth) => {
+        return this.configFacade.config$.pipe(
+          filter((config) => !isPlatformServer(this.platformId) && !!config),
+          switchMap((config) => {
+            const requestData = {
+              data: {
+                idType: 'PBM_QL_ENC_PARTICIPANT_ID_TYPE',
+                lookupReq: request.data.lookupReq
+              }
+            };
+
+            const headers = new HttpHeaders({
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${ssrAuth.access_token}`,
+              'x-experienceId': b2bConfig.expId,
+              'x-api-key': b2bConfig['x-api-key']
+            });
+
+            return this.httpService
+              .post<GetMemberInfoAndTokenResponse>(
+                `${config.environment.basePath}${b2bConfig.b2bUrl}`,
+                b2bConfig.MOCK,
+                {
+                  headers
+                },
+                requestData,
+                {
+                  maxRequestTime: 10_000
+                }
+              )
+              .pipe(
+                mapResponseBody(),
+                map((response: GetMemberInfoAndTokenResponse) => {
+                  return response;
+                })
+              );
+          })
+        );
+      })
+    );
   }
 }
