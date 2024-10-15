47e6c1d8a80161df105c72b1168b90f63f1cb2a6
Syed Hussain <aftab.jersey@gmail.com>
2024-10-14 20:06:58 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,18 +1,20 @@
 deleteCookie(): void {
-    const cookieName = "access_token"
-    if (typeof document === 'undefined') {
-      return undefined;
-    }
-    let cookie = document?.cookie
-      .split('; ')
-      .map((cookie) => {
-        const [name, value, path] = cookie.split('=');
+  const cookieName = "access_token";
 
-        return {
-          name,
-          value,
-          path
-        };
-      })
-      // cookie = `${name}=; Path=${path}; Expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+  if (typeof document === 'undefined') {
+    return;
   }
+
+  // Function to delete a cookie with a specific path
+  const deleteCookieAtPath = (path: string): void => {
+    document.cookie = `${cookieName}=; Path=${path}; Expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
+  };
+
+  // List of possible paths where the cookies might exist
+  const possiblePaths = ['/', '/some-path']; // Add other paths as needed
+
+  // Loop through the possible paths and delete the cookie at each path
+  for (const path of possiblePaths) {
+    deleteCookieAtPath(path);
+  }
+}
