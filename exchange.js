93137fdbe049f1418d6a43947a3f9bd4ba83a586
Syed Hussain <aftab.jersey@gmail.com>
2024-10-21 21:16:55 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -3,7 +3,7 @@ import { HttpHeaders } from '@angular/common/http';
 import { ConfigFacade } from '@digital-blocks/angular/core/store/config';
 import { HttpService, mapResponseBody } from '@digital-blocks/angular/core/util/services';
 import { SsrAuthFacade } from '@digital-blocks/angular/pharmacy/shared/store/ssr-auth';
-import { Observable, switchMap, take, tap } from 'rxjs';
+import { Observable, switchMap, take, tap, throwError } from 'rxjs';
 import { GetMemberInfoAndTokenRequest, GetMemberInfoAndTokenResponse } from '../+state/member-authentication.interfaces';
 import { b2bConfig } from './member-authentication.config';
 
@@ -21,20 +21,22 @@ export class MemberAuthenticationService {
     // Trigger SSR Auth token retrieval
     this.ssrAuthFacade.getSsrAuth(useTransferSecret);
     
-    // Wait for the SSR Auth token and then make the B2B call
+    // Wait for the SSR Auth token and then immediately make the B2B call
     return this.ssrAuthFacade.ssrAuth$.pipe(
       take(1),
+      tap(ssrAuth => console.log('SSR Auth token received:', ssrAuth?.access_token ? 'Valid token' : 'No token')),
       switchMap(ssrAuth => {
         if (!ssrAuth?.access_token) {
-          throw new Error('Failed to obtain SSR Auth token');
+          console.error('Failed to obtain SSR Auth token');
+          return throwError(() => new Error('Failed to obtain SSR Auth token'));
         }
+        console.log('Making B2B call with SSR Auth token');
         return this.makeB2BCall(request, ssrAuth.access_token);
       })
     );
   }
 
   private makeB2BCall(request: GetMemberInfoAndTokenRequest, token: string): Observable<GetMemberInfoAndTokenResponse> {
-    console.log('Making B2B call');
     return this.configFacade.config$.pipe(
       take(1),
       switchMap(config => {
@@ -45,7 +47,7 @@ export class MemberAuthenticationService {
           'x-api-key': config.b2bApiKey
         });
 
-        console.log('B2B call headers set');
+        console.log('B2B call headers set, making the call');
         return this.httpService.post<GetMemberInfoAndTokenResponse>(
           `${config.environment.basePath}${b2bConfig.b2bUrl}`,
           b2bConfig.MOCK,
@@ -53,7 +55,7 @@ export class MemberAuthenticationService {
           { data: { idType: 'PBM_QL_ENC_PARTICIPANT_ID_TYPE', lookupReq: request.data.lookupReq } },
           { maxRequestTime: 20_000 }
         ).pipe(
-          tap(() => console.log('B2B call made')),
+          tap(() => console.log('B2B call completed')),
           mapResponseBody()
         );
       })
