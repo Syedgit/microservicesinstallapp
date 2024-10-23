479c5744894b37f4c4c38b478a8c0f4cafd2ce5f
Syed Hussain <aftab.jersey@gmail.com>
2024-10-23 12:08:36 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,48 +1,35 @@
- public submitTransfer(): void {
-    try {
-      if (this.currentPrescriptions.length > 0 && this.selectedPharmacy) {
-        const transferOrderRequest = this.buildTransferOrderRequest(
-          this.currentPrescriptions
-        );
+public submitTransfer(): void {
+  try {
+    if (this.currentPrescriptions.length > 0 && this.selectedPharmacy) {
+      const transferOrderRequest = this.buildTransferOrderRequest(this.currentPrescriptions);
+      
+      this.store.submitTransfer(transferOrderRequest);
+      this.store.submitTransferResponse$.subscribe((response) => {
+        if (response) {
+          const submitExternalTransfer = response.data?.submitExternalTransfer || [];
 
-        this.store.submitTransfer(transferOrderRequest);
-        this.store.submitTransferResponse$.subscribe((response) => {
-          if (response) {
-            if (response.statusCode === '0000') {
-              this.handleSuccess();
-            } else {
-              this.handleError(response);
-            }
+          const hasSuccess = submitExternalTransfer.some(
+            (transfer) => transfer.statusCode === '0000'
+          );
+
+          const allFailed = submitExternalTransfer.every(
+            (transfer) => transfer.statusCode === '0002'
+          );
+
+          if (hasSuccess) {
+            this.handleSuccess();
+          } else if (allFailed) {
+            throw new Error('All transfers failed');
           }
-        });
-      } else {
-        this.store.setStateFailure(true);
-        throw new Error('No prescriptions selected for transfer');
-      }
-    } catch (error) {
+        }
+      });
+    } else {
       this.store.setStateFailure(true);
-      errorMessage('submitTransferFailure', error);
-      throw error;
+      throw new Error('No prescriptions selected for transfer');
     }
+  } catch (error) {
+    this.store.setStateFailure(true);
+    errorMessage('submitTransferFailure', error);
+    throw error;
   }
-
-Response :
-
-{
-    "statusCode": "0000",
-    "statusDescription": "Success",
-    "data": {
-        "submitExternalTransfer": [
-            {
-                "statusCode": "0000",
-                "statusDescription": "Success",
-                "confirmationNumber": "TU20241022160335XM3X"
-            },
-            {
-                "statusCode": "0002",
-                "statusDescription": "Failure",
-                "confirmationNumber": "TU202410221603353EDJ"
-            }
-        ]
-    }
 }
