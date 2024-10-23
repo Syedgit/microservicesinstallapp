7038de60becd252f2e28e58b7fe45c9a7308f620
Syed Hussain <aftab.jersey@gmail.com>
2024-10-23 13:18:13 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,54 +1,50 @@
+import { of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
 public submitTransfer(): void {
-  if (this.currentPrescriptions.length > 0 && this.selectedPharmacy) {
-    const transferOrderRequest = this.buildTransferOrderRequest(
-      this.currentPrescriptions
-    );
-
-    this.store.submitTransfer(transferOrderRequest);
-
-    this.store.submitTransferResponse$
-      .pipe(
-        map((response) => {
-          if (!response || !response.data?.submitExternalTransfer) {
-            throw new Error('Invalid response format');
-          }
-
-          // Check if all transfers failed
-          const allFailed = response.data.submitExternalTransfer.every(
-            (transfer) => transfer.statusCode === '0002'
-          );
-
-          if (allFailed) {
-            throw new Error('All transfers failed');
-          }
-
-          return response; // Pass the response along the stream if successful
-        }),
-        catchError((error) => {
-          this.store.setStateFailure(true);
-          errorMessage('submitTransferFailure', error);
-          return throwError(() => error); // Rethrow the error to trigger catch block
-        })
-      )
-      .subscribe({
-        next: (response) => {
-          const hasSuccessfulTransfer = response.data.submitExternalTransfer.some(
-            (transfer) => transfer.statusCode === '0000'
-          );
-
-          if (hasSuccessfulTransfer) {
-            this.handleSuccess(); // Navigate to the confirmation page
-          } else {
-            this.handleError(response); // Handle the error scenario
-          }
-        },
-        error: (error) => {
-          // This will be triggered when any error is thrown
-          this.handleError({ statusDescription: error.message });
-        }
-      });
-  } else {
+  if (this.currentPrescriptions.length === 0 || !this.selectedPharmacy) {
     this.store.setStateFailure(true);
     throw new Error('No prescriptions selected for transfer');
   }
+
+  const transferOrderRequest = this.buildTransferOrderRequest(this.currentPrescriptions);
+
+  this.store.submitTransfer(transferOrderRequest);
+
+  this.store.submitTransferResponse$
+    .pipe(
+      map((response) => {
+        const transfers = response?.data?.submitExternalTransfer || [];
+        
+        // Check if all transfers failed
+        const allFailed = transfers.every(transfer => transfer.statusCode === '0002');
+
+        if (allFailed) {
+          throw new Error('All transfers failed');
+        }
+
+        return transfers; // Pass transfers along if at least one succeeds
+      }),
+      catchError((error) => {
+        this.store.setStateFailure(true);
+        errorMessage('submitTransferFailure', error);
+        return throwError(() => new Error('Failed to submit transfer')); // Propagate the error
+      })
+    )
+    .subscribe({
+      next: (transfers) => {
+        // Check if at least one transfer succeeded
+        const hasSuccessfulTransfer = transfers.some(transfer => transfer.statusCode === '0000');
+
+        if (hasSuccessfulTransfer) {
+          this.handleSuccess(); // Navigate on success
+        } else {
+          this.handleError({ statusDescription: 'Transfer failed' });
+        }
+      },
+      error: (error) => {
+        // Handle any errors from catch block
+        this.handleError({ statusDescription: error.message });
+      }
+    });
 }
