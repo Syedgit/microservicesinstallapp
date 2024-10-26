9707397661f63728044045f2b28962dd0f9bce73
Syed Hussain <aftab.jersey@gmail.com>
2024-10-26 00:56:58 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,106 +1,145 @@
-import { inject, Injectable } from '@angular/core';
-import { errorMessage } from '@digital-blocks/angular/core/util/error-handler';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
-import { catchError, map, of, switchMap, tap } from 'rxjs';
-import { NavigationService } from '@digital-blocks/angular/core/util/services';
+import { isPlatformServer } from '@angular/common';
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable, inject, PLATFORM_ID } from '@angular/core';
+import { ConfigFacade } from '@digital-blocks/angular/core/store/config';
+import { Config } from '@digital-blocks/angular/core/util/config';
+import {
+  HttpService,
+  mapResponseBody
+} from '@digital-blocks/angular/core/util/services';
+import { SsrAuthFacade } from '@digital-blocks/angular/pharmacy/shared/store/ssr-auth';
+import {
+  catchError,
+  filter,
+  Observable,
+  of,
+  switchMap,
+  throwError,
+  take
+} from 'rxjs';
 
-import { MemberAuthenticationService } from '../services/member-authentication.service';
-import { MemberAuthenticationActions } from './member-authentication.actions';
-import { GetMemberInfoAndTokenResponse } from './member-authentication.interfaces';
-import { MemberAuthenticationState } from './member-authentication.reducer';
-import { AdobeTaggingConstants } from '../../constants';
+import {
+  GetMemberInfoAndTokenRequest,
+  GetMemberInfoAndTokenResponse,
+  OauthResponse
+} from '../+state/member-authentication.interfaces';
 
-@Injectable()
-export class MemberAuthenticationEffects {
-  private readonly actions$ = inject(Actions);
-  private readonly memberAuthService = inject(MemberAuthenticationService);
-  private readonly store = inject(Store<MemberAuthenticationState>);
-  private readonly navigationService = inject(NavigationService);
-  private readonly errorTag = 'MemberAuthenticationEffects';
+import { b2bConfig } from './member-authentication.config';
 
-  public getMemberInfoAndToken$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(MemberAuthenticationActions.getMemberInfoAndToken),
-      switchMap(({ request, useTransferSecret }) => {
-        return this.memberAuthService
-          .getMemberInfoAndToken(request, useTransferSecret)
-          .pipe(
-            map((response) => {
-              const memberTokenResponse: GetMemberInfoAndTokenResponse =
-                response;
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberAuthenticationService {
+  private readonly ssrAuthFacade = inject(SsrAuthFacade);
+  private readonly configFacade = inject(ConfigFacade);
+  private readonly httpService = inject(HttpService);
+  private readonly platformId = inject(PLATFORM_ID);
 
-              return response?.statusCode === '0000'
-                ? MemberAuthenticationActions.getMemberInfoAndTokenSuccess({
-                    memberTokenResponse
-                  })
-                : MemberAuthenticationActions.getMemberInfoAndTokenFailure({
-                    error: errorMessage(
-                      this.errorTag,
-                      'B2B Member Authentication API failed'
-                    )
-                  });
-            }),
-            catchError((error: unknown) => {
-              return of(
-                MemberAuthenticationActions.getMemberInfoAndTokenFailure({
-                  error: errorMessage(this.errorTag, error)
-                })
-              );
-            })
-          );
+  /**
+   * Retrieves member info and token.
+   * @param request Member request object.
+   * @param useTransferSecret Flag to determine whether to use transfer secret.
+   */
+  getMemberInfoAndToken(
+    request: GetMemberInfoAndTokenRequest,
+    useTransferSecret = true
+  ): Observable<GetMemberInfoAndTokenResponse> {
+    this.ssrAuthFacade.getSsrAuth(useTransferSecret);
+    return this.ssrAuthFacade.ssrAuth$.pipe(
+      filter(
+        (ssrAuth): ssrAuth is OauthResponse =>
+          !!ssrAuth && !!ssrAuth.access_token
+      ),
+      switchMap((ssrAuth) => {
+        return this.configFacade.config$.pipe(
+          filter((config) => !isPlatformServer(this.platformId) && !!config),
+          switchMap((config) =>
+            this.makeB2BCall(request, ssrAuth.access_token, config)
+          )
+        );
+      }),
+      catchError((error: unknown) => {
+        return throwError(
+          () => new Error('Failed to get member info and token')
+        );
       })
     );
-  });
+  }
 
-  // Effect to handle navigation, rehydration, and logging on success
-  public handleNavigationOnSuccess$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(MemberAuthenticationActions.getMemberInfoAndTokenSuccess),
-        tap(() => {
-          // Navigate to the next page
-          this.navigationService.navigate(
-            '/pharmacy/benefits/transfer/current-prescriptions',
-            { queryParamsHandling: 'preserve' },
-            { navigateByPath: true }
-          );
+  /**
+   * Makes the B2B call using the current SSR access token.
+   * Retries if the token is invalid (based on B2B response or 401).
+   * @param request Member request object.
+   * @param token SSR access token.
+   * @param config Configuration object.
+   */
+  private makeB2BCall(
+    request: GetMemberInfoAndTokenRequest,
+    token: string,
+    config: Config
+  ): Observable<GetMemberInfoAndTokenResponse> {
+    const requestData = {
+      data: {
+        idType: 'PBM_QL_ENC_PARTICIPANT_ID_TYPE',
+        lookupReq: request.data.lookupReq
+      }
+    };
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+      'x-experienceId': b2bConfig.expId,
+      'x-api-key': config.b2bApiKey
+    });
 
-          // Save member info and log the authentication action
-          this.store.dispatch(
-            MemberAuthenticationActions.saveMemberInfoRehydrate({
-              rehydrate: ['patientInfo']
-            })
-          );
-          this.store.dispatch(
-            MemberAuthenticationActions.logMemberAuthLink({
-              linkName: AdobeTaggingConstants.ONCLICK_CONTINUE.link_name,
-              details: AdobeTaggingConstants.ONCLICK_CONTINUE.details
-            })
-          );
+    return this.httpService
+      .post<GetMemberInfoAndTokenResponse>(
+        `${config.environment.basePath}${b2bConfig.b2bUrl}`,
+        b2bConfig.MOCK,
+        { headers },
+        requestData,
+        { maxRequestTime: 20_000 }
+      )
+      .pipe(
+        mapResponseBody(),
+        switchMap((response: GetMemberInfoAndTokenResponse) => {
+          if (
+            response?.statusCode === '1009' ||
+            response?.statusDescription.includes(
+              'Access token is no longer valid'
+            )
+          ) {
+            return this.retrySsrAuthAndB2B(request);
+          }
+
+          return of(response);
         })
-      ),
-    { dispatch: false }
-  );
+      );
+  }
 
-  // Handle failure case by logging the error
-  public handleAuthFailure$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(MemberAuthenticationActions.getMemberInfoAndTokenFailure),
-        tap(() => {
-          const tagData = AdobeTaggingConstants.VALIDATION_ERROR;
-          this.store.dispatch(
-            MemberAuthenticationActions.logMemberAuthLink({
-              linkName: tagData.link_name,
-              details: {
-                field_errors: tagData.details.field_error,
-                error_messages: '1'
-              }
-            })
-          );
-        })
+  /**
+   * Helper method to retry the SSR Auth and B2B call.
+   * @param request Member request object.
+   */
+  public retrySsrAuthAndB2B(
+    request: GetMemberInfoAndTokenRequest
+  ): Observable<GetMemberInfoAndTokenResponse> {
+    this.ssrAuthFacade.getSsrAuth(true);
+
+    return this.ssrAuthFacade.ssrAuth$.pipe(
+      filter(
+        (ssrAuth): ssrAuth is OauthResponse =>
+          !!ssrAuth && !!ssrAuth.access_token
       ),
-    { dispatch: false }
-  );
+      take(1),
+      switchMap((ssrAuth) =>
+        this.configFacade.config$.pipe(
+          filter((config) => !!config),
+          take(1),
+          switchMap((config) =>
+            this.makeB2BCall(request, ssrAuth.access_token, config)
+          )
+        )
+      )
+    );
+  }
 }
