93f324047fee9ccb4ff8f9904d3c325611c3bee3
Syed Hussain <aftab.jersey@gmail.com>
2024-11-06 16:39:49 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -9,22 +9,23 @@
   <ng-template #memberAuthTemplate>
     <ng-container>
       <div class="member-container">
-        @if (!hasErrors && error$) {
-          <ps-alert-bar>
-            <h2 slot="heading">
-              {{ backendError.heading }}
-            </h2>
-            <p>{{ backendError.description }}</p>
-          </ps-alert-bar>
-        } 
-        @if (!error$ && hasErrors) {
-          <ps-alert-bar>
-            <h2 slot="heading">
-              {{ genericError.heading }}
-            </h2>
-            <p>{{ genericError.description }}</p>
-          </ps-alert-bar>
-        }
+        
+        <!-- Backend error -->
+        <ps-alert-bar *ngIf="(error$ | async) && !hasErrors">
+          <h2 slot="heading">
+            {{ backendError.heading }}
+          </h2>
+          <p>{{ backendError.description }}</p>
+        </ps-alert-bar>
+        
+        <!-- Form validation error -->
+        <ps-alert-bar *ngIf="hasErrors && !(error$ | async)">
+          <h2 slot="heading">
+            {{ genericError.heading }}
+          </h2>
+          <p>{{ genericError.description }}</p>
+        </ps-alert-bar>
+
         <div class="sub-header-content">
           <lib-transfer-prescriptions-sub-header
             [isCmsHeading]="true"
@@ -32,71 +33,49 @@
               (store.cmsSpotContents$ | async)?.TransferRxIntroSpot ?? ''
             "></lib-transfer-prescriptions-sub-header>
         </div>
+        
         <p>All fields required.</p>
-        <ng-container>
-          <form [formGroup]="memberForm">
-            <ps-input
-              label="First Name"
-              [error]="
-                getError('first') == '' ? undefined : formErrors['firstName']
-              ">
-              <input
-                formControlName="firstName"
-                ngDefaultControl
-                slot="input"
-                required
-                is-error-announced="true" />
-            </ps-input>
-            <ps-input
-              label="Last Name"
-              [error]="
-                getError('last') == '' ? undefined : formErrors['lastName']
-              ">
-              <input
-                formControlName="lastName"
-                ngDefaultControl
-                input-required="true"
-                slot="input"
-                is-error-announced="true" />
-            </ps-input>
-            <ps-date-fieldset
-              legend="Date of Birth"
-              is-dob
-              [error]="getError('dob')">
-              <!-- eslint-disable-next-line -- custom inputs are needed -->
-              <input slot="month" formControlName="month" ngDefaultControl />
-              <!-- eslint-disable-next-line -- custom inputs are needed -->
-              <input slot="day" formControlName="day" ngDefaultControl />
-              <!-- eslint-disable-next-line -- custom inputs are needed -->
-              <input slot="year" formControlName="year" ngDefaultControl />
-            </ps-date-fieldset>
-            <ps-input
-              label="Member ID"
-              [error]="hasErrors ? getError('memberId') : undefined">
-              <input
-                formControlName="memberId"
-                ngDefaultControl
-                name="memberId"
-                slot="input"
-                input-required="true"
-                is-error-announced="true" />
-            </ps-input>
-            <div>
-              <ps-button
-                is-full-width="true"
-                size="md"
-                submit="true"
-                variant="solid"
-                [isFullWidth]="layoutFacade.breakpointSmall$ | async"
-                (keydown)="handleKeyDown($event)"
-                (click)="getMemberInfoAndToken()"
-                class="continue-button">
-                Continue
-              </ps-button>
-            </div>
-          </form>
-        </ng-container>
-      </div>
-    </ng-container>
-  </ng-template>
-</ng-container>
+        <form [formGroup]="memberForm">
+          <ps-input
+            label="First Name"
+            [error]="
+              getError('first') == '' ? undefined : formErrors['firstName']
+            ">
+            <input
+              formControlName="firstName"
+              ngDefaultControl
+              slot="input"
+              required
+              is-error-announced="true" />
+          </ps-input>
+          
+          <ps-input
+            label="Last Name"
+            [error]="
+              getError('last') == '' ? undefined : formErrors['lastName']
+            ">
+            <input
+              formControlName="lastName"
+              ngDefaultControl
+              input-required="true"
+              slot="input"
+              is-error-announced="true" />
+          </ps-input>
+          
+          <ps-date-fieldset
+            legend="Date of Birth"
+            is-dob
+            [error]="getError('dob')">
+            <input slot="month" formControlName="month" ngDefaultControl />
+            <input slot="day" formControlName="day" ngDefaultControl />
+            <input slot="year" formControlName="year" ngDefaultControl />
+          </ps-date-fieldset>
+          
+          <ps-input
+            label="Member ID"
+            [error]="hasErrors ? getError('memberId') : undefined">
+            <input
+              formControlName="memberId"
+              ngDefaultControl
+              name="memberId"
+              slo
