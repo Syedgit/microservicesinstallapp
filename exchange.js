6068610d58817177f291f46016c2ae9f98a35905
Syed Hussain <aftab.jersey@gmail.com>
2024-11-19 15:58:52 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,34 +1,95 @@
-getError(field: string): string {
-  switch (field) {
-    case 'first': {
-      return this.firstNameError();
-    }
-    case 'last': {
-      return this.lastNameError();
-    }
-    case 'dob': {
-      if (this.clickedContinue) {
-        this.formErrors['dateOfBirth'] = this.isDateValid();
-      } else {
-        this.validateDate();
-      }
-      if (this.formErrors['dateOfBirth']) {
-        this.taggingErrors += this.formErrors['dateOfBirth'] + '|';
-      }
-      return this.formErrors['dateOfBirth'];
-    }
-    case 'memberId': {
-      const message = this.memberForm.controls['memberId'].hasError('required')
-        ? Constants.MEM_ID_ERROR
-        : '';
-      this.formErrors['memberId'] = message;
-      if (message) {
-        this.taggingErrors += message + '|';
-      }
-      return message;
-    }
-    default: {
-      return 'This is the default case. Choose an option.';
-    }
-  }
-}
+Overall Comments on Goal Achievement:
+
+I am pleased to report that I have successfully accomplished the following goals throughout the year, aligning with the company's OKRs and focusing on code quality, application development process improvements, and enhancing the Valet metric:
+
+1. SPIL2 Migration for Specialty APIs:
+I led the successful migration of specialty APIs to the new SPIL2 platform, prioritizing code quality and optimizing performance to contribute to the Valet metric enhancement. This migration ensured the efficient and secure operation of the APIs, aligning with our OKR to improve overall system reliability.
+
+2. SDK Lift and Shift:
+In executing the SDK lift and shift project, I paid careful attention to the Valet metric by optimizing the development process to reduce turnaround time for new features and bug fixes. The successful migration of 80 APIs to the cloud improved scalability and performance, resulting in an enhanced Valet metric for the organization.
+
+3. Specialty Transfer for Client Convenience:
+By enhancing the specialty transfer feature, I focused on improving the user experience and streamlining the transfer process. This initiative positively impacted the Valet metric by reducing the time required to transfer prescriptions, contributing to increased customer satisfaction and loyalty.
+
+4. Support for Welcome Season and Monitoring Activities:
+During the welcome season, I actively monitored the Valet metric and identified areas for improvement. By proactively addressing any issues, optimizing code, and providing timely resolutions, I contributed to the enhancement of the Valet metric during this critical period.
+
+6. Apigee onboarding 
+In the second half of the year, I played a pivotal role in onboarding several clients to Apigee for seamless consumption of our services. This involved providing extensive support for clients to understand and utilize Apigee effectively. By ensuring a smooth onboarding process, I contributed to strengthening our client relationships and expanding our service reach.
+
+7. Code Quality Enhancement (OKR) 
+I have been dedicated to enhancing our code quality by advocating and implementing best coding practices, conducting regular code reviews, and actively participating in peer programming sessions. This proactive approach ensures that our codebase remains robust, scalable, and adherent to industry standards
+
+8. Prod Releases and Bug Free Code Delivery (OKR) 
+ I've been pivotal in our production deployments and releases, overseeing the delivery process to guarantee bug-free code reaches production. This involves thorough testing, collaboration with cross-functional teams, and stringent quality checks to uphold our standard of delivering reliable apps to our clients.
+
+9. End to End Testing and Feature Understanding 
+As part of client support, I actively engaged in end-to-end testing, ensuring that our clients had a comprehensive understanding of the features available. This hands-on approach not only facilitated a smoother integration of our services into client systems but also improved overall client satisfaction by addressing any potential issues in a proactive manner.
+
+
+10. Team leadership and support 
+Throughout the year, I've taken a leadership role within our development team, actively troubleshooting and resolving blockers to ensure project continuity. I've played a key part in mentoring new team members, fostering a collaborative environment, and facilitating smooth onboarding
+
+11. Knowledge Sharing and Skill Development 
+I've championed knowledge-sharing sessions, emphasizing continuous learning. These sessions, tailored especially for new team members, have not only encouraged the exchange of expertise but have significantly contributed to skill development. This aligns with our organizational goals of fostering a culture of learning and innovation.
+
+Overall, Heart At Behavior 
+
+Heart at Behaviors:
+
+1. Put People First:
+I prioritize creating a supportive and inclusive work environment, fostering open communication, and empowering team members to drive success.
+
+2. Rise to Challenges:
+I embrace challenges as opportunities for growth, leveraging my technical expertise and problem-solving skills to overcome obstacles and deliver innovative solutions.
+
+3. Join Forces:
+I promote collaboration and teamwork, leveraging diverse skills and perspectives to work together towards common goals and achieve synergy.
+
+4. Create Simplicity:
+I strive to simplify complex processes and workflows, finding efficient and streamlined solutions that enhance productivity and drive success.
+
+5. Inspire Trust:
+Through my integrity, transparency, and accountability, I build trust among team members and stakeholders, fostering effective collaboration and achieving desired outcomes.
+
+
+
+First Half of the Year  
+•	SPIL2 Migration for Specialty APIs:
+o	Successfully led the migration of specialty APIs to the new SPIL2 platform.
+o	Prioritized code quality and optimized performance to enhance the Valet metric.
+o	Ensured the efficient and secure operation of APIs, aligning with the OKR to improve overall system reliability.
+•	SDK Lift and Shift:
+o	Executed the SDK lift and shift project with careful attention to the Valet metric.
+o	Optimized the development process, reducing turnaround time for new features and bug fixes.
+o	Migrated 80 APIs to the cloud, improving scalability and performance, resulting in an enhanced Valet metric.
+•	Specialty Transfer for Client Convenience:
+o	Enhanced the specialty transfer feature to improve the user experience and streamline the transfer process this help client transfer the specialty drugs from non-cvs to cvs pharmacy.
+o	Positively impacted the Valet metric by reducing the time required to transfer prescriptions.
+o	Contributed to increased customer satisfaction and loyalty.
+•	Support for Welcome Season and Monitoring Activities:
+o	Actively monitored the Valet metric during the welcome season.
+o	Identified areas for improvement, optimized code, and provided timely resolutions.
+o	Contributed to the enhancement of the Valet metric during this critical period.
+
+Second Half of the Year:
+5.     Apigee Onboarding:
+o	Played a pivotal role in onboarding several clients to Apigee for seamless service consumption.
+o	Provided extensive support for clients to understand and utilize Apigee effectively.
+o	Contributed to strengthening client relationships and expanding our service reach.
+6.     Code Quality Enhancement (OKR):
+o	Dedicated efforts to enhance code quality by advocating best coding practices.
+o	Conducted regular code reviews and actively participated in peer programming sessions.
+o	Ensured our codebase remains robust, scalable, and adherent to industry standards.
+7.     Prod Releases and Bug-Free Code Delivery (OKR):
+o	Pivotal role in production deployments, overseeing the delivery process for bug-free code.
+o	Thorough testing, collaboration with cross-functional teams, and stringent quality checks.
+o	Upheld the standard of delivering reliable applications to our clients.
+8.     End-to-End Testing and Feature Understanding:
+o	Actively engaged in end-to-end testing as part of client support.
+o	Ensured clients had a comprehensive understanding of available features.
+o	Proactively addressed potential issues, improving overall client satisfaction.
+9.	 Retail Order Status, Retail Capability Implementation for BCBSMA
+o	Led the implementation of Retail Order Status and Retail Capability for BCBSMA.
+o	Collaborated effectively with partner teams, ensuring seamless integration and functionality.
+
