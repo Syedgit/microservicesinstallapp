9d854a860719105da7ed387b7f2cbc29082ad039
Syed Hussain <aftab.jersey@gmail.com>
2025-02-03 14:27:58 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,88 +1,163 @@
+actions.ts
 
-Here’s the updated self-assessment document with references to OKRs removed:
+import { ReportableError } from '@digital-blocks/angular/core/util/error-handler';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
-Self-Assessment: 2024
-Overall Comments on Goal Achievement
-I am pleased to report that I have successfully accomplished several key goals this year, focusing on code quality, user experience, process improvements, and client support. These initiatives have enhanced system reliability, streamlined user flows, improved member satisfaction, and strengthened client relationships. Additionally, as a Tech Lead, I played a critical role in ensuring the team understood requirements and delivered projects effectively.
+export const PlPharmacyDetailActions = createActionGroup({
+  source: 'PlPharmacyDetail',
+  events: {
+    'Get PlPharmacyDetail': emptyProps(),
+    'Get PlPharmacyDetail Success': emptyProps(),
+    'Get PlPharmacyDetail Failure': props<{ error: ReportableError }>(),
+    'Edit PlPharmacyDetail': props<{ plPharmacyDetail: string }>()
+  }
+});
 
-Client API Migrated Specialty Order Services to SPIL2:
-I led the migration of Specialty Order Services to SPIL2 for Client APIs, prioritizing code quality and optimizing performance. This migration improved scalability and ensured seamless operations, contributing to the reliability and efficiency of the system.
 
-Onboarding Aetna Retail Guest Flow:
-I contributed to the successful onboarding of the Aetna Retail Guest Flow, working on retail services to ensure they met the specific needs of the Aetna retail process. This onboarding enabled Aetna to seamlessly utilize our guest flow and place retail orders using these services. The project not only enhanced the user experience for Aetna members but also ensured that their retail order requirements were fully met, further strengthening our client partnership and driving service adoption.
+Effects.ts
 
-Pharmacy Locator - Specialty Filter Enhancement:
-I implemented a Specialty Filter enhancement to the Caremark UI within the Pharmacy Locator. This improvement allowed for better usability and a more personalized user experience, addressing a key need for specialty users. Additionally, it supported filtering Mail and Retail (Specialty) pharmacies based on the member's filter search criteria, enabling members to find the most relevant pharmacy options with ease and efficiency.
+import { inject, Injectable } from '@angular/core';
+import { errorMessage } from '@digital-blocks/angular/core/util/error-handler';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, of } from 'rxjs';
 
-HyVee Transfer Prescriptions:
-I built a new application enabling members to transfer prescriptions from out-of-network pharmacies to HyVee. This application simplified the prescription transfer process, increasing convenience and improving member satisfaction.
+import { PlPharmacyDetailActions } from './pl-pharmacy-detail.actions';
 
-Find Pharmacy Services Migration to i90:
-I successfully migrated all Find Pharmacy services in the backend to i90 for Client APIs. This migration improved backend performance, streamlined data flow, and ensured greater scalability, contributing to enhanced service delivery.
+@Injectable()
+export class PlPharmacyDetailEffects {
+  private readonly actions$ = inject(Actions);
 
-Managed Production Releases and Deployed Bug-Free Code:
-Throughout the year, I played a pivotal role in managing production releases across all key projects. By conducting thorough testing and implementing robust quality control measures, I consistently delivered bug-free code, ensuring reliability and client satisfaction. My proactive management of release cycles minimized downtime and maintained high operational standards.
+  private readonly errorTag = 'PlPharmacyDetailEffects';
 
-Client Support for BCBSMA, Aetna, and Molina:
-Throughout the year, I provided extensive client support for BCBSMA, Aetna, and Molina to consume client API services. This included resolving queries, addressing questions, and facilitating smooth integrations. These efforts helped strengthen client relationships, improved service adoption, and ensured that client needs were met efficiently and effectively.
+  public getPlPharmacyDetail$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(PlPharmacyDetailActions.getPlPharmacyDetail),
+      map(() => {
+        return PlPharmacyDetailActions.getPlPharmacyDetailSuccess();
+      }),
+      catchError((error: unknown) => {
+        return of(
+          PlPharmacyDetailActions.getPlPharmacyDetailFailure({
+            error: errorMessage(this.errorTag, error)
+          })
+        );
+      })
+    );
+  });
+}
 
-Team Leadership as a Tech Lead:
-As a Tech Lead, I led the team by facilitating clear communication, ensuring developers fully understood requirements in a timely manner, and providing guidance throughout the project lifecycle. I also worked to align the team’s efforts with organizational goals, maintaining high productivity and delivering projects successfully.
+facade.ts
 
-First Half of the Year
-Client API Migration to SPIL2:
+import { Injectable, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
 
-Successfully migrated Specialty Order Services to SPIL2.
-Ensured robust performance and scalability through optimized architecture.
-Contributed to the reliability and efficiency of the system.
-Onboarding Aetna Retail Guest Flow:
+import { PlPharmacyDetailActions } from './pl-pharmacy-detail.actions';
+import { PlPharmacyDetailFeature } from './pl-pharmacy-detail.reducer';
 
-Collaborated with cross-functional teams to onboard the Aetna Retail Guest Flow.
-Ensured retail services met Aetna’s specific requirements.
-Enabled Aetna to seamlessly utilize the guest flow and place retail orders using these services.
-Second Half of the Year
-Pharmacy Locator - Specialty Filter Enhancement:
+@Injectable({ providedIn: 'root' })
+export class PlPharmacyDetailFacade {
+  protected readonly store = inject(Store);
 
-Developed and deployed a Specialty Filter feature in the Caremark UI.
-Enhanced usability and member experience by streamlining specialty pharmacy searches.
-Supported filtering Mail and Retail (Specialty) pharmacies based on member filter search criteria.
-HyVee Transfer Prescriptions Application:
+  public readonly plPharmacyDetail$ = this.store.select(
+    PlPharmacyDetailFeature.selectPlPharmacyDetail
+  );
 
-Designed and implemented a new app for members to transfer prescriptions from out-of-network pharmacies.
-Focused on simplifying the process for users, enhancing their experience, and addressing a critical user need.
-Find Pharmacy Services Migration to i90:
+  public readonly loading$ = this.store.select(
+    PlPharmacyDetailFeature.selectLoading
+  );
 
-Migrated all backend services for Find Pharmacy functionality to i90.
-Improved backend performance and scalability, supporting seamless service delivery for clients.
-Throughout the Year
-Production Release Management:
+  public readonly error$ = this.store.select(
+    PlPharmacyDetailFeature.selectError
+  );
 
-Effectively managed production deployments across all key projects.
-Conducted comprehensive end-to-end testing to ensure bug-free delivery.
-Collaborated with stakeholders to streamline the release process, minimizing downtime and ensuring reliability.
-Client Support for BCBSMA, Aetna, and Molina:
+  public editPlPharmacyDetail(plPharmacyDetail: string): void {
+    this.store.dispatch(
+      PlPharmacyDetailActions.editPlPharmacyDetail({ plPharmacyDetail })
+    );
+  }
+}
 
-Provided comprehensive support for clients to consume API services.
-Resolved queries and ensured smooth integration of services into client systems.
-Strengthened client relationships through timely and effective communication.
-Team Leadership as a Tech Lead:
+reducer.ts
 
-Guided team members to understand project requirements effectively and on time.
-Ensured alignment of team efforts with organizational objectives.
-Supported developers in overcoming challenges and maintaining productivity throughout the development lifecycle.
-Heart at Behaviors
-Put People First:
-I maintained a people-first approach, supporting my team through active mentoring and fostering an inclusive work environment.
+import { ReportableError } from '@digital-blocks/angular/core/util/error-handler';
+import { ActionReducer, createFeature, createReducer, on } from '@ngrx/store';
 
-Rise to Challenges:
-I tackled complex technical challenges, such as the seamless migration of APIs to SPIL2, ensuring minimal downtime and robust performance.
+import { PlPharmacyDetailActions } from './pl-pharmacy-detail.actions';
 
-Join Forces:
-Collaborated with diverse teams on cross-functional projects, particularly during the onboarding of Aetna Retail Guest Flow, achieving shared success.
+export const PL_PHARMACY_DETAIL_FEATURE_KEY = 'pl-pharmacy-detail';
 
-Create Simplicity:
-Simplified complex workflows, such as the HyVee prescription transfer process, to enhance user convenience and satisfaction.
+export interface PlPharmacyDetailState {
+  plPharmacyDetail: string;
+  loading: boolean;
+  error: ReportableError | undefined;
+}
 
-Inspire Trust:
-Through transparency and accountability, I strengthened relationships with stakeholders and ensured high-quality delivery of projects.
+export const initialPlPharmacyDetailState: PlPharmacyDetailState = {
+  plPharmacyDetail: '',
+  loading: false,
+  error: undefined
+};
 
+const reducer: ActionReducer<PlPharmacyDetailState> = createReducer(
+  initialPlPharmacyDetailState,
+  on(PlPharmacyDetailActions.getPlPharmacyDetail, (state) => ({
+    ...state,
+    loading: true
+  })),
+  on(PlPharmacyDetailActions.getPlPharmacyDetailSuccess, (state) => ({
+    ...state,
+    loading: false
+  })),
+  on(
+    PlPharmacyDetailActions.getPlPharmacyDetailFailure,
+    (state, { error }) => ({
+      ...state,
+      loading: false,
+      error
+    })
+  ),
+  on(
+    PlPharmacyDetailActions.editPlPharmacyDetail,
+    (state, { plPharmacyDetail }) => ({
+      ...state,
+      plPharmacyDetail
+    })
+  )
+);
+
+export const PlPharmacyDetailFeature = createFeature({
+  name: PL_PHARMACY_DETAIL_FEATURE_KEY,
+  reducer
+});
+
+
+component.ts
+
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { PlPharmacyDetailStore } from './pl-pharmacy-detail.store';
+
+@Component({
+  selector: 'lib-pl-pharmacy-detail',
+  imports: [CommonModule],
+  templateUrl: 'pl-pharmacy-detail.component.html',
+  styleUrls: ['pl-pharmacy-detail.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [PlPharmacyDetailStore],
+  host: { ngSkipHydration: 'true' }
+})
+export class PlPharmacyDetailComponent {}
+
+store.ts
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PlPharmacyDetailStore {}
+
+
+mock response 
+
+[
+                { "field1": "testValue", "field2": "test2"}]
