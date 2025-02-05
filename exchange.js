7b66d8bd15ce3ab9d4c178780f1f07e822866cf8
Syed Hussain <aftab.jersey@gmail.com>
2025-02-05 14:36:48 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,12 +1,34 @@
-import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import { Injectable, inject } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
-import { PlPharmacyDetailFeature } from './+state';
+import { PharmacyDetail } from '../pl-pharmacy-detail.types';
 
-@NgModule({
-  imports: [
-    // EffectsModule.forFeature([PlPharmacyDetailEffects]),
-    StoreModule.forFeature(PlPharmacyDetailFeature)
-  ]
-})
-export class PlPharmacyDetailModule {}
+import { PlPharmacyDetailActions } from './pl-pharmacy-detail.actions';
+import { PlPharmacyDetailFeature } from './pl-pharmacy-detail.reducer';
+
+@Injectable({ providedIn: 'root' })
+export class PlPharmacyDetailFacade {
+  protected readonly store = inject(Store);
+
+  public readonly selectedPharmacy$: Observable<PharmacyDetail | null> =
+    this.store.pipe(select(PlPharmacyDetailFeature.selectSelectedPharmacy));
+
+  public readonly loading$ = this.store.select(
+    PlPharmacyDetailFeature.selectLoading
+  );
+
+  public readonly error$ = this.store.select(
+    PlPharmacyDetailFeature.selectError
+  );
+
+  public setSelectedPharmacy(pharmacy: PharmacyDetail): void {
+    this.store.dispatch(
+      PlPharmacyDetailActions.setSelectedPharmacy({ pharmacy })
+    );
+  }
+
+  clearSelectedPharmacy(): void {
+    this.store.dispatch(PlPharmacyDetailActions.clearSelectedPharmacy());
+  }
+}
