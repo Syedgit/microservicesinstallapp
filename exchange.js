977edeb00e022731789be809f191eda7408ff7a2
Syed Hussain <aftab.jersey@gmail.com>
2025-02-05 12:37:28 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,86 +1,163 @@
-Search html
+pharmacy-detail action
 
-<ps-tabs>
-      <ps-tab label="Retail" selected>
-        <div
-          class="pl-pharmacy-retail-tab-container"
-          *ngIf="
-            (plPharmacySearchStore.selectRetailFindPharmacy$ | async) !==
-              undefined &&
-            (plPharmacySearchStore.selectRetailFindPharmacy$ | async)?.length
-          ">
-          <p class="no-of-pharmacy-text">
-            Showing {{ retailFindPharmacyDisplayed.length }} results near
-            {{ selectedAddress?.postalCode }}
-          </p>
-          <ps-divider is-decorative="true"></ps-divider>
-          <div class="pharmacy-retail-scrollable-block">
-            <div
-              class="pl-pharmacy-retail-pharmacy-tile"
-              *ngFor="
-                let pharmacyData of retailFindPharmacyDisplayed;
-                let i = index
-              ">
-              <div class="pharmacy-tile-left-block">
-                <h2 class="pharmacy-name">
-                  {{ pharmacyData.pharmacyName | titlecase }}
-                </h2>
-                <div class="pharmacy-address-block">
-                  <p class="pharmacy-address-line">
-                    {{ pharmacyData.formattedAddresses?.line | titlecase }}
-                  </p>
-                  <p class="pharmacy-city-state-zip">
-                    {{ pharmacyData.formattedAddresses?.city | titlecase }},
-                    {{ pharmacyData.formattedAddresses?.state }}
-                    {{ pharmacyData.formattedAddresses?.postalCode }}
-                  </p>
-                </div>
-                <div
-                  class="info-tag-block"
-                  *ngIf="
-                    pharmacyData?.ninetyDaySupplyNetwork ||
-                    pharmacyData?.specialtyInd === 'Y'
-                  ">
-                  <ps-info-tag *ngIf="pharmacyData?.ninetyDaySupplyNetwork">{{
-                    staticContent.networkSupplyInfoTag
-                  }}</ps-info-tag>
-                  <ps-info-tag *ngIf="pharmacyData?.specialtyInd === 'Y'">{{
-                    staticContent.specialtyInfoTag
-                  }}</ps-info-tag>
-                </div>
-                <div class="pharmacy-detail-link-block">
-                  <ps-link
-                    class="pharmacy-details-link"
-                    link-href="javascript:void(0)"
-                    (click)="loadPharmacyDetail(i)"
-                    (keyPress)="loadPharmacyDetail(i)"
-                    variant="standalone"
-                    subtype="default"
-                    weight="strong"
-                    >Pharmacy details</ps-link
-                  >
-                </div>
-              </div>
+import { createActionGroup, props } from '@ngrx/store';
+import { PharmacyDetail } from './pharmacy-detail.interface';
 
-search component.ts 
+export const PlPharmacyDetailActions = createActionGroup({
+  source: 'PharmacyDetail',
+  events: {
+    'Set Selected Pharmacy': props<{ pharmacy: PharmacyDetail }>(),
+    'Clear Selected Pharmacy': props<{}>()
+  }
+});
 
-export class PlPharmacySearchComponent implements OnInit {
-  @Input() public staticContent = {
-    networkSupplyInfoTag: '',
-    specialtyInfoTag: ''
-  };
-  public findPharmacyRequest = {};
-  public isDefaultAddressSearch = true;
-  public retailFindPharmacyFromStore: IFormattedPharmaciesResponse[] = [];
-  public retailFindPharmacyDisplayed: IFormattedPharmaciesResponse[] = [];
-  public retailPharmacyShowMoreCount = 0;
-  public selectedAddress: Address = {
-    formattedAddress: ''
-  };
-  public pharmacyDetailIndex = 0;
 
- public loadPharmacyDetail(clickedPharmacyIndex: number): void {
-    this.pharmacyDetailIndex = clickedPharmacyIndex;
+Pharmacy-Detail Reducer
+
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { PlPharmacyDetailActions } from './pharmacy-detail.actions';
+import { PharmacyDetail } from './pharmacy-detail.interface';
+
+export interface PharmacyDetailState {
+  selectedPharmacy: PharmacyDetail | null;
+}
+
+export const initialState: PharmacyDetailState = {
+  selectedPharmacy: null
+};
+
+export const pharmacyDetailReducer = createReducer(
+  initialState,
+  on(PlPharmacyDetailActions.setSelectedPharmacy, (state, { pharmacy }) => ({
+    ...state,
+    selectedPharmacy: pharmacy
+  })),
+  on(PlPharmacyDetailActions.clearSelectedPharmacy, (state) => ({
+    ...state,
+    selectedPharmacy: null
+  }))
+);
+
+export const PharmacyDetailFeature = createFeature({
+  name: 'pharmacyDetail',
+  reducer: pharmacyDetailReducer
+});
+
+Pharmacy Detail Facade
+
+import { Injectable } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { PharmacyDetailState, PharmacyDetailFeature } from './pharmacy-detail.reducer';
+import { PlPharmacyDetailActions } from './pharmacy-detail.actions';
+import { PharmacyDetail } from './pharmacy-detail.interface';
+
+@Injectable({ providedIn: 'root' })
+export class PharmacyDetailFacade {
+  constructor(private readonly store: Store<PharmacyDetailState>) {}
+
+  // Observable for selected pharmacy
+  selectedPharmacy$: Observable<PharmacyDetail | null> = this.store.pipe(
+    select(PharmacyDetailFeature.selectSelectedPharmacy)
+  );
+
+  // When a user clicks "View Details", save the pharmacy to state
+  setSelectedPharmacy(pharmacy: PharmacyDetail): void {
+    this.store.dispatch(PlPharmacyDetailActions.setSelectedPharmacy({ pharmacy }));
   }
 
+  // Clear pharmacy data when leaving the page
+  clearSelectedPharmacy(): void {
+    this.store.dispatch(PlPharmacyDetailActions.clearSelectedPharmacy());
+  }
 }
+
+Search Page Component
+
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PharmacyDetailFacade } from '../store/pharmacy-detail.facade';
+import { PharmacyDetail } from '../store/pharmacy-detail.interface';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent {
+  pharmacies: PharmacyDetail[] = [
+    {
+      pharmacyNumber: '001',
+      pharmacyName: 'ABC Pharmacy',
+      nationalProviderId: '12345',
+      addresses: { city: 'New York', state: 'NY', postalCode: '10001', phoneNumber: '123-456-7890' },
+      distance: 1.2
+    },
+    {
+      pharmacyNumber: '002',
+      pharmacyName: 'XYZ Pharmacy',
+      nationalProviderId: '67890',
+      addresses: { city: 'Los Angeles', state: 'CA', postalCode: '90001', phoneNumber: '987-654-3210' },
+      distance: 2.5
+    }
+  ];
+
+  constructor(
+    private pharmacyDetailFacade: PharmacyDetailFacade,
+    private router: Router
+  ) {}
+
+  viewPharmacyDetails(pharmacy: PharmacyDetail): void {
+    // Save selected pharmacy to store
+    this.pharmacyDetailFacade.setSelectedPharmacy(pharmacy);
+
+    // Navigate to the pharmacy details page
+    this.router.navigate(['/pharmacy/pharmacy-locator/pharmacyDetails', pharmacy.pharmacyNumber]);
+  }
+}
+
+Search Page Html 
+
+<div *ngFor="let pharmacy of pharmacies">
+  <h3>{{ pharmacy.pharmacyName }}</h3>
+  <p>{{ pharmacy.addresses.city }}, {{ pharmacy.addresses.state }}</p>
+  <p>Distance: {{ pharmacy.distance }} miles</p>
+  <button (click)="viewPharmacyDetails(pharmacy)">View Details</button>
+</div>
+
+
+Pharmacy Detail Component
+
+import { Component, OnInit } from '@angular/core';
+import { PharmacyDetailFacade } from '../store/pharmacy-detail.facade';
+import { PharmacyDetail } from '../store/pharmacy-detail.interface';
+
+@Component({
+  selector: 'app-pharmacy-detail',
+  templateUrl: './pharmacy-detail.component.html',
+  styleUrls: ['./pharmacy-detail.component.scss']
+})
+export class PharmacyDetailComponent implements OnInit {
+  selectedPharmacy: PharmacyDetail | null = null;
+
+  constructor(private pharmacyDetailFacade: PharmacyDetailFacade) {}
+
+  ngOnInit(): void {
+    this.pharmacyDetailFacade.selectedPharmacy$.subscribe((pharmacy) => {
+      this.selectedPharmacy = pharmacy;
+    });
+  }
+}
+
+Pharmacy Detail html
+
+<div *ngIf="selectedPharmacy; else noPharmacy">
+  <h2>{{ selectedPharmacy.pharmacyName }}</h2>
+  <p>Address: {{ selectedPharmacy.addresses.city }}, {{ selectedPharmacy.addresses.state }}</p>
+  <p>Phone: {{ selectedPharmacy.addresses.phoneNumber }}</p>
+  <p>Distance: {{ selectedPharmacy.distance }} miles</p>
+</div>
+
+<ng-template #noPharmacy>
+  <p>No pharmacy selected. Go back to search.</p>
+</ng-template>
