1cd3e7f5bb110d8e9aa5b9c309d56add0181009f
Syed Hussain <aftab.jersey@gmail.com>
2025-02-07 12:05:01 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,200 +1,53 @@
-actions
+<div class="pharmacy-detail-container" *ngIf="selectedPharmacy">
+  <h2>{{ selectedPharmacy.pharmacyName }}</h2>
 
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+  <div class="pharmacy-address">
+    <p *ngIf="selectedPharmacy.addresses">
+      {{ selectedPharmacy.addresses.line.join(', ') }}, 
+      {{ selectedPharmacy.addresses.city }}, 
+      {{ selectedPharmacy.addresses.state }} - 
+      {{ selectedPharmacy.addresses.postalCode }}
+    </p>
+    <p *ngIf="selectedPharmacy.addresses.phoneNumber">
+      📞 Phone: {{ selectedPharmacy.addresses.phoneNumber }}
+    </p>
+  </div>
 
-import { PharmacyDetail } from '../pl-pharmacy-detail.types';
+  <div class="pharmacy-hours" *ngIf="selectedPharmacy.pharmacyHours">
+    <h3>Operating Hours</h3>
+    <ul>
+      <li *ngFor="let hour of selectedPharmacy.pharmacyHours.dayHours">
+        <strong>{{ hour.day | titlecase }}:</strong> {{ hour.hours }}
+      </li>
+    </ul>
+  </div>
 
-export const PlPharmacyDetailActions = createActionGroup({
-  source: 'PlPharmacyDetail',
-  events: {
-    'Set Selected Pharmacy': props<{ selectedPharmacy: PharmacyDetail }>(),
-    'Clear Selected Pharmacy': emptyProps()
-  }
-});
+  <div class="pharmacy-features">
+    <h3>Features</h3>
+    <ul>
+      <li *ngIf="selectedPharmacy.open24hours">🏪 Open 24 Hours</li>
+      <li *ngIf="selectedPharmacy.nintyDayRetail">💊 90-Day Supply Available</li>
+      <li *ngIf="selectedPharmacy.specialtyInd === 'Y'">⭐ Specialty Pharmacy</li>
+      <li *ngIf="selectedPharmacy.prefPharmInd">🏆 Preferred Pharmacy</li>
+      <li *ngIf="selectedPharmacy.physicalDisabilityInd === 'Y'">♿ Accessible</li>
+    </ul>
+  </div>
 
-Facade
+  <div class="pharmacy-map">
+    <h3>Location</h3>
+    <iframe 
+      width="100%" 
+      height="250" 
+      frameborder="0" 
+      style="border:0"
+      [src]="'https://www.google.com/maps/embed/v1/place?key=YOUR_GOOGLE_MAPS_API_KEY&q=' 
+      + selectedPharmacy.latitude + ',' + selectedPharmacy.longitude" 
+      allowfullscreen>
+    </iframe>
+  </div>
 
-import { Injectable, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-
-import { PharmacyDetail } from '../pl-pharmacy-detail.types';
-
-import { PlPharmacyDetailActions } from './pl-pharmacy-detail.actions';
-import { PlPharmacyDetailFeature } from './pl-pharmacy-detail.reducer';
-
-@Injectable({ providedIn: 'root' })
-export class PlPharmacyDetailFacade {
-  protected readonly store = inject(Store);
-
-  public readonly selectedPharmacy$: Observable<PharmacyDetail | null> =
-    this.store.select(PlPharmacyDetailFeature.selectSelectedPharmacy);
-
-  public readonly loading$ = this.store.select(
-    PlPharmacyDetailFeature.selectLoading
-  );
-
-  public readonly error$ = this.store.select(
-    PlPharmacyDetailFeature.selectError
-  );
-
-  public setSelectedPharmacy(selectedPharmacy: PharmacyDetail): void {
-    this.store.dispatch(
-      PlPharmacyDetailActions.setSelectedPharmacy({ selectedPharmacy })
-    );
-  }
-
-  clearSelectedPharmacy(): void {
-    this.store.dispatch(PlPharmacyDetailActions.clearSelectedPharmacy());
-  }
-}
-
-reducer 
-
-import { ReportableError } from '@digital-blocks/angular/core/util/error-handler';
-import { ActionReducer, createFeature, createReducer, on } from '@ngrx/store';
-
-import { PharmacyDetail } from '../pl-pharmacy-detail.types';
-
-import { PlPharmacyDetailActions } from './pl-pharmacy-detail.actions';
-
-export const PL_PHARMACY_DETAIL_FEATURE_KEY = 'pl-pharmacy-detail';
-
-export interface PlPharmacyDetailState {
-  selectedPharmacy: PharmacyDetail | null;
-  loading: boolean;
-  error: ReportableError | undefined;
-}
-
-export const initialPlPharmacyDetailState: PlPharmacyDetailState = {
-  selectedPharmacy: null,
-  loading: false,
-  error: undefined
-};
-
-const reducer: ActionReducer<PlPharmacyDetailState> = createReducer(
-  initialPlPharmacyDetailState,
-  on(
-    PlPharmacyDetailActions.setSelectedPharmacy,
-    (state, { selectedPharmacy }) => ({
-      ...state,
-      selectedPharmacy,
-      loading: true
-    })
-  ),
-  on(PlPharmacyDetailActions.clearSelectedPharmacy, (state) => ({
-    ...state,
-    selectedPharmacy: null,
-    loading: false
-  }))
-);
-
-export const PlPharmacyDetailFeature = createFeature({
-  name: PL_PHARMACY_DETAIL_FEATURE_KEY,
-  reducer
-});
-
-
-Effects
-
-import { Injectable, inject } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { tap } from 'rxjs/operators';
-
-import { PlPharmacyDetailActions } from './pl-pharmacy-detail.actions';
-
-@Injectable()
-export class PlPharmacyDetailEffects {
-  private readonly actions$ = inject(Actions);
-  storeSelectedPharmacy$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(PlPharmacyDetailActions.setSelectedPharmacy),
-        tap(({ selectedPharmacy }) => {
-          sessionStorage.setItem(
-            'selectedPharmacy',
-            JSON.stringify(selectedPharmacy)
-          );
-        })
-      ),
-    { dispatch: false }
-  );
-
-  clearPharmacyOnLeave$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(PlPharmacyDetailActions.clearSelectedPharmacy),
-        tap(() => {
-          sessionStorage.removeItem('selectedPharmacy');
-        })
-      ),
-    { dispatch: false }
-  );
-}
-
-pl-Pharmacy-detail.store.ts 
-
-import { Injectable, inject } from '@angular/core';
-import {
-  PlPharmacyDetailFacade,
-  PharmacyDetail
-} from '@digital-blocks/angular/pharmacy/pharmacy-locator/store/pl-pharmacy-detail';
-import { Observable } from 'rxjs';
-
-@Injectable()
-export class PlPharmacyDetailStore {
-  private readonly pharmacyDetailFacade = inject(PlPharmacyDetailFacade);
-
-  public readonly selectedPharmacy$: Observable<PharmacyDetail | null> =
-    this.pharmacyDetailFacade.selectedPharmacy$;
-}
-
-
-Component.ts
-
-import { CommonModule } from '@angular/common';
-import {
-  Component,
-  OnInit,
-  CUSTOM_ELEMENTS_SCHEMA,
-  inject
-} from '@angular/core';
-import {
-  PharmacyDetail,
-  PlPharmacyDetailFacade
-} from '@digital-blocks/angular/pharmacy/pharmacy-locator/store/pl-pharmacy-detail';
-
-import { PlPharmacyDetailStore } from './pl-pharmacy-detail.store';
-
-@Component({
-  selector: 'lib-pl-pharmacy-detail',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: 'pl-pharmacy-detail.component.html',
-  styleUrls: ['pl-pharmacy-detail.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [PlPharmacyDetailStore],
-  host: { ngSkipHydration: 'true' }
-})
-export class PlPharmacyDetailComponent implements OnInit {
-  public selectedPharmacy = {};
-  protected readonly pharmacyDetailFacade = inject(PlPharmacyDetailFacade);
-  selectedPharmacy$ = this.pharmacyDetailFacade.selectedPharmacy$;
-  constructor(private readonly pharmacyDetailStore: PlPharmacyDetailStore) {}
-
-  ngOnInit(): void {
-    this.pharmacyDetailStore.selectedPharmacy$.subscribe(
-      (data: PharmacyDetail | null) => {
-        if (data?.pharmacyNumber) {
-          this.selectedPharmacy = data;
-        }
-      }
-    );
-    // this.selectedPharmacy$.subscribe((selectedPharmacy) => {
-    //   if (selectedPharmacy) {
-    //     this.selectedPharmacy = selectedPharmacy;
-    //   }
-    // });
-  }
-}
+  <a *ngIf="selectedPharmacy.websiteURL" [href]="selectedPharmacy.websiteURL" target="_blank">
+    🌐 Visit Website
+  </a>
+</div>
 
