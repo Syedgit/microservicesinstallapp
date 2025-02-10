5474af7a6a8e5f05bb1d0a17b950dc75a8ff5623
Syed Hussain <aftab.jersey@gmail.com>
2025-02-10 12:11:10 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,22 +1,36 @@
-bingMaps directive
+directive
 
-
-import { Directive, ElementRef, Input, AfterViewInit, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  PLATFORM_ID,
+  Inject,
+  AfterViewInit
+} from '@angular/core';
 import { ConfigFacade } from '@digital-blocks/angular/core/store/config';
-
-declare const Microsoft: any;
 
 @Directive({
   selector: '[utilBingMaps]'
 })
 export class BingMapsDirective implements AfterViewInit {
   @Input() options: { center: { lat: number; lng: number }, pushpins: { lat: number; lng: number }[] } | null = null;
-  private configFacade = inject(ConfigFacade);
-  private bingApiKey!: string;
 
-  constructor(private el: ElementRef) {}
+  private bingApiKey!: string;
+  private isBrowser: boolean;
+
+  constructor(
+    private el: ElementRef,
+    private configFacade: ConfigFacade,
+    @Inject(PLATFORM_ID) platformId: object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   async ngAfterViewInit(): Promise<void> {
+    if (!this.isBrowser) return; // ✅ Prevent SSR from running Microsoft Maps
+
     this.bingApiKey = await this.configFacade.bingMapsAPIKey$.toPromise();
     this.loadBingMap();
   }
@@ -24,34 +38,46 @@ export class BingMapsDirective implements AfterViewInit {
   private loadBingMap(): void {
     if (!this.options) return;
 
-    const script = document.createElement('script');
-    script.src = `https://www.bing.com/api/maps/mapcontrol?callback=__bingMapsLoaded&key=${this.bingApiKey}`;
-    script.async = true;
-    script.defer = true;
+    const scriptId = 'bing-maps-script';
+    if (!document.getElementById(scriptId)) {
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.src = `https://www.bing.com/api/maps/mapcontrol?callback=__bingMapsLoaded&key=${this.bingApiKey}`;
+      script.async = true;
+      script.defer = true;
 
-    window['__bingMapsLoaded'] = () => {
-      const map = new Microsoft.Maps.Map(this.el.nativeElement, {
-        credentials: this.bingApiKey,
-        center: new Microsoft.Maps.Location(this.options.center.lat, this.options.center.lng),
-        zoom: 12
-      });
+      window['__bingMapsLoaded'] = () => {
+        this.initializeMap();
+      };
 
-      this.options.pushpins.forEach((pin) => {
-        const location = new Microsoft.Maps.Location(pin.lat, pin.lng);
-        const pushpin = new Microsoft.Maps.Pushpin(location);
-        map.entities.push(pushpin);
-      });
-    };
+      document.body.appendChild(script);
+    } else {
+      this.initializeMap();
+    }
+  }
 
-    document.body.appendChild(script);
+  private initializeMap(): void {
+    if (!this.options) return;
+
+    const map = new Microsoft.Maps.Map(this.el.nativeElement, {
+      credentials: this.bingApiKey,
+      center: new Microsoft.Maps.Location(this.options.center.lat, this.options.center.lng),
+      zoom: 12
+    });
+
+    this.options.pushpins.forEach((pin) => {
+      const location = new Microsoft.Maps.Location(pin.lat, pin.lng);
+      const pushpin = new Microsoft.Maps.Pushpin(location);
+      map.entities.push(pushpin);
+    });
   }
 }
 
 
-pl-pharmacy-map
+pl-pharmacy-map component
 
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Input, Inject, PLATFORM_ID } from '@angular/core';
 import { BingMapsDirective } from './bing-maps.directive';
 import { PharmacyDetail } from '@digital-blocks/angular/pharmacy/pharmacy-locator/store/pl-pharmacy-detail';
 
@@ -65,27 +91,30 @@ import { PharmacyDetail } from '@digital-blocks/angular/pharmacy/pharmacy-locato
 export class PlPharmacyMapComponent {
   @Input() pharmacies: PharmacyDetail[] = [];
 
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) platformId: object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
   get mapOptions() {
-    return {
-      center: this.pharmacies.length ? { lat: this.pharmacies[0].latitude, lng: this.pharmacies[0].longitude } : { lat: 0, lng: 0 },
-      pushpins: this.pharmacies.map((pharmacy) => ({
-        lat: pharmacy.latitude,
-        lng: pharmacy.longitude
-      }))
-    };
+    return this.isBrowser
+      ? {
+          center: this.pharmacies.length
+            ? { lat: this.pharmacies[0].latitude, lng: this.pharmacies[0].longitude }
+            : { lat: 0, lng: 0 },
+          pushpins: this.pharmacies.map((pharmacy) => ({
+            lat: pharmacy.latitude,
+            lng: pharmacy.longitude
+          }))
+        }
+      : null;
   }
 }
 
-pl-pharmacy-map html
 
-<div utilBingMaps class="pharmacy-map" [options]="mapOptions"></div>
+map html
 
-
-
-using directive
-
-
-<lib-pl-pharmacy-map [pharmacies]="[selectedPharmacy]"></lib-pl-pharmacy-map>
-
-
-<lib-pl-pharmacy-map [pharmacies]="pharmacies"></lib-pl-pharmacy-map>
+@if (mapOptions) {
+  <div utilBingMaps class="pharmacy-map" [options]="mapOptions"></div>
+}
