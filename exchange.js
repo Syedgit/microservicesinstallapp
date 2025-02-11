fa09591fd385d0a8b7b4c636146f0f4047550603
Syed Hussain <aftab.jersey@gmail.com>
2025-02-11 16:12:40 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,130 +1,65 @@
-import { CommonModule } from '@angular/common';
-import {
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-  DestroyRef,
-  EventEmitter,
-  inject,
-  Input,
-  OnInit,
-  Output
-} from '@angular/core';
-import { BehaviorSubject, combineLatest, of } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+public ngOnInit(): void {
+  // Mock multiple pharmacies data (for search page scenario)
+  this.pharmacies = [
+    {
+      id: '1',
+      name: 'Pharmacy One',
+      latitude: 40.73061,
+      longitude: -73.935242
+    },
+    {
+      id: '2',
+      name: 'Pharmacy Two',
+      latitude: 40.7128,
+      longitude: -74.006
+    }
+  ];
 
-import { GoogleMapsDirective } from '@digital-blocks/angular/core/util/directives';
-import { Pharmacy } from '../pl-pharmacy.types';
-import { PlPharmacyMapStore } from './pl-pharmacy-map.store';
+  // Mock single pharmacy data (for pharmacy detail scenario)
+  this.selectedPharmacy = {
+    id: '1',
+    name: 'Pharmacy One',
+    latitude: 40.73061,
+    longitude: -73.935242
+  };
 
-@Component({
-  selector: 'lib-pl-pharmacy-map',
-  standalone: true,
-  imports: [CommonModule, GoogleMapsDirective],
-  templateUrl: './pl-pharmacy-map.component.html',
-  styleUrls: ['./pl-pharmacy-map.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [PlPharmacyMapStore],
-  host: { ngSkipHydration: 'true' }
-})
-export class PlPharmacyMapComponent implements OnInit {
-  @Input() public googleURL!: string;
-  @Input() public isModal!: boolean;
-  @Input() public externalMapHref!: string;
-  @Input() public pharmacies$!: BehaviorSubject<Pharmacy[] | undefined>;
-  @Input() public selectedPharmacy$!: BehaviorSubject<Pharmacy | undefined>; // Handle Selected Pharmacy
-
-  @Output() public createPlatFormSpecificURL = new EventEmitter<{
-    baseURL: string;
-    lat: string;
-    long: string;
-  }>();
-
-  private readonly destroyRef = inject(DestroyRef);
-
-  pharmacies: Pharmacy[] | undefined;
-  selectedPharmacy: Pharmacy | undefined;
-  options:
-    | {
-        center: { lat: number; lng: number };
-        zoom?: number;
-        markers: { position: { lat: number; lng: number } }[];
-      }
-    | undefined;
-
-  public ngOnInit(): void {
-    combineLatest([this.pharmacies$ ?? of([]), this.selectedPharmacy$ ?? of(undefined)])
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(([pharmacies, selectedPharmacy]) => {
-        this.pharmacies = pharmacies;
-        this.selectedPharmacy = selectedPharmacy;
-
-        if (selectedPharmacy) {
-          // If a selected pharmacy exists, prioritize it
-          this.options = {
-            center: {
-              lat: Number(selectedPharmacy.latitude),
-              lng: Number(selectedPharmacy.longitude)
-            },
-            zoom: 15, // More zoom for a single location
-            markers: [
-              {
-                position: {
-                  lat: Number(selectedPharmacy.latitude),
-                  lng: Number(selectedPharmacy.longitude)
-                }
-              }
-            ]
-          };
-        } else if (pharmacies.length > 0) {
-          // Otherwise, load multiple pharmacies from search
-          this.options = {
-            center: {
-              lat: Number(pharmacies[0].latitude),
-              lng: Number(pharmacies[0].longitude)
-            },
-            zoom: 12,
-            markers: pharmacies.map((pharmacy) => ({
-              position: {
-                lat: Number(pharmacy.latitude),
-                lng: Number(pharmacy.longitude)
-              }
-            }))
-          };
+  // Logic to set options based on selectedPharmacy or multiple pharmacies
+  if (this.selectedPharmacy) {
+    this.options = {
+      center: {
+        lat: this.selectedPharmacy.latitude,
+        lng: this.selectedPharmacy.longitude
+      },
+      zoom: 15,
+      markers: [
+        {
+          position: {
+            lat: this.selectedPharmacy.latitude,
+            lng: this.selectedPharmacy.longitude
+          }
         }
-      });
+      ]
+    };
+  } else if (this.pharmacies.length > 0) {
+    this.options = {
+      center: {
+        lat: this.pharmacies[0].latitude,
+        lng: this.pharmacies[0].longitude
+      },
+      zoom: 12,
+      markers: this.pharmacies.map((pharmacy) => ({
+        position: {
+          lat: pharmacy.latitude,
+          lng: pharmacy.longitude
+        }
+      }))
+    };
+  } else {
+    // Fallback default location
+    this.options = {
+      center: { lat: 40.7128, lng: -74.006 },
+      zoom: 10,
+      markers: []
+    };
   }
 }
-
-
-
-map html
-
-@if (options) {
-  <div class="map-container">
-    <h3>
-      {{ selectedPharmacy ? 'Pharmacy Location' : 'Nearby Pharmacies' }}
-    </h3>
-    <div utilGoogleMaps [options]="options"></div>
-  </div>
-} @else {
-  <util-spinning-loader [loading]="true" />
-}
-
-
-pharmacy search 
-
-<lib-pl-pharmacy-map
-  [googleURL]="googleURL"
-  [isModal]="false"
-  [selectedPharmacy$]="selectedPharmacy$"
-  [externalMapHref]="externalMapHref"
-  (createPlatFormSpecificURL)="createPlatFormSpecificURL.emit($event)" />
-
-
-  <lib-pl-pharmacy-map
-  [googleURL]="googleURL"
-  [isModal]="false"
-  [pharmacies$]="pharmacies$"
-  [externalMapHref]="externalMapHref"
-  (createPlatFormSpecificURL)="createPlatFormSpecificURL.emit($event)" />
-
