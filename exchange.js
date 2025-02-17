77011ee6ce1162a421ec8e34b65dd2fa96136a4e
Syed Hussain <aftab.jersey@gmail.com>
2025-02-17 13:09:56 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,10 +1,11 @@
 index IN ("digital_apigee", "ed_pharmacy_services") CVSEVENT=EXIT experienceId="0e7c09ae-108b-" appName="teamapp_benefits_1662658221"
+| fillnull value="UNKNOWN" statusCde
 | bin _time span=1s 
-| stats count as TPS, count(eval(statusCde="SUCCESS")) as Successes, count(eval(statusCde="FAILURE")) as Failures, count(eval(statusCde="WARNING")) as Warnings by _time
-| eventstats max(TPS) as Peak_TPS, avg(TPS) as Average_TPS
+| stats count as TPS by _time, statusCde
+| eventstats max(TPS) as Peak_TPS, avg(TPS) as Average_TPS by statusCde
 | bin _time span=1h
-| stats sum(TPS) as Transactions_Per_Hour, max(TPS) as Peak_Transactions_Per_Hour, sum(Successes) as Success_Transactions, sum(Failures) as Failed_Transactions, sum(Warnings) as Warning_Transactions by _time
-| eventstats max(Peak_Transactions_Per_Hour) as Peak_Usage, sum(Transactions_Per_Hour) as Total_Transactions
-| stats first(_time) as Peak_Hour, last(_time) as Typical_Hour, max(Peak_Usage) as Peak_Usage, max(Peak_TPS) as Peak_TPS, avg(Average_TPS) as Average_TPS, sum(Total_Transactions) as Total_Transactions, sum(Success_Transactions) as Total_Success, sum(Failed_Transactions) as Total_Failures, sum(Warning_Transactions) as Total_Warnings
-| eval Peak_Hour=strftime(Peak_Hour, "%H:00 - %H:59"), 
-       Typical_Hour=strftime(Typical_Hour, "%H:00 - %H:59")
+| stats sum(TPS) as Transactions_Per_Hour, max(TPS) as Peak_Transactions_Per_Hour by _time, statusCde
+| eventstats max(Peak_Transactions_Per_Hour) as Peak_Usage by statusCde
+| eval Hour=strftime(_time, "%H") 
+| stats max(Peak_TPS) as Peak_TPS, avg(Average_TPS) as Average_TPS, max(Peak_Usage) as Peak_Usage, max(Transactions_Per_Hour) as "#Transactions_Per_Hour", first(_time) as Peak_Hours, last(_time) as Typical_Hours by statusCde
+| eval Peak_Hours=strftime(Peak_Hours, "%I %p"), Typical_Hours=strftime(Typical_Hours, "%I %p")
