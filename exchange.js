1dd8d535d800c476c1ac2a52f651ef4aaf3d13a6
Syed Hussain <aftab.jersey@gmail.com>
2025-02-17 12:47:32 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -5,7 +5,12 @@ index IN ("digital_apigee", "ed_pharmacy_services") CVSEVENT=EXIT experienceId="
 | eventstats avg(TPS) as Average_TPS max(TPS) as Peak_TPS
 | bin _time span=1h
 | stats sum(TPS) as Transactions_Per_Hour, max(TPS) as Peak_Transactions_Per_Hour, avg(TPS) as Avg_Transactions_Per_Hour by _time
-| sort -Peak_Transactions_Per_Hour
-| eventstats first(_time) as Peak_Hour
-| sort _time
-| eventstats last(_time) as Typical_Hour
+| stats 
+    max(Peak_Transactions_Per_Hour) as Peak_Usage, 
+    avg(Avg_Transactions_Per_Hour) as Average_TPS, 
+    max(Peak_TPS) as Peak_TPS, 
+    sum(Transactions_Per_Hour) as Total_Transactions,
+    first(_time) as Peak_Hour,
+    last(_time) as Typical_Hour
+| eval Peak_Hour=strftime(Peak_Hour, "%H:00 - %H:59"), 
+       Typical_Hour=strftime(Typical_Hour, "%H:00 - %H:59")
