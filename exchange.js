380ace3758bf1c6b341bd6292203575088162d5a
Syed Hussain <aftab.jersey@gmail.com>
2025-02-17 12:42:24 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,10 +1,11 @@
-index IN ("digital_apigee", "ed_pharmacy_services") CVSEVENT=EXIT experienceId="0e7c09ae-108b-" appName="teamapp_benefits_1662658221"| fillnull "~" name,statusCde, statusMsg, jsonPayload.totalApiResponseTime
-| stats count(status) as Volume, count(eval(status="SUCCESS")) as Successes, count(eval(status="WARNING")) as Warnings, count(eval(status="FAILURE")) as Failures, perc75(jsonPayload.totalApiResponseTime) AS RespTime75th, perc50(jsonPayload.totalApiResponseTime) AS RespTime50th, perc90(jsonPayload.totalApiResponseTime) AS RespTime90th, perc95(jsonPayload.totalApiResponseTime) AS RespTime95th, perc99(jsonPayload.totalApiResponseTime) AS RespTime99th ,avg(jsonPayload.totalApiResponseTime) as Average max(jsonPayload.totalApiResponseTime) as max_respTime
-
-
-Peak TPS
-Average TPS
-Peak Hours
-Typical Hours
-Peak Usage
-#transactions/per hour
+index IN ("digital_apigee", "ed_pharmacy_services") CVSEVENT=EXIT experienceId="0e7c09ae-108b-" appName="teamapp_benefits_1662658221"
+| fillnull "~" name, statusCde, statusMsg, jsonPayload.totalApiResponseTime
+| bin _time span=1s 
+| stats count(status) as TPS by _time 
+| eventstats avg(TPS) as Average_TPS max(TPS) as Peak_TPS
+| bin _time span=1h
+| stats sum(TPS) as Transactions_Per_Hour, max(TPS) as Peak_Transactions_Per_Hour, avg(TPS) as Avg_Transactions_Per_Hour by _time
+| sort -Peak_Transactions_Per_Hour
+| eventstats first(_time) as Peak_Hour
+| sort _time
+| eventstats last(_time) as Typical_Hour
