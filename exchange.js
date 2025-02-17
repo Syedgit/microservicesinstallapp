44b5823922c244610a0b4a33694106268ebfce4e
Syed Hussain <aftab.jersey@gmail.com>
2025-02-17 12:38:35 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,86 +1,10 @@
-import { CommonModule } from '@angular/common';
-import {
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-  DestroyRef,
-  EventEmitter,
-  inject,
-  Input,
-  OnInit,
-  Output
-} from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { SpinningLoaderComponent } from '@digital-blocks/angular/core/components';
-import { GoogleMapsDirective } from '@digital-blocks/angular/core/util/directives';
-import {
-  IPharmacyDetails,
-  PlPharmacySearchFacade
-} from '@digital-blocks/angular/pharmacy/pharmacy-locator/store/pl-pharmacy-search';
-import { combineLatest } from 'rxjs';
+index IN ("digital_apigee", "ed_pharmacy_services") CVSEVENT=EXIT experienceId="0e7c09ae-108b-4e5f-97d4-cb117bac1cb4" appName="teamapp_benefits_1662658221"| fillnull "~" name,statusCde, statusMsg, jsonPayload.totalApiResponseTime
+| stats count(status) as Volume, count(eval(status="SUCCESS")) as Successes, count(eval(status="WARNING")) as Warnings, count(eval(status="FAILURE")) as Failures, perc75(jsonPayload.totalApiResponseTime) AS RespTime75th, perc50(jsonPayload.totalApiResponseTime) AS RespTime50th, perc90(jsonPayload.totalApiResponseTime) AS RespTime90th, perc95(jsonPayload.totalApiResponseTime) AS RespTime95th, perc99(jsonPayload.totalApiResponseTime) AS RespTime99th ,avg(jsonPayload.totalApiResponseTime) as Average max(jsonPayload.totalApiResponseTime) as max_respTime
 
-import { PlPharmacyMapStore } from './pl-pharmacy-map.store';
 
-@Component({
-  selector: 'lib-pl-pharmacy-map',
-  standalone: true,
-  imports: [CommonModule, GoogleMapsDirective, SpinningLoaderComponent],
-  templateUrl: './pl-pharmacy-map.component.html',
-  styleUrls: ['./pl-pharmacy-map.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [PlPharmacyMapStore],
-  host: { ngSkipHydration: 'true' }
-})
-export class PlPharmacyMapComponent implements OnInit {
-  @Input() public googleURL!: string;
-  @Input() public isModal!: boolean;
-  @Input() public externalMapHref!: string;
-
-  @Output() public createPlatFormSpecificURL = new EventEmitter<{
-    baseURL: string;
-    lat: string;
-    long: string;
-  }>();
-
-  private readonly destroyRef = inject(DestroyRef);
-  protected readonly plPharmacySearchFacade = inject(PlPharmacySearchFacade);
-
-  pharmacies: IPharmacyDetails[] | undefined;
-  selectedPharmacy: IPharmacyDetails | undefined;
-  options:
-    | {
-        center: { lat: number; lng: number };
-        zoom?: number;
-        markers?: { position: { lat: number; lng: number } }[];
-      }
-    | undefined;
-
-  public ngOnInit(): void {
-    combineLatest([this.plPharmacySearchFacade.retailFindPharmacy$])
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(([pharmacies]) => {
-        this.pharmacies = pharmacies;
-
-        if (pharmacies && pharmacies.length > 0) {
-          const markers = pharmacies.map((pharmacy) => ({
-            position: {
-              lat: Number(pharmacy.latitude.toFixed(6)),
-              lng: Number(pharmacy.longitude.toFixed(6))
-            }
-          }));
-
-          this.options = {
-            center: markers[0].position, // Center on the first pharmacy
-            zoom: 12,
-            markers
-          };
-
-          // Emit first pharmacy's location for directions
-          this.createPlatFormSpecificURL.emit({
-            baseURL: 'https://www.google.com/maps/search/?api=1&query=',
-            lat: markers[0].position.lat.toFixed(6),
-            long: markers[0].position.lng.toFixed(6)
-          });
-        }
-      });
-  }
-}
+Peak TPS
+Average TPS
+Peak Hours
+Typical Hours
+Peak Usage
+#transactions/per hour
