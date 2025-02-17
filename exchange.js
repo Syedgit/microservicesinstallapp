0cfa2ddf38a0892dc062292585fb3b0b3c5b43df
Syed Hussain <aftab.jersey@gmail.com>
2025-02-17 13:12:41 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -2,10 +2,10 @@ index IN ("digital_apigee", "ed_pharmacy_services") CVSEVENT=EXIT experienceId="
 | fillnull value="UNKNOWN" statusCde
 | bin _time span=1s 
 | stats count as TPS by _time, statusCde
-| eventstats max(TPS) as Peak_TPS, avg(TPS) as Average_TPS by statusCde
+| eventstats max(TPS) as Peak_TPS, avg(TPS) as Average_TPS
 | bin _time span=1h
-| stats sum(TPS) as Transactions_Per_Hour, max(TPS) as Peak_Transactions_Per_Hour by _time, statusCde
-| eventstats max(Peak_Transactions_Per_Hour) as Peak_Usage by statusCde
-| eval Hour=strftime(_time, "%H") 
-| stats max(Peak_TPS) as Peak_TPS, avg(Average_TPS) as Average_TPS, max(Peak_Usage) as Peak_Usage, max(Transactions_Per_Hour) as "#Transactions_Per_Hour", first(_time) as Peak_Hours, last(_time) as Typical_Hours by statusCde
-| eval Peak_Hours=strftime(Peak_Hours, "%I %p"), Typical_Hours=strftime(Typical_Hours, "%I %p")
+| stats sum(TPS) as Transactions_Per_Hour by _time, statusCde
+| eventstats max(Transactions_Per_Hour) as Peak_Usage
+| eval Hour=strftime(_time, "%H")
+| stats max(Peak_TPS) as Peak_TPS, avg(Average_TPS) as Average_TPS, max(Peak_Usage) as Peak_Usage, max(Transactions_Per_Hour) as "#Transactions_Per_Hour", first(Hour) as Peak_Hours, last(Hour) as Typical_Hours
+| eval Peak_Hours=strftime(Peak_Hours*3600, "%I %p"), Typical_Hours=strftime(Typical_Hours*3600, "%I %p")
