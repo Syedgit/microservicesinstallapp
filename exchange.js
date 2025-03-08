b94d69133b6ca23e312bbafa41299699f5d8e6e3
Syed Hussain <aftab.jersey@gmail.com>
2025-03-07 23:26:30 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,66 +1,29 @@
-import { inject, Injectable } from '@angular/core';
-import { AuthFacade } from '@digital-blocks/angular/core/store/auth';
-import { PageDefinitionFacade } from '@digital-blocks/angular/core/store/page-definition';
-import { ExperienceService } from '@digital-blocks/angular/core/util/experience-service';
-import { mapResponseBody } from '@digital-blocks/angular/core/util/services';
-import { filter, map, Observable, of, switchMap } from 'rxjs';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlPharmacyContentSpotFacade } from '../store/pl-pharmacy-content-spot.facade';
+import { SpotRequest } from '../store/pl-pharmacy-content-spot.actions';
 
-import { CMSAPIResponse, PageData } from '../../../../static-page-spots/src/lib/static-page-spots-interface';
-import { CmsContentConfig } from './pl-pharmacy-content-spot.config';
-import { SpotRequest } from './pl-pharmacy-content-spot.actions';
-
-@Injectable({
-  providedIn: 'root'
+@Component({
+  selector: 'app-pl-pharmacy-content',
+  templateUrl: './pl-pharmacy-content.component.html',
+  styleUrls: ['./pl-pharmacy-content.component.scss'],
+  standalone: true,
+  imports: [CommonModule] // Add additional dependencies if needed
 })
-export class PlPharmacyContentSpotService {
-  private readonly experienceService = inject(ExperienceService);
-  private readonly pageDefinition = inject(PageDefinitionFacade);
-  private readonly authFacade = inject(AuthFacade);
+export class PlPharmacyContentComponent implements OnInit {
+  private readonly plPharmacyFacade = inject(PlPharmacyContentSpotFacade);
 
-  /**
-   * @description Fetches multiple PlPharmacyContentSpots data from the CMS.
-   * 
-   * @param cmsSpots The array of SpotRequest objects.
-   * @returns Observable containing an array of PageData objects.
-   */
-  public fetchMultiplePlPageContents(cmsSpots: SpotRequest[]): Observable<PageData[]> {
-    return this.pageDefinition.page$.pipe(
-      filter((page) => page !== undefined),
-      switchMap((page) => {
-        return page.metadata?.authenticated
-          ? of(true)
-          : this.authFacade.guestTokenValid$.pipe(
-              filter((guestTokenValid) => guestTokenValid),
-              map(() => false)
-            );
-      }),
-      switchMap((isAuth) => {
-        return this.experienceService
-          .post<CMSAPIResponse>(
-            CmsContentConfig.clientId,
-            CmsContentConfig.experiences,
-            `${CmsContentConfig.mock}${isAuth ? '_auth.json' : '_unauth.json'}`,
-            {
-              data: {
-                cmsContentInput: {
-                  spots: cmsSpots // Send array of objects
-                }
-              }
-            },
-            {
-              maxRequestTime: 10_000, // Matches gateway timeout
-              additionalHeaders: {
-                'x-appName': CmsContentConfig.appName
-              }
-            }
-          )
-          .pipe(
-            mapResponseBody(),
-            map((response) => 
-              response.data.cmsContent.flatMap((spot) => spot.content) // Extracts & flattens content
-            )
-          );
-      })
-    );
+  public plPharmacyContentSpots$ = this.plPharmacyFacade.plPharmacyContentSpots$; // Observable for data
+  public loading$ = this.plPharmacyFacade.loading$; // Loading state
+  public error$ = this.plPharmacyFacade.error$; // Error state
+
+  ngOnInit(): void {
+    const spots: SpotRequest[] = [
+      { spotName: 'headerSpot' },
+      { spotName: 'bodySpot' },
+      { spotName: 'promoSpot' }
+    ];
+
+    this.plPharmacyFacade.getPlPharmacyContentSpots(spots);
   }
 }
