7c6090ea6c52cb79bf9f57ea0abd919971c6a4cf
Syed Hussain <aftab.jersey@gmail.com>
2025-03-07 23:08:11 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,6 +1,66 @@
-Type 'Observable<{ contentId: string; spotName: string; content: PageData[]; clientCode: string; }[]>' is not assignable to type 'Observable<PageData[]>'.
-  Type '{ contentId: string; spotName: string; content: PageData[]; clientCode: string; }[]' is not assignable to type 'PageData[]'.
-    Type '{ contentId: string; spotName: string; content: PageData[]; clientCode: string; }' is missing the following properties from type 'PageData': elementType, elementValuets(2322)
+import { inject, Injectable } from '@angular/core';
+import { AuthFacade } from '@digital-blocks/angular/core/store/auth';
+import { PageDefinitionFacade } from '@digital-blocks/angular/core/store/page-definition';
+import { ExperienceService } from '@digital-blocks/angular/core/util/experience-service';
+import { mapResponseBody } from '@digital-blocks/angular/core/util/services';
+import { filter, map, Observable, of, switchMap } from 'rxjs';
 
+import { CMSAPIResponse, PageData } from '../../../../static-page-spots/src/lib/static-page-spots-interface';
+import { CmsContentConfig } from './pl-pharmacy-content-spot.config';
+import { SpotRequest } from './pl-pharmacy-content-spot.actions';
 
- return this.pageDefinition.page$.pipe(
+@Injectable({
+  providedIn: 'root'
+})
+export class PlPharmacyContentSpotService {
+  private readonly experienceService = inject(ExperienceService);
+  private readonly pageDefinition = inject(PageDefinitionFacade);
+  private readonly authFacade = inject(AuthFacade);
+
+  /**
+   * @description Fetches multiple PlPharmacyContentSpots data from the CMS.
+   * 
+   * @param cmsSpots The array of SpotRequest objects.
+   * @returns Observable containing an array of PageData objects.
+   */
+  public fetchMultiplePlPageContents(cmsSpots: SpotRequest[]): Observable<PageData[]> {
+    return this.pageDefinition.page$.pipe(
+      filter((page) => page !== undefined),
+      switchMap((page) => {
+        return page.metadata?.authenticated
+          ? of(true)
+          : this.authFacade.guestTokenValid$.pipe(
+              filter((guestTokenValid) => guestTokenValid),
+              map(() => false)
+            );
+      }),
+      switchMap((isAuth) => {
+        return this.experienceService
+          .post<CMSAPIResponse>(
+            CmsContentConfig.clientId,
+            CmsContentConfig.experiences,
+            `${CmsContentConfig.mock}${isAuth ? '_auth.json' : '_unauth.json'}`,
+            {
+              data: {
+                cmsContentInput: {
+                  spots: cmsSpots // Send array of objects
+                }
+              }
+            },
+            {
+              maxRequestTime: 10_000, // Matches gateway timeout
+              additionalHeaders: {
+                'x-appName': CmsContentConfig.appName
+              }
+            }
+          )
+          .pipe(
+            mapResponseBody(),
+            map((response) => 
+              response.data.cmsContent.flatMap((spot) => spot.content) // Extracts & flattens content
+            )
+          );
+      })
+    );
+  }
+}
