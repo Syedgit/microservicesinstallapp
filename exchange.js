673d91a822ecf5b2ce487d8b64ed37082d9e1998
Syed Hussain <aftab.jersey@gmail.com>
2025-03-07 23:06:30 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,3 +1,6 @@
 Type 'Observable<{ contentId: string; spotName: string; content: PageData[]; clientCode: string; }[]>' is not assignable to type 'Observable<PageData[]>'.
   Type '{ contentId: string; spotName: string; content: PageData[]; clientCode: string; }[]' is not assignable to type 'PageData[]'.
     Type '{ contentId: string; spotName: string; content: PageData[]; clientCode: string; }' is missing the following properties from type 'PageData': elementType, elementValuets(2322)
+
+
+ return this.pageDefinition.page$.pipe(
