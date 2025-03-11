55304715a43968dc5b54583ffb9ad3939d1d9226
Syed Hussain <aftab.jersey@gmail.com>
2025-03-11 12:46:13 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,90 +1,33 @@
-facade
+public getPlPharmacyContentSpots$ = createEffect(() => {
+  return this.actions$.pipe(
+    ofType(PlPharmacyContentSpotActions.getPlPharmacyContentSpots),
+    tap((action) => console.log('🟡 Effects - Full Action Object:', action)), // Debug log
+    switchMap((action) => {
+      console.log('🟡 Effects - Extracted cmsSpots:', action.cmsSpots); // Debug log
 
-import { Injectable, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
+      if (!action.cmsSpots || action.cmsSpots.length === 0) {
+        console.error('🔴 Effects - Error: cmsSpots is empty or undefined!');
+        return of(PlPharmacyContentSpotActions.getPlPharmacyContentSpotsFailure({
+          error: errorMessage(this.errorTag, 'cmsSpots is empty or undefined!')
+        }));
+      }
 
-import {
-  PlPharmacyContentSpotActions,
-  SpotRequest
-} from './pl-pharmacy-content-spot.actions';
-import {
-  PlPharmacyContentSpotFeature,
-  PlPharmacyContentSpotState
-} from './pl-pharmacy-content-spot.reducer';
-
-@Injectable({ providedIn: 'root' })
-export class PlPharmacyContentSpotFacade {
-  protected readonly store = inject(Store<PlPharmacyContentSpotState>);
-
-  public readonly plPharmacyContentSpots$ = this.store.select(
-    PlPharmacyContentSpotFeature.selectPlPharmacyContentSpots
-  );
-
-  public getPlPharmacyContentSpots(spots: SpotRequest[]): void {
-    this.store.dispatch(
-      PlPharmacyContentSpotActions.getPlPharmacyContentSpots({
-        cmsSpots: spots
-      })
-    );
-  }
-}
-
-
-effects
-
-import { inject, Injectable } from '@angular/core';
-import { errorMessage } from '@digital-blocks/angular/core/util/error-handler';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, of, switchMap } from 'rxjs';
-
-import { PlPharmacyContentSpotService } from '../services/pl-pharmacy-content-spot.service';
-
-import {
-  PlPharmacyContentSpotActions,
-  SpotRequest
-} from './pl-pharmacy-content-spot.actions';
-
-@Injectable()
-export class PlPharmacyContentSpotEffects {
-  private readonly actions$ = inject(Actions);
-  private readonly getPlPageSpotsService = inject(PlPharmacyContentSpotService);
-  private readonly errorTag = 'PlPharmacyContentSpotEffects';
-
-  public getPlPharmacyContentSpots$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(PlPharmacyContentSpotActions.getPlPharmacyContentSpots),
-      switchMap(({ cmsSpots }: { cmsSpots: SpotRequest[] }) => {
-        return this.getPlPageSpotsService
-          .fetchMultiplePlPageContents(cmsSpots)
-          .pipe(
-            map((spotData) => {
-              return PlPharmacyContentSpotActions.getPlPharmacyContentSpotsSuccess(
-                {
-                  plPharmacyContentSpots: spotData
-                }
-              );
-            }),
-            catchError((error: unknown) => {
-              return of(
-                PlPharmacyContentSpotActions.getPlPharmacyContentSpotsFailure({
-                  error: errorMessage(this.errorTag, error)
-                })
-              );
+      return this.getPlPageSpotsService.fetchMultiplePlPageContents(action.cmsSpots).pipe(
+        map((spotData) => {
+          console.log('✅ Effects - API Response:', spotData);
+          return PlPharmacyContentSpotActions.getPlPharmacyContentSpotsSuccess({
+            plPharmacyContentSpots: spotData
+          });
+        }),
+        catchError((error: unknown) => {
+          console.error('🔴 Effects - Error:', error);
+          return of(
+            PlPharmacyContentSpotActions.getPlPharmacyContentSpotsFailure({
+              error: errorMessage(this.errorTag, error)
             })
           );
-      })
-    );
-  });
-}
-
-
-sample request thats how it is being passed from component to pl pharmacy content facade 
-
- cmsSpots: [
-          {
-            spotName: 'PortletIntegratedPharmacyLocatorSearchAnnouncementSpot'
-          },
-          {
-            spotName: 'PortletIntegratedPharmacyLocatorResultsAnnouncementSpot'
-          }
-        ]
+        })
+      );
+    })
+  );
+});
