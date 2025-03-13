84aeb162718d1bfedda105e08d28f6a321b2d5a0
Syed Hussain <aftab.jersey@gmail.com>
2025-03-13 18:21:24 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,87 +1,52 @@
-Effects
+reducer test failure 
 
 
-import { TestBed } from '@angular/core/testing';
-import { AuthFacade } from '@digital-blocks/angular/core/store/auth';
-import { errorMessage } from '@digital-blocks/angular/core/util/error-handler';
-import { provideMockActions } from '@ngrx/effects/testing';
-import { provideMockStore } from '@ngrx/store/testing';
-import { firstValueFrom, Observable, of, throwError } from 'rxjs';
+ FAIL   angular-pharmacy-pharmacy-locator-store-pl-pharmacy-content-spot  libs/angular/pharmacy/pharmacy-locator/store/pl-pharmacy-content-spot/src/lib/+state/pl-pharmacy-content-spot.reducer.spec.ts
+  ● PlPharmacyContentSpotReducer › should set loading to true on getPlPharmacyContentSpots
 
-import { PlPharmacyContentSpotService } from '../services';
-import { plPharmacySpotsMockResponse, error } from './mocks/pl-pharmacy-content-spot.constants.spec';
+    expect(received).toEqual(expected) // deep equality
+
+    - Expected  - 0
+    + Received  + 8
+
+      Object {
+    +   "cmsSpots": Array [
+    +     Object {
+    +       "spotName": "PortletIntegratedPharmacyLocatorSearchAnnouncementSpot",
+    +     },
+    +     Object {
+    +       "spotName": "PortletIntegratedPharmacyLocatorResultsAnnouncementSpot",
+    +     },
+    +   ],
+        "error": undefined,
+        "loading": true,
+        "plPharmacyContentSpots": null,
+      }
+
+      20 |     };
+      21 |
+    > 22 |     expect(reducer(initialPlPharmacyContentSpotState, action)).toEqual(
+         |                                                                ^
+      23 |       expectedState
+      24 |     );
+      25 |   });
+
+      at src/lib/+state/pl-pharmacy-content-spot.reducer.spec.ts:22:64
+      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.15.0/node_modules/zone.js/bundles/zone.umd.js:416:32)
+      at ProxyZoneSpec.Object.<anonymous>.ProxyZoneSpec.onInvoke (../../../../../../node_modules/.pnpm/zone.js@0.15.0/node_modules/zone.js/bundles/zone-testing.umd.js:2177:43)
+      at _ZoneDelegate.invoke (../../../../../../node_modules/.pnpm/zone.js@0.15.0/node_modules/zone.js/bundles/zone.umd.js:415:38)
+      at ZoneImpl.run (../../../../../../node_modules/.pnpm/zone.js@0.15.0/node_modules/zone.js/bundles/zone.umd.js:147:47)
+      at Object.wrappedFunc (../../../../../../node_modules/.pnpm/zone.js@0.15.0/node_modules/zone.js/bundles/zone-testing.umd.js:450:38)
+
+
+reducer
+
+import { plPharmacySpotsMockResponse } from './mocks/pl-pharmacy-content-spot.constants.spec';
 import { PlPharmacyContentSpotActions } from './pl-pharmacy-content-spot.actions';
-import { PlPharmacyContentSpotEffects } from './pl-pharmacy-content-spot.effects';
-
-describe('PlPharmacyContentSpotEffects', () => {
-  let actions$: Observable<any>;
-  let effects: PlPharmacyContentSpotEffects;
-  let plPharmacySpotsService: PlPharmacyContentSpotService;
-
-  const cmsContentSpots = [
-    { spotName: 'PortletIntegratedPharmacyLocatorSearchAnnouncementSpot' },
-    { spotName: 'PortletIntegratedPharmacyLocatorResultsAnnouncementSpot' }
-  ];
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        PlPharmacyContentSpotEffects,
-        provideMockStore(),
-        provideMockActions(() => actions$),
-        {
-          provide: PlPharmacyContentSpotService,
-          useValue: {
-            fetchMultiplePlPageContents: jest.fn(() => of(plPharmacySpotsMockResponse)), // ✅ Ensure this method exists
-          }
-        },
-        {
-          provide: AuthFacade,
-          useValue: { guestTokenValid$: of(true) }
-        }
-      ]
-    });
-
-    effects = TestBed.inject(PlPharmacyContentSpotEffects);
-    plPharmacySpotsService = TestBed.inject(PlPharmacyContentSpotService);
-  });
-
-  describe('getPlPharmacyContentSpots$', () => {
-    it('should dispatch success action when service returns data', async () => {
-      const expectedAction = PlPharmacyContentSpotActions.getPlPharmacyContentSpotsSuccess({
-        fullResponse: plPharmacySpotsMockResponse
-      });
-
-      actions$ = of(PlPharmacyContentSpotActions.getPlPharmacyContentSpots({ cmsSpots: cmsContentSpots }));
-
-      jest.spyOn(plPharmacySpotsService, 'fetchMultiplePlPageContents').mockReturnValue(of(plPharmacySpotsMockResponse));
-
-      expect(await firstValueFrom(effects.getPlPharmacyContentSpots$)).toEqual(expectedAction);
-    });
-
-    it('should dispatch failure action when service throws an error', async () => {
-      const expectedAction = PlPharmacyContentSpotActions.getPlPharmacyContentSpotsFailure({
-        error: errorMessage(effects.constructor.name, error)
-      });
-
-      actions$ = of(PlPharmacyContentSpotActions.getPlPharmacyContentSpots({ cmsSpots: cmsContentSpots }));
-
-      jest.spyOn(plPharmacySpotsService, 'fetchMultiplePlPageContents').mockReturnValue(
-        throwError(() => errorMessage(effects.constructor.name, error))
-      );
-
-      expect(await firstValueFrom(effects.getPlPharmacyContentSpots$)).toEqual(expectedAction);
-    });
-  });
-});
-
-
-Reducer
-
-
-import { mockPageContent } from './mocks/pl-pharmacy-content-spot.constants.spec';
-import { PlPharmacyContentSpotActions } from './pl-pharmacy-content-spot.actions';
-import { initialPlPharmacyContentSpotState, reducer } from './pl-pharmacy-content-spot.reducer';
+import {
+  initialPlPharmacyContentSpotState,
+  reducer
+} from './pl-pharmacy-content-spot.reducer';
 
 describe('PlPharmacyContentSpotReducer', () => {
   it('should set loading to true on getPlPharmacyContentSpots', () => {
@@ -97,89 +62,25 @@ describe('PlPharmacyContentSpotReducer', () => {
       loading: true
     };
 
-    expect(reducer(initialPlPharmacyContentSpotState, action)).toEqual(expectedState);
+    expect(reducer(initialPlPharmacyContentSpotState, action)).toEqual(
+      expectedState
+    );
   });
 
   it('should update fullApiResponse on getPlPharmacyContentSpotsSuccess', () => {
-    const action = PlPharmacyContentSpotActions.getPlPharmacyContentSpotsSuccess({
-      fullResponse: { data: { cmsContent: mockPageContent } } // ✅ Ensure correct structure
-    });
+    const action =
+      PlPharmacyContentSpotActions.getPlPharmacyContentSpotsSuccess({
+        plPharmacyContentSpots: plPharmacySpotsMockResponse
+      });
 
     const expectedState = {
       ...initialPlPharmacyContentSpotState,
-      fullApiResponse: { data: { cmsContent: mockPageContent } },
+      plPharmacyContentSpots: plPharmacySpotsMockResponse,
       loading: false
     };
 
-    expect(reducer(initialPlPharmacyContentSpotState, action)).toEqual(expectedState);
+    expect(reducer(initialPlPharmacyContentSpotState, action)).toEqual(
+      expectedState
+    );
   });
 });
-
-
-facade
-
-
-import { TestBed } from '@angular/core/testing';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { mockPageContent } from './mocks/pl-pharmacy-content-spot.constants.spec';
-import { PlPharmacyContentSpotActions } from './pl-pharmacy-content-spot.actions';
-import { PlPharmacyContentSpotFacade } from './pl-pharmacy-content-spot.facade';
-import { PlPharmacyContentSpotState, PlPharmacyContentSpotFeature } from './pl-pharmacy-content-spot.reducer';
-
-describe('PlPharmacyContentSpotFacade', () => {
-  let facade: PlPharmacyContentSpotFacade;
-  let store: MockStore<PlPharmacyContentSpotState>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        PlPharmacyContentSpotFacade,
-        provideMockStore({
-          initialState: {
-            fullApiResponse: null,
-            loading: false,
-            error: undefined
-          },
-          selectors: [
-            { selector: PlPharmacyContentSpotFeature.selectFullApiResponse, value: mockPageContent }
-          ]
-        })
-      ]
-    });
-
-    facade = TestBed.inject(PlPharmacyContentSpotFacade);
-    store = TestBed.inject(MockStore);
-  });
-
-  describe('getPlPharmacyContentSpots', () => {
-    it('should dispatch action to getPlPharmacyContentSpots', () => {
-      const action = PlPharmacyContentSpotActions.getPlPharmacyContentSpots({
-        cmsSpots: [
-          { spotName: 'PortletIntegratedPharmacyLocatorSearchAnnouncementSpot' },
-          { spotName: 'PortletIntegratedPharmacyLocatorResultsAnnouncementSpot' }
-        ]
-      });
-
-      const spy = jest.spyOn(store, 'dispatch');
-
-      facade.getPlPharmacyContentSpots([
-        { spotName: 'PortletIntegratedPharmacyLocatorSearchAnnouncementSpot' },
-        { spotName: 'PortletIntegratedPharmacyLocatorResultsAnnouncementSpot' }
-      ]);
-
-      expect(spy).toHaveBeenCalledWith(action);
-    });
-  });
-});
-
-
-action
-
-import { PlPharmacyContentSpotActions } from './pl-pharmacy-content-spot.actions';
-
-describe('PlPharmacyContentSpotActions', () => {
-  it('should create actions successfully', () => {
-    expect(PlPharmacyContentSpotActions).toBeTruthy();
-  });
-});
-
