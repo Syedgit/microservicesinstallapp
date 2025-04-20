31edbc30952952f90812a136f39f44e93f62f319
Syed Hussain <aftab.jersey@gmail.com>
2025-04-20 12:54:33 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,113 +1,63 @@
-@if ((locationSearchStore.hideLocationSearch$ | async) === false) {
-  <div
-    class="location-search-container"
-    [ngClass]="!!customizedClass ? customizedClass : ''">
-    <lib-custom-type-ahead
-      [label]="locationSearchStaticContent.heading"
-      inputId="location-search-input"
-      [loadingMessage]="locationSearchStaticContent.loadingMessage"
-      [errorMessage]="locationSearchErrorText$ | async"
-      [suggestionList]="locationSearchFacade.locationSuggestions$ | async"
-      (search)="handleSearch($event)"
-      (suggestionSelected)="handleSuggestionSelected($event)"
-      (typeAheadBlur)="freeInputZipcodeSearch($event)"
-      (inputCleared)="onInputClear()"
-      [clearInput]="clearInput"
-      [initialInputValue]="initialValue">
-    </lib-custom-type-ahead>
-    @if (showUseCurrentLocationBtn) {
-      <ps-button
-        class="uselocation-button"
-        variant="text"
-        size="sm"
-        (click)="useMyLocation()"
-        (keyPress)="useMyLocation()"
-        [isFullWidth]="isUseCurrentLocationBtnFullWidth">
-        @if (userCurrentLocationBtnLeadingImage) {
-          <!-- eslint-disable-next-line @nx/workspace/no-setting-inner-html-in-templates -- content has been sanitized -->
-          <div
-            slot="leading"
-            [innerHTML]="
-              userCurrentLocationBtnLeadingImage | sanitizeHtml: true
-            "></div>
-        }
-        {{ locationSearchStaticContent.currentLocationBtnTxt }}
-      </ps-button>
-    }
-  </div>
+// location-search.reducer.ts
+export interface LocationSearchState {
+  ...
+  includeServiceAreaInLabel: boolean; // 👈 new
 }
 
-config as page dedinations :
+export const initialLocationSearchState: LocationSearchState = {
+  ...
+  includeServiceAreaInLabel: false
+};
+
+on(LocationSearchActions.updateLabelIncludeServiceArea, (state, { include }) => ({
+  ...state,
+  includeServiceAreaInLabel: include
+}))
 
 
-{
-  "identifiers": {
-    "platform": "web",
-    "tenant": "caremark",
-    "path": "/pharmacy/benefits/pharmacy-locator/search"
-  },
-  "payload": {
-    "page": {
-      "header": {
-        "name": "CaremarkHeader"
-      },
-      "main": [
-        {
-          "name": "PlHeading",
-          "inputs": {
-            "headerText": "Find an in-network pharmacy",
-            "breadcrumbMenuList": [
-              {
-                "pageName": "Home",
-                "externalPath": "/memberportal/cmkdashboard"
-              },
-              {
-                "pageName": "Find an in-network pharmacy",
-                "pagePath": "pharmacy/benefits/pharmacy-locator/search"
-              }
-            ],
-            "hideBreadcrumbForMweb": true
-          }
-        },
-        {
-          "name": "LocationSearch",
-          "inputs": {
-            "locationSearchStaticContent": {
-              "heading": "ZIP code or location",
-              "loadingMessage": "Searching locations",
-              "currentLocationBtnTxt": "Use my current location",
-              "searchErrorText": "Select an address from the drop down"
-            },
-            "customizedClass": "pl-search-flow"
-          }
-        },
-        {
-          "name": "PlPharmacyMap",
-          "inputs": {
-            "staticContent": {}
-          }
-        },
-        {
-          "name": "PlDisclaimers",
-          "inputs": {}
-        }
-      ],
-      "footer": {
-        "name": "CaremarkFooter"
-      },
-      "metadata": {
-        "title": "CVS Caremark - Pharmacy Locator search",
-        "tealium": {
-          "pageCategory": "plan_and_benefits",
-          "pageIdentifier": "plan_and_benefits:pl"
-        },
-        "styles": {
-          "margin": false
-        },
-        "publish": true,
-        "authenticated": true
-      }
-    }
-  }
+// location-search.actions.ts
+export const updateLabelIncludeServiceArea = createAction(
+  '[LocationSearch] Update Label Include Service Area',
+  props<{ include: boolean }>()
+);
+
+
+// location-search.facade.ts
+public readonly includeServiceAreaInLabel$ = this.store.select(
+  LocationSearchFeature.selectIncludeServiceAreaInLabel
+);
+
+public setIncludeServiceAreaInLabel(include: boolean) {
+  this.store.dispatch(LocationSearchActions.updateLabelIncludeServiceArea({ include }));
 }
+
+// pl-search-component.ts
+
+
+if (this.filteredServiceAreaList.length > 0) {
+  ...
+  this.plPharmacySearchStore.updateLocationSuggestions({ ... });
+  this.locationSearchFacade.setIncludeServiceAreaInLabel(true); // 👈 Set flag
+}
+
+// locationsearchComponent.ts
+
+public includeServiceAreaInLabel$ = this.locationSearchFacade.includeServiceAreaInLabel$;
+
+public computedHeading$ = combineLatest([
+  this.includeServiceAreaInLabel$,
+  of(this.locationSearchStaticContent.heading)
+]).pipe(
+  map(([includeServiceArea, originalHeading]) =>
+    includeServiceArea ? `${originalHeading} or Service Area` : originalHeading
+  )
+);
+
+// location.search.comp.html
+
+<lib-custom-type-ahead
+  [label]="computedHeading$ | async"
+  ...
+>
+</lib-custom-type-ahead>
 
