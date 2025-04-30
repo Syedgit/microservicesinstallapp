8c61c56cdbcfeae538c94173b187ff6fde50276f
Syed Hussain <aftab.jersey@gmail.com>
2025-04-30 13:04:19 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -2,3 +2,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+nano ~/.zshrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+command -v nvm
+
