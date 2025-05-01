8fb3aae7180e80aade8ef6a2c366c3db4ebcea83
Syed Hussain <aftab.jersey@gmail.com>
2025-05-01 15:21:45 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,42 +1,18 @@
- public loadPharmacyDetail(
-    clickedPharmacyIndex: number,
-    pharmacyData: IFormattedPharmaciesResponse
-  ): void {
-    this.pharmacyDetailIndex = clickedPharmacyIndex;
-    this.plPharmacySearchStore.setSelectedPharmacy(
-      pharmacyData as PharmacyDetail
-    );
-    this.navigationService.navigate(
-      '/pharmacy/benefits/pharmacy-locator/pharmacy-details',
-      { queryParamsHandling: 'preserve' },
-      {
-        navigateByPath: true
-      }
-    );
-    this.plPharmacySearchStore.resetStateOnInputClear();
-    if (pharmacyData.pharmacyLob == 'M' || pharmacyData.pharmacyLob == 'S') {
-      this.plPharmacySearchStore.linkEvent(
-        AdobeTaggingConstants.onClick_mail_details_link
-      );
-    } else if (pharmacyData.pharmacyLob == 'R')
-      this.plPharmacySearchStore.linkEvent(
-        AdobeTaggingConstants.onClick_retail_details_link
-      );
-  }
+<ps-link
+  class="pharmacy-details-link"
+  [link-href]="getPharmacyDetailsUrl()"
+  (click)="loadPharmacyDetail(i, pharmacyData)">
+  Pharmacy details
+</ps-link>
+
+import { Router } from '@angular/router';
+
+constructor(private router: Router) {}
+
+getPharmacyDetailsUrl(): string {
+  return this.router.serializeUrl(
+    this.router.createUrlTree(['/pharmacy/benefits/pharmacy-locator/pharmacy-details'])
+  );
+}
 
 
-this is html 
-
-<div
-                          class="retail-pharmacy-detail-link-block pharmacy-detail-link-block">
-                          <ps-link
-                            class="pharmacy-details-link"
-                            link-href="javascript:void(0)"
-                            (click)="loadPharmacyDetail(i, pharmacyData)"
-                            (keyPress)="loadPharmacyDetail(i, pharmacyData)"
-                            variant="standalone"
-                            subtype="default"
-                            weight="strong"
-                            >Pharmacy details</ps-link
-                          >
-                        </div>
