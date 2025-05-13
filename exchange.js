bdfc8ed75c78072cf3ec7adf776e7d9716df4ca4
Syed Hussain <aftab.jersey@gmail.com>
2025-05-13 13:33:03 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,138 +1,86 @@
-1. client request 
+// constants.ts
+export const I90_ID_TYPE = "PBM_QL_PARTICIPANT_ID_TYPE";
+export const DEFAULT_ROUTE = "test-route-123";
+export const STATUS_SUCCESS = "0000";
+export const STATUS_FAILURE = "9999";
 
-{
-    "setPrimaryPharmacyRequest": {
-        "header": {
-            "serviceContext": {
-                "apiVersion": "1.0",
-                "lineOfBusiness": "",
-                "appName": "SDK_VOYAGE",
-                "channelName": "Web",
-                "responseFormat": "JSON",
-                "requesterId": "DIGITAL_SPECIALTY",
-                "refID": "4857a0f56e51403daf1750109aa9a3d0",
-                "tokenID": "123",
-                "clientName": ""
-            },
-            "securityContext": {
-                "securityType": "apiKey",
-                "apiKey": "123"
-            }
-        },
-        "details": {
-            "primaryPharmacyPresent": "true",
-            "pharmacyNumber": "1489524",
-            "pharmacyName": "KEDVON PHARMACY INTEGRATIONS",
-            "pharmacyAddress": "770 S BUFFALO GROVE",
-            "pharmacyCity": "BUFFALO GROVE",
-            "pharmacyState": "IL",
-            "pharmacyZipCode": "60089",
-            "pharmacyPhone": "847-947-2601",
-            "planId": "8302TDMACC001"
-        }
-    }
+
+
+core 
+
+import { I90_ID_TYPE, DEFAULT_ROUTE } from './constants';
+
+core(args: SetPrimaryPharmacyClientRequest, requestMethod: any, _dependencies: any): Promise<any> {
+  const details = args.setPrimaryPharmacyRequest?.details || {};
+  const memberId = details.participantID || args.memberInfo?.internalID;
+
+  if (!memberId) {
+    return Promise.reject(new Error("Missing participantID or memberInfo.internalID"));
+  }
+
+  const requestBody = {
+    id: memberId,
+    idType: I90_ID_TYPE,
+    city: details.pharmacyCity,
+    state: details.pharmacyState,
+    postalCode: details.pharmacyZipCode,
+    address: details.pharmacyAddress,
+    name: details.pharmacyName,
+    nabpId: details.pharmacyNumber,
+    phone: details.pharmacyPhone
+  };
+
+  const headers = {
+    "content-type": "application/json",
+    "x-route": DEFAULT_ROUTE
+  };
+
+  // Optional logging
+  this.Helper?.Logger?.info?.({
+    method: 'setPrimaryPharmacy:core',
+    requestBody,
+    headers
+  });
+
+  return requestMethod.http.makeRequest({
+    url: {
+      name: "save_primary_pharmacy",
+      params: {}
+    },
+    headers,
+    body: requestBody,
+    handler: this.Handler
+  });
 }
 
-2. client response 
 
-{
-    "setprimarypharmacymultiplanResponse": {
-        "header": {
-            "statusCode": "0000",
-            "statusDesc": "Success",
-            "refID": "4857a0f56e51403daf1750109aa9a3d0"
-        }
+after 
+
+import { STATUS_SUCCESS, STATUS_FAILURE } from './constants';
+
+after(args: SetPrimaryPharmacyClientRequest, data: any, _header: any): Promise<any> {
+  const statusCode = data?.statusCode || STATUS_FAILURE;
+  const statusDesc = data?.statusDesc || "Failure";
+  const refID = args.setPrimaryPharmacyRequest?.header?.serviceContext?.refID || "";
+
+  const response = {
+    setprimarypharmacymultiplanResponse: {
+      header: {
+        statusCode,
+        statusDesc,
+        refID
+      }
     }
+  };
+
+  // Optional logging
+  this.Helper?.Logger?.info?.({
+    method: 'setPrimaryPharmacy:after',
+    backendResponse: data,
+    clientResponse: response
+  });
+
+  return Promise.resolve({
+    get: () => response
+  });
 }
-
-3. new backend i90 request 
-
-{
-    "id": "632991602",
-    "idType": "PBM_QL_PARTICIPANT_ID_TYPE",
-    "city": "LAKE ZURICH",
-    "state": "IL",
-    "postalCode": "60047",
-    "address": "1350 E IL ROUTE 22",
-    "name": "RICHARDSON PHARMACY",
-    "nabpId": "1475474",
-    "phone": "972-479-9799"
-}
-
-4. new backend i90 response 
-
-{
-    "statusCode": "0000",
-    "statusDesc": "Success",
-    "pharmacy": [
-        {
-            "pharmacyLob": "R",
-            "ninetyDaySupplyNetwork": false,
-            "pharmacyNumber": "1475474",
-            "pharmacyName": "RICHARDSON PHARMACY",
-            "addresses": {
-                "line": [
-                    "1350 E IL ROUTE 22"
-                ],
-                "city": "LAKE ZURICH",
-                "state": "IL",
-                "postalCode": "60047",
-                "phoneNumber": "972-479-9799"
-            }
-        }
-    ]
-}
-
-5. core will build the request this needs to be updated with new i90 request 
-   core(args: SetPrimaryPharmacyParam, requestMethod: Interface.Core.RequestMethod, _dependencies: any): Promise<any> {
-        let requestBody = {
-            "setPrimaryPharmacyRequest": {
-                "Request": {
-                    "primaryPharmacyPresent": args.primaryPharmacyPresent,
-                    "participantID": args.memberInfo.internalID,
-                    "pharmNabpId": args.pharmacyNumber,
-                    "pharmName": args.pharmacyName,
-                    "pharmAdrTx": args.pharmacyAddress,
-                    "pharmCityTx": args.pharmacyCity,
-                    "pharmStateCd": args.pharmacyState,
-                    "pharmZipCd": args.pharmacyZipCode,
-                    "pharmPhone": args.pharmacyPhone
-                },
-                "memberInfo": args.memberInfo
-            }
-        };
-        return requestMethod.http.makeRequest({
-            url: {
-                name: 'dbpl_setPrimaryPharmacy',
-                params: {
-                    "version": "1.0",
-                    "appName": args.appName,
-                    "refId": args.refId
-                }
-            },
-            body: requestBody,
-            handler: this.Handler
-        });
-    }
-
-6. after will build the response and return the what client is expecting refId we can take it from the request.
-        
-    after(_args: SetPrimaryPharmacyParam, _data: any, header: Interface.Response.AfterHeader): Promise<Interface.Response.AfterResponse<Interface.Response.AfterHeader>> {
-        return Promise.resolve({
-            get: function () {
-                return header;
-            }
-        });
-    }
-
-
-  const headers: any = {
-                "content-type": "application/json",
-                "x-grid": args.refId ? args.refId.substring(0, 32) : '',
-                "x-consumername": args.appName ? args.appName : '',
-                "x-customappname": args.appName ? args.appName : '',
-                "x-memberToken": args.tokenId ? args.tokenId : '',
-                "x-api-key": (process && process.env) ? process.env.I90_API_KEY : '',
-                "x-clientrefid": args.refId.substring(0, 32)
-            };
-
