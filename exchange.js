42b30e6314774f601b2fc72695e61440d2bd3bc8
Syed Hussain <aftab.jersey@gmail.com>
2025-05-27 12:20:00 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,108 +1,179 @@
-import * as express from 'express';
-import * as logger from 'morgan';
-import * as bodyParser from 'body-parser';
-import * as caremark from '@cvsdigital_caremark/caremark-node-api';
-import * as retail from '@cvsdigital_caremark/retail-node-api';
-import * as combinator from '@cvsdigital_specialty/combinator-node-api';
-import * as specialty from '@cvsdigital_specialty/specialty-node-api';
-import * as _ from 'lodash';
-import * as path from 'path';
-import { validateToken } from './middleware/validateToken';
-import serveIndex = require('serve-index');
-import { Router, NextFunction, Request, Response } from 'express';
-import { LogEntryRequest, LogExitRequest } from './middleware/logging';
-const redisrouter = require('@cvsdigital_specialty/redis-node-api');
-const cors = require('cors');
+errors
 
-class App {
-    public express: express.Application;
-    private allowedHttpsVerbs: string[] = ["OPTIONS", "POST", "GET", "HEAD"];
+dist/karma/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(144,9): error TS1005: ':' expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(71,57): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(71,71): error TS1005: ':' expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(72,67): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(72,78): error TS1005: ':' expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(95,21): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(95,29): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(95,35): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(95,36): error TS1003: Identifier expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(99,11): error TS1005: ':' expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(116,31): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(116,60): error TS1005: ':' expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(117,31): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(117,55): error TS1005: ':' expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(118,53): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(118,61): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(118,77): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(118,89): error TS1005: ':' expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(140,19): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(140,27): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(140,33): error TS1109: Expression expected.
+dist/node/@cvsdigital_caremark_core-sdk/src/ts/api/Pharmacy/SetPrimaryPharmacyI90/SetPrimaryPharmacyApiI90.node.ts(140,34): error TS1003: Identifier expected.
+dist/node/@cvsdigital_caremark_core-sd
 
-    constructor() {
-        this.express = express();
-        this.middleware();
-        this.routes();
-    }
+here is the file SetprimaryPharmacyaI90.node,ts
 
-    private middleware(): void {
-        if ((process.env.CORS) === 'true') {
-            this.express.use(cors({ origin: "*" }));
-        }
-        this.express.set("view engine", "pug");
-        this.express.use(logger('dev'));
-        this.express.use(bodyParser.json());
-        this.express.use(bodyParser.urlencoded({ extended: false }));
-        this.express.use(this.allowKnownHttpVerbs());
-    }
+import {SetPrimaryPharmacyParamI90} from "./SetPrimaryPharmacyApiI90.interface";
+import {Interface} from "@cvsdigital_caremark/core-sdk-lib/dist/src/ts/interface/index";
+import {Types} from "@cvsdigital_caremark/core-sdk-lib/dist/src/ts/common/index";
+import {ValidationErrorEnum} from '../../../common/ValidationErrorFactory';
+import { I90_ID_TYPE, STATUS_FAILURE, X_ROUTE } from "./SetPrimaryPharmacyI90.constants";
+import { SetPrimaryPharmacyMultiPlanResult } from "../SetPrimaryPharmacy/SetPrimaryPharmacyMultiPlanApi.interface";
 
-    private allowKnownHttpVerbs() {
-        const __this = this;
-        return (req: Request, res: Response, next: NextFunction) => {
-            if (__this.allowedHttpsVerbs.indexOf(req.method) === -1) {
-                const ErrorDetails = {
-                    header: {
-                        statusCode: 9999,
-                        statusDesc: "Method Not Allowed!!",
-                    }
-                };
-                res.status(405);
-                res.send(JSON.stringify(ErrorDetails));
-            } else {
-                next();
+export class SetPrimaryPharmacyApiI90 extends Types.ModuleBase<SetPrimaryPharmacyParamI90, Interface.Response.AfterHeader> {
+
+    getValidators(): Interface.Validation.Validator[] {
+        let reqExpForZipCode = /^[0-9]{5}$/;
+        return [{
+            errorKey: ValidationErrorEnum.InvalidPrimaryPharmacyPresent,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return typeof args.primaryPharmacyPresent === "boolean" ||
+                    args.primaryPharmacyPresent === 'true' ||
+                    args.primaryPharmacyPresent === 'false';
             }
-        };
+        }, {
+            errorKey: ValidationErrorEnum.InvalidMemberId,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return args.memberInfo.internalID;
+            }
+        }, {
+            errorKey: ValidationErrorEnum.InvalidPharmacyNumber,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return args.pharmacyNumber;
+            }
+        }, {
+            errorKey: ValidationErrorEnum.InvalidPharmacyName,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return args.pharmacyName;
+            }
+        }, {
+            errorKey: ValidationErrorEnum.InvalidPharmacyAddress,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return args.pharmacyAddress;
+            }
+        }, {
+            errorKey: ValidationErrorEnum.InvalidPharmacyCity,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return args.pharmacyCity;
+            }
+        }, {
+            errorKey: ValidationErrorEnum.InvalidPharmacyState,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return args.pharmacyState;
+            }
+        }, {
+            errorKey: ValidationErrorEnum.InvalidPharmacyZipCode,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return args.pharmacyZipCode && reqExpForZipCode.test(args.pharmacyZipCode);
+            }
+        }, {
+            errorKey: ValidationErrorEnum.InvalidPharmacyPhone,
+            successCondition: (args: SetPrimaryPharmacyParamI90) => {
+                return args.pharmacyPhone;
+            }
+        }];
     }
 
-    private routes(): void {
-        LogExitRequest();
-
-        this.express.use('/client-sdk/AGP/api/client',
-            LogEntryRequest(),
-            validateToken(),
-            LogExitRequest(),
-            new combinator.CombinatorRouter().getCombinatorRouter,
-            LogExitRequest()
-        );
-
-        if (true || (process.env.ENV && process.env.ENV.toLowerCase().indexOf('prod') === -1)) {
-            this.express.use('/log', express.static(path.resolve(process.env.NODE_PATH || "", './../../log')));
-            this.express.use('/log', serveIndex(path.resolve(process.env.NODE_PATH || "", './../../log')));
-            this.express.use('/config', express.static(path.resolve(process.env.NODE_PATH || "", './../../config')));
-            this.express.use('/config', serveIndex(path.resolve(process.env.NODE_PATH || "", './../../config')));
+    before(args: SetPrimaryPharmacyParamI90): Promise<SetPrimaryPharmacyParamI90> {
+        if (args.pharmacyNumber && args.pharmacyNumber.toString().length < 7) {
+            args.pharmacyNumber = ('0000000' + args.pharmacyNumber).substr(-7);
         }
 
-        this.express.use('/api/client', redisrouter.redisRouter);
+        return Promise.resolve(args);
+    }
 
-        const hcrouter: Router = Router();
-        hcrouter.use(function (_req: Request, res: Response, next: NextFunction) {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-            next();
+    core(args: SetPrimaryPharmacyParamI90, requestMethod: Interface.Core.RequestMethod, _dependencies: any): Promise<SetPrimaryPharmacyMultiPlanResult> {
+        const details = args.SetPrimaryPharmacyParamI90?.details || {};
+        const memberId = details.participantID || args.memberInfo?.internalID;
+      
+        if (!memberId) {
+          return Promise.reject(new Error("Missing participantID or memberInfo.internalID"));
+        }
+      
+        const requestBody = {
+          id: memberId,
+          idType: I90_ID_TYPE,
+          city: details.pharmacyCity,
+          state: details.pharmacyState,
+          postalCode: details.pharmacyZipCode,
+          address: details.pharmacyAddress,
+          name: details.pharmacyName,
+          nabpId: details.pharmacyNumber,
+          phone: details.pharmacyPhone
+        };
+      
+        const headers = {
+          "content-type": "application/json",
+          "x-route": X_ROUTE
+        };
+      
+        this.Helper?.Logger?.info?.({
+          method: 'setPrimaryPharmacy:core',
+          requestBody,
+          headers
         });
-
-        hcrouter.get('/healthcheck', (_req: Request, _res: Response) => {
-            _res.status(200).send('OK');
+      
+        return requestMethod.http.makeRequest({
+          url: {
+            name: "save_primary_pharmacy",
+            params: {}
+          },
+          headers,
+          body: requestBody,
+          handler: this.Handler
         });
-
-        this.express.use('/', hcrouter);
-
-        const routers = _.filter(this.express._router.stack, (item) => {
-            return item.name === 'router';
-        });
-
-        const routeMetadata: any = {};
-        _.each(routers, (item) => {
-            routeMetadata[item.regexp.source] = [];
-            const routes = _.filter(item.handle.stack, (item1) => {
-                return !_.isNil(item1.route);  // ✅ replaced isNullOrUndefined with _.isNil
-            });
-            _.each(routes, (item1) => {
-                routeMetadata[item.regexp.source].push(item1.route);
-            });
-        });
+      }
+    after(
+      args: SetPrimaryPharmacyParamI90,
+      data: any,
+      _header: any
+    ): Promise<{ get: () => Interface.Response.AfterHeader; formattedResponse: any }> {
+      const statusCode = data?.statusCode || STATUS_FAILURE;
+      const statusDesc = data?.statusDesc || "Failure";
+      const refID = args.SetPrimaryPharmacyParamI90?.header?.serviceContext?.refID || "";
+    
+      // Required AfterHeader structure
+      const header: Interface.Response.AfterHeader = {
+        statusCode,
+        statusDesc,
+        refId: refID,
+        planId: '',
+        operationName: 'setPrimaryPharmacy',
+        xhrTrace: {} as Interface.Response.XhrProbe
+      };
+    
+      const clientResponse = {
+        setprimarypharmacymultiplanResponse: {
+          header: {
+            statusCode,
+            statusDesc,
+            refID
+          }
+        }
+      };
+    
+      this.Helper?.Logger?.info?.({
+        method: 'setPrimaryPharmacy:after',
+        backendResponse: data,
+        clientResponse
+      });
+    
+      return Promise.resolve({
+        get: () => header,
+        formattedResponse: clientResponse
+      });
     }
 }
-
-export default new App().express;
-export { combinator, specialty };
