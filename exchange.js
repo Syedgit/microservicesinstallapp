654b2c7cab7073c5370cab04620bdd3f89cf120e
Syed Hussain <aftab.jersey@gmail.com>
2025-06-03 20:02:59 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -8,3 +8,11 @@ url: 'https://internal-pt.pt1.digital-pbm-rx-pt.cvshealth.com/microservices/rxpb
                     url: response.config.url ? response.config.url.substr(0, 70) + _crypto.encryptWithCipher(response.config.url.substr(70), "sdkKey") : 'N/A',
                     timeout: response.config.timeout,
                     method: response.config.method
+
+
+  encryptWithCipher(buffer, cipherKey) {
+        const cipher = crypto.createCipheriv('aes-128-cbc', cipherKey);
+        let encryptText = cipher.update(buffer, 'utf8', 'hex');
+        encryptText += cipher.final('hex');
+        return encryptText;
+    }
