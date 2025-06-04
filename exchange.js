a5d529bc2481a04049c23e4c6af6ec3ec116b2a7
Syed Hussain <aftab.jersey@gmail.com>
2025-06-03 20:17:13 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -16,3 +16,12 @@ url: 'https://internal-pt.pt1.digital-pbm-rx-pt.cvshealth.com/microservices/rxpb
         encryptText += cipher.final('hex');
         return encryptText;
     }
+
+
+encryptWithCipher(buffer, cipherKey) {
+    const iv = Buffer.alloc(16, 0); // 16 bytes of zero
+    const cipher = crypto.createCipheriv('aes-128-cbc', Buffer.from(cipherKey, 'utf8'), iv);
+    let encryptText = cipher.update(buffer, 'utf8', 'hex');
+    encryptText += cipher.final('hex');
+    return encryptText;
+}
