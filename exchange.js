8ebbf8b1ebe6a844b12b3fe29c79e80ba5271160
Syed Hussain <aftab.jersey@gmail.com>
2025-06-04 23:06:41 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,25 +1,57 @@
-return requestMethod.http.makeRequest({
-            url: {
-                name: 'experience_getDeliveryDateRange_pbm',
-                params: {}
-            },
-            headers: customHeaders,
-            body: requestBody,
-            handler: this.Handler,
-            processResponse: async (response: any): Promise<Interface.Core.ProcessResponse> => {
-                // if (response.data && response.data.getAccountBalance && response.data.getOrderShippingDetails 
-                //      && response.data.getPbmPaymentDetails) {
-                const infoLog = {
-                    statusCode: response.statusCode,
-                    statusDescription: response.statusDescription,
-                    data: response.data ? 'FOUND' : 'NOTFOUND'
-                };
-                this.Helper.Logger.info(infoLog);
-                if (response && response.data) {
-                    return response;
-                } else {
-                    this.Helper.Logger.error("Invalid response from getDeliveryDateRange experience API");
-                    throw response;
-                }
+core(
+  args: SetPrimaryPharmacyParamI90,
+  requestMethod: Interface.Core.RequestMethod,
+  _dependencies: any
+): Promise<Interface.Response.Header> {
+    const requestBody = {
+        id: args.memberInfo.internalID,
+        idType: I90_ID_TYPE,
+        city: args.pharmacyCity,
+        state: args.pharmacyState,
+        postalCode: args.pharmacyZipCode,
+        address: args.pharmacyAddress,
+        name: args.pharmacyName,
+        nabpId: args.pharmacyNumber,
+        phone: args.pharmacyPhone
+    };
+
+    const headers = {
+        "content-type": "application/json",
+        "x-route": X_ROUTE
+    };
+
+    return requestMethod.http.makeRequest({
+        url: {
+            name: "save_primary_pharmacy",
+            params: {
+                "x-app-name": args.appName ? args.appName : "sdk-i90"
             }
-        });
+        },
+        headers,
+        body: requestBody,
+        handler: this.Handler,
+        processResponse: async (response: any): Promise<any> => {
+            const infoLog = {
+                statusCode: response.statusCode,
+                statusDescription: response.statusDesc,
+                data: response ? 'FOUND' : 'NOTFOUND'
+            };
+            this.Helper.Logger.info(infoLog);
+
+            if (response && response.statusCode) {
+                // Wrap the response to include a 'header' property for framework compatibility
+                const wrappedResponse = {
+                    header: {
+                        statusCode: response.statusCode || "9999",
+                        statusDesc: response.statusDesc || "Failure"
+                    },
+                    ...response
+                };
+                return wrappedResponse;
+            } else {
+                this.Helper.Logger.error("Invalid response from savePrimaryPharmacy I90 API");
+                throw response;
+            }
+        }
+    });
+}
