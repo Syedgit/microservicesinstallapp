f173f3ad88172d60c5178d387f6a718b5bb5b5ac
Syed Hussain <aftab.jersey@gmail.com>
2025-06-04 22:51:09 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,12 +1,9 @@
-Response = '{"statusCode":"0000","statusDesc":"Success","pharmacy":[{"pharmacyLob":"R","ninetyDaySupplyNetwork":false,"pharmacyNumber":"1489524","Name":"test","addresses":{"line":["771 S BUFFALO GROVE"],"city":"B","state":"IL","}}]}'
-xhrprobe = '{"status":200,"statusText":"OK","headers":{,"conversationid":"Id-6",,"content-type":"application/json","x-envoy-upstream-service-time":"345","server":"istio-envoy","connection":"close"}}'
-
  async processResponse(response, xhrProbe) {
         xhrProbe;
         let _root = response[Object.keys(response)[0]];
-         if (_root === Common_constant_1.STATUS_CODES.SUCCESS) {
+        if (_root.header.statusCode === Common_constant_1.STATUS_CODES.SUCCESS) {
             return _root;
-        } 
+        }
         else {
             await this.log({
                 message: 'Backend api Failure',
@@ -16,14 +13,4 @@ xhrprobe = '{"status":200,"statusText":"OK","headers":{,"conversationid":"Id-6",
             throw _root.header;
         }
     }
-
-Error 
-
- "errorDetails": [
-                {
-                    "planId": "8302TDMACC001",
-                    "statusCode": "9999",
-                    "statusDesc": "Cannot read properties of undefined (reading 'statusCode')",
-                    "lob": "pbm",
-                    "statusContext": 7
-                }
+}
