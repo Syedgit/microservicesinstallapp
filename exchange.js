bcf2481e014fc0b10d83ea82a8d6716791c8f880
Syed Hussain <aftab.jersey@gmail.com>
2025-06-09 16:56:00 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,33 +1,17 @@
-import * as Express from 'express';
-import { PBMBaseController } from '../../common/PBMBase.Controller';
-import { PBM } from '../../../../interface/PBMBase.interface';
-import { Interface } from "@cvsdigital_caremark/core-sdk-lib/dist/src/ts/interface/index";
+public after(
+    _args: SetPrimaryPharmacyParamI90,
+    data: { [key: string]: any },
+    header: Interface.Response.AfterHeader
+): Promise<Interface.Response.AfterResponse<any>> {
+    const response: { [key: string]: any } = {};
 
-export class SetPrimaryPharmacyHandler extends PBMBaseController {
-
-    public static process(
-        request: Express.Request,
-        response: Express.Response,
-        next: Express.NextFunction
-    ) {
-        super.process(request, response, next, new SetPrimaryPharmacyHandler(
-            PBM.SET_PRIMARY_PHARMACY
-        ));
+    if (data?.setprimarypharmacymultiplani90Response) {
+        response.setprimarypharmacymultiplanResponse = data.setprimarypharmacymultiplani90Response;
+    } else {
+        response.setprimarypharmacymultiplanResponse = data;
     }
 
-    // 🛠️ Override after() to remap the response key
-    public after(
-        _args: any,
-        data: any,
-        header: Interface.Response.AfterHeader
-    ): Promise<Interface.Response.AfterResponse<Interface.Response.AfterHeader>> {
-        if (data?.setprimarypharmacymultiplani90Response) {
-            data.setprimarypharmacymultiplanResponse = data.setprimarypharmacymultiplani90Response;
-            delete data.setprimarypharmacymultiplani90Response;
-        }
-
-        return Promise.resolve({
-            get: () => data
-        });
-    }
+    return Promise.resolve({
+        get: () => response
+    });
 }
