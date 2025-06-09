567bd195cc2101360ab34722c7ce2e6e7140fcc7
Syed Hussain <aftab.jersey@gmail.com>
2025-06-09 14:34:26 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,57 +1,46 @@
-core(
-  args: SetPrimaryPharmacyParamI90,
-  requestMethod: Interface.Core.RequestMethod,
-  _dependencies: any
-): Promise<Interface.Response.Header> {
-    const requestBody = {
-        id: args.memberInfo.internalID,
-        idType: I90_ID_TYPE,
-        city: args.pharmacyCity,
-        state: args.pharmacyState,
-        postalCode: args.pharmacyZipCode,
-        address: args.pharmacyAddress,
-        name: args.pharmacyName,
-        nabpId: args.pharmacyNumber,
-        phone: args.pharmacyPhone
-    };
+pbmBase.interface.ts
 
-    const headers = {
-        "content-type": "application/json",
-        "x-route": X_ROUTE
-    };
+SET_PRIMARY_PHARMACY = '/pharmacy/setPrimaryPharmacyMultiPlanI90',
 
-    return requestMethod.http.makeRequest({
-        url: {
-            name: "save_primary_pharmacy",
-            params: {
-                "x-app-name": args.appName ? args.appName : "sdk-i90"
-            }
-        },
-        headers,
-        body: requestBody,
-        handler: this.Handler,
-        processResponse: async (response: any): Promise<any> => {
-            const infoLog = {
-                statusCode: response.statusCode,
-                statusDescription: response.statusDesc,
-                data: response ? 'FOUND' : 'NOTFOUND'
-            };
-            this.Helper.Logger.info(infoLog);
+  Set Primary Handler 
 
-            if (response && response.statusCode) {
-                // Wrap the response to include a 'header' property for framework compatibility
-                const wrappedResponse = {
-                    header: {
-                        statusCode: response.statusCode || "9999",
-                        statusDesc: response.statusDesc || "Failure"
-                    },
-                    ...response
-                };
-                return wrappedResponse;
-            } else {
-                this.Helper.Logger.error("Invalid response from savePrimaryPharmacy I90 API");
-                throw response;
-            }
+import * as Express from 'express';
+import { PBMBaseController } from '../../common/PBMBase.Controller';
+import { PBM } from '../../../../interface/PBMBase.interface';
+
+export class SetPrimaryPharmacyHandler extends PBMBaseController {
+
+    public static process(request: Express.Request,
+        response: Express.Response,
+        next: Express.NextFunction) {
+        super.process(request, response, next, new SetPrimaryPharmacyHandler(
+            PBM.SET_PRIMARY_PHARMACY
+        ));
+    }
+
+}
+
+
+legacy response 
+
+{
+    "setprimarypharmacymultiplanResponse": {
+        "header": {
+            "statusCode": "0000",
+            "statusDesc": "Success",
+            "refID": "4857a0f56e51403daf1750109aa9a3d0"
         }
-    });
+    }
+}
+
+new response after i90 
+
+{
+    "setprimarypharmacymultiplani90Response": {
+        "header": {
+            "statusCode": "0000",
+            "statusDesc": "Success",
+            "refID": "4857a0f56e51403daf1750109aa9a3d0"
+        }
+    }
 }
