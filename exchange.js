55d2811c5f2e13fe954822e73f7e7a7710217d5e
Syed Hussain <aftab.jersey@gmail.com>
2025-07-07 12:00:12 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -4,13 +4,20 @@ tell application "Notes"
 		set noteName to the name of n
 		set noteBody to the body of n
 		
-		-- Clean filename to remove special characters
+		-- Clean the filename to be safe
 		set safeName to do shell script "echo " & quoted form of noteName & " | tr -cd '[:alnum:]_-'" 
 		
 		-- Set export path to Desktop
-		set filePath to (POSIX path of (path to desktop)) & "ExportedNotes/" & safeName & ".txt"
+		set folderPath to (POSIX path of (path to desktop)) & "ExportedNotes/"
+		set filePath to folderPath & safeName & ".txt"
 		
-		-- Create folder and write the note body to the file
-		do shell script "mkdir -p ~/Desktop/ExportedNotes && echo " & quoted form of noteBody & " > " & quoted form of filePath
+		-- Create folder (once)
+		do shell script "mkdir -p " & quoted form of folderPath
+		
+		-- Write the body using text file methods (avoids shell echo issues)
+		set fileRef to open for access POSIX file filePath with write permission
+		set eof of fileRef to 0
+		write noteBody to fileRef as «class utf8»
+		close access fileRef
 	end repeat
 end tell
