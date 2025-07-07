bd2397a62e9c45d02e2d605db9945e4c480964b9
Syed Hussain <aftab.jersey@gmail.com>
2025-07-07 14:21:59 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,19 +1,7 @@
-Below are the steps to move over Apple Notes
-
-On Old Mac:
-
-Click on Finder
-“Go” from the menu bar
-“Go to folder…”
-Copy and paste: ~/Library/Group Containers/
-Look for the folder called: group.com.apple.notes
-Right click on it and copy the folder
-Paste it into OneDrive
-
-On your new Mac:
-
-Click on Finder
-“Go” from the menu bar
-“Go to folder…”
-Copy and paste: ~/Library/Group Containers/
-Paste the group.com.apple.notes folder from OneDrive (you will replace it)
+Host github.com
+  Hostname ssh.github.com
+  Port 443
+  User git
+  AddKeysToAgent yes
+  UseKeychain yes
+  IdentityFile ~/.ssh/id_rsa
