c143600501f969c8183ad3c0ec2ab10f610564cb
Syed Hussain <aftab.jersey@gmail.com>
2025-07-07 11:55:29 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -8,3 +8,14 @@ tell application "Notes"
 		do shell script "echo " & quoted form of noteText & " > " & quoted form of filePath
 	end repeat
 end tell'
+
+
+
+osascript <<EOF
+tell application "Notes"
+	set noteList to every note
+	repeat with n in noteList
+		display dialog name of n
+	end repeat
+end tell
+EOF
