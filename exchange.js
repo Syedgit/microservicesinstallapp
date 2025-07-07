31caa6ea97bc3f7d774fa1b17444b9f1d62ab9bc
Syed Hussain <aftab.jersey@gmail.com>
2025-07-07 13:24:46 -0400

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,59 +1,19 @@
-use framework "Foundation"
-use scripting additions
+Below are the steps to move over Apple Notes
 
-on stripHTML(theHTML)
-	set theNSString to current application's NSString's stringWithString:theHTML
-	set regexPattern to "<[^>]+>"
-	set options to current application's NSRegularExpressionSearch
-	set range to {location:0, |length|:theNSString's |length|()}
-	set cleanString to theNSString's stringByReplacingOccurrencesOfString:regexPattern withString:"" options:options range:range
-	return cleanString as string
-end stripHTML
+On Old Mac:
 
--- Collect notes
-tell application "Notes"
-	set notesList to every note
-	set allCleanNotes to {}
-	repeat with n in notesList
-		set noteName to the name of n
-		set noteBody to the body of n
-		set end of allCleanNotes to {noteName, noteBody}
-	end repeat
-end tell
+Click on Finder
+“Go” from the menu bar
+“Go to folder…”
+Copy and paste: ~/Library/Group Containers/
+Look for the folder called: group.com.apple.notes
+Right click on it and copy the folder
+Paste it into OneDrive
 
--- Export clean files
-repeat with noteData in allCleanNotes
-	set noteName to item 1 of noteData
-	set noteBody to item 2 of noteData
-	
-	-- Fallback if title is empty
-	if noteName is "" then
-		set noteName to "untitled_" & (random number from 100000 to 999999) as string
-	end if
-	
-	-- Remove HTML from body
-	set cleanText to stripHTML(noteBody)
-	
-	-- Clean file name
-	set safeName to do shell script "echo " & quoted form of noteName & " | tr -cd '[:alnum:]_-'"
-	
-	-- Paths
-	set folderPath to (POSIX path of (path to desktop)) & "ExportedNotesClean/"
-	set filePath to folderPath & safeName & ".txt"
-	
-	-- Make folder
-	do shell script "mkdir -p " & quoted form of folderPath
-	
-	try
-		-- Write to file safely
-		set fileDescriptor to open for access (POSIX file filePath) with write permission
-		set eof of fileDescriptor to 0
-		write cleanText to fileDescriptor as «class utf8»
-		close access fileDescriptor
-	on error errMsg number errNum
-		try
-			close access (POSIX file filePath)
-		end try
-		log "Error writing note: " & noteName & " — " & errMsg
-	end try
-end repeat
+On your new Mac:
+
+Click on Finder
+“Go” from the menu bar
+“Go to folder…”
+Copy and paste: ~/Library/Group Containers/
+Paste the group.com.apple.notes folder from OneDrive (you will replace it)
