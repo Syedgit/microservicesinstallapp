51ba44e60663f409d572eed0bef4f7b64992375d
Syed Hussain <aftab.jersey@gmail.com>
2025-12-27 22:25:55 -0500

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,47 +1,272 @@
-<nav>
-  <ul class="nav-menu">
-    <div class="nav-menu__wrap-items"
-         *ngIf="authUser">
-      <li class="nav-menu__item"
-          routerLinkActive="active">
-        <a routerLink="/recipes">All recipes</a>
-      </li>
-      <li class="nav-menu__item"
-          routerLinkActive="active">
-        <a routerLink="/recipe/add">Add recipe</a>
-      </li>
-      <li class="nav-menu__item"
-          routerLinkActive="active">
-        <a [routerLink]="['/profile', authUser.id]">User profile</a>
-      </li>
-    </div>
-    <div class="nav-menu__wrap-items"
-         *ngIf="authUser && authUser?.isAdmin">
-      <li class="nav-menu__item"
-          routerLinkActive="active">
-        <a [routerLink]="['/admin/recipes']">Recipes</a>
-      </li>
-      <li class="nav-menu__item"
-          routerLinkActive="active">
-        <a [routerLink]="['/admin/users']">Users</a>
-      </li>
-    </div>
-    <div *ngIf="!authUser"
-         class="nav-menu__wrap-items">
-      <li class="nav-menu__item"
-          routerLinkActive="active">
-        <a routerLink="/login">Login</a>
-      </li>
-      <li class="nav-menu__item"
-          routerLinkActive="active">
-        <a routerLink="/register">Register</a>
-      </li>
-    </div>
-    <div *ngIf="authUser"
-         class="nav-menu__wrap-items">
-      <li class="nav-menu__item">
-        <a (click)="logout()">Logout</a>
-      </li>
-    </div>
-  </ul>
-</nav>
+solution # 1
+
+
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  isServerError = false;
+  serverErrorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.loginForm = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required]),
+      },
+      this.formValidator.bind(this)
+    );
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(this.loginForm.value).subscribe(
+      () => {
+        const redirectUrl = this.authService.redirectUrl;
+        if (redirectUrl) {
+          this.router.navigate([redirectUrl]);
+        } else {
+          this.router.navigate(['/recipes']);
+        }
+      },
+      (error) => {
+        switch (error.error.type) {
+          case 'badCredentialsException':
+            this.serverErrorMessage = 'Invalid email or password';
+            break;
+          case 'accessDeniedException':
+            this.serverErrorMessage = 'The user is blocked';
+            break;
+          default:
+            this.serverErrorMessage = 'Login error. Please try again later';
+        }
+
+        this.isServerError = true;
+        this.loginForm.updateValueAndValidity();
+      }
+    );
+  }
+
+  formValidator() {
+    if (!this.isServerError) return null;
+
+    this.isServerError = false;
+    return { serverError: true };
+  }
+}
+
+Solution #2 
+
+Selected solution 
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  isServerError = false;
+  serverErrorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.loginForm = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required]),
+      },
+      this.formValidator.bind(this)
+    );
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(this.loginForm.value).subscribe(
+      () => {
+        const redirectUrl = this.authService.redirectUrl;
+        const authUser = this.authService.userChange$.value;
+
+        if (
+          (redirectUrl && !redirectUrl.includes('admin')) ||
+          (redirectUrl && redirectUrl.includes('admin') && authUser?.isAdmin)
+        ) {
+          this.router.navigate([redirectUrl]);
+        } else {
+          this.router.navigate(['/recipes']);
+        }
+      },
+      (error) => {
+        switch (error.error.type) {
+          case 'badCredentialsException':
+            this.serverErrorMessage = 'Invalid email or password';
+            break;
+          case 'accessDeniedException':
+            this.serverErrorMessage = 'The user is blocked';
+            break;
+          default:
+            this.serverErrorMessage = 'Login error. Please try again later';
+        }
+
+        this.isServerError = true;
+        this.loginForm.updateValueAndValidity();
+      }
+    );
+  }
+
+  formValidator() {
+    if (!this.isServerError) return null;
+
+    this.isServerError = false;
+    return { serverError: true };
+  }
+}
+
+Solution # 3
+
+
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  isServerError = false;
+  serverErrorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.loginForm = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required]),
+      },
+      this.formValidator.bind(this)
+    );
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(this.loginForm.value).subscribe(
+      () => {
+        const redirectUrl = this.authService.redirectUrl;
+        if (redirectUrl) {
+          this.router.navigate([redirectUrl]);
+        } else {
+          this.router.navigate(['/recipes']);
+        }
+      },
+      (error) => {
+        switch (error.error.type) {
+          case 'badCredentialsException':
+            this.serverErrorMessage = 'Invalid email or password';
+            break;
+          case 'accessDeniedException':
+            this.serverErrorMessage = 'The user is blocked';
+            break;
+          default:
+            this.serverErrorMessage = 'Login error. Please try again later';
+        }
+
+        this.isServerError = true;
+        this.loginForm.updateValueAndValidity();
+      }
+    );
+  }
+
+  formValidator() {
+    if (!this.isServerError) return null;
+
+    this.isServerError = false;
+    return { serverError: true };
+  }
+}
+
+Solution # 4
+
+
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  isServerError = false;
+  serverErrorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.loginForm = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required]),
+      },
+      this.formValidator.bind(this)
+    );
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(this.loginForm.value).subscribe(
+      () => {
+        const redirectUrl = this.authService.redirectUrl;
+        if (redirectUrl) {
+          this.router.navigate([redirectUrl]);
+        } else {
+          this.router.navigate(['/recipes']);
+        }
+      },
+      (error) => {
+        switch (error.error.type) {
+          case 'badCredentialsException':
+            this.serverErrorMessage = 'Invalid email or password';
+            break;
+          case 'accessDeniedException':
+            this.serverErrorMessage = 'The user is blocked';
+            break;
+          default:
+            this.serverErrorMessage = 'Login error. Please try again later';
+        }
+
+        this.isServerError = true;
+        this.loginForm.updateValueAndValidity();
+      }
+    );
+  }
+
+  formValidator() {
+    if (!this.isServerError) return null;
+
+    this.isServerError = false;
+    return { serverError: true };
+  }
+}
